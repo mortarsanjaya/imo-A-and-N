e22f025a30f8feb482f32b5e2a3854181112d1ce
Gian Sanjaya <mortarsanjaya@gmail.com>
2022-06-05 17:03:52 -0400

    Create A6_real.lean
    Completed 2017 A6 for the original case F = R

---
+++ src/IMO2017/A6/A6_real.lean
@@ -0,0 +1,27 @@
+import
+  IMO2017.A6.A6
+  IMO2017.A6.A6_char_ne_2
+  data.real.basic
+
+/-
+  Solution of 2017 A6 for the originial case : F = ℝ
+  This file simply states the main result in "A6_char_ne_2.lean" for the original case..
+-/
+
+namespace IMO2017A6
+
+namespace case_real
+
+theorem fn_all :
+  set_of (fn_eq ℝ) = {0, 1 - id, id - 1} :=
+begin
+  apply case_char_ne_2.fn_all ℝ,
+  rw ring_char.eq ℝ 0,
+  norm_num,
+end
+
+end case_real
+
+end IMO2017A6
+
+
