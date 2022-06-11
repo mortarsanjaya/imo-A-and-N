0d367d99c3c4da4f4ff3371e26e803d3841873b6
Gian Sanjaya <mortarsanjaya@gmail.com>
2022-06-10 21:19:14 -0400

    Update poly_result.lean
    Finishes the proof of the main result

--- src/IMO2017/A6/A6_char_eq_2/poly_result.lean
+++ src/IMO2017/A6/A6_char_eq_2/poly_result.lean
@@ -1,13 +1,31 @@
 import
   IMO2017.A6.A6_char_eq_2.base_lemma
-  data.zmod.basic
   data.zmod.algebra
-  data.polynomial.basic
   data.polynomial.eval
-  data.polynomial.inductions
+  data.polynomial.div
 
 /-
-  Results involving the polynomial ring 𝔽₂[X]
+  The big polynomial result: for any a, b ∈ F, if f(a) = f(b), then
+          ∀ P ∈ 𝔽₂[X], f(P(a)) = f(P(b))
+
+  Proof:
+  Strong induction on deg(P) with deg(P) ≤ 1 as a base case.
+
+  Base case:
+  For deg(P) ≤ 1, P = cX + d for c, d ∈ 𝔽₂[X]
+  For c = 0, trivial
+  For c = 1, d = 0, trivial as well
+  For c = 1, d = 1, true by f(x + 1) = f(x) + 1
+
+  Induction step:
+  Suppose that for some n > 1, the above result holds whenever deg(P) < n.
+  Take some P with deg(P) = n.
+  By f(x + 1) = f(x) + 1, WLOG we can assume that X ∣ P.
+  Write P = XQ for some Q ∈ 𝔽₂[X].
+  Since deg(Q) < deg(P) = n, by IH, f(Q(a)) = f(Q(b)).
+  Together with f(a) = f(b), we get f(Q(a) a) + f(Q(a) + a) = f(Q(b) b) + f(Q(b) + b).
+  But Q + X has degree ≤ max(1, deg(Q)) < n.
+  Thus, f(Q(a) + a) = f(Q(b) + b), which implies f(Q(a) a) = f(Q(b) b).
 -/
 
 
@@ -18,11 +36,7 @@ universe u
 variable F : Type u
 variable [field F]
 
-/-
-  The big polynomial result: for any a, b ∈ F, if f(a) = f(b), then
-          ∀ P ∈ 𝔽₂[X], f(P(a)) = f(P(b))
-  We proceed by induction on deg(P)
--/
+
 
 namespace case_char_eq_2
 
@@ -42,29 +56,6 @@ open polynomial
 
 def phi2F := zmod.cast_hom (dvd_refl 2)
 
-lemma zmod2_values :
-  ∀ c : zmod 2, c = 0 ∨ c = 1 :=
-begin
-  intros c,
-  cases c with x x_prop,
-  rcases x with _ | x | x,
-  { left; refl, },
-  { right; refl, },
-  { exfalso,
-    rw [nat.succ_lt_succ_iff, nat.add_def, add_zero, nat.succ_lt_succ_iff] at x_prop,
-    have := nat.not_lt_zero x,
-    contradiction, },
-end
-
-lemma zmod2_coe :
-  ∀ c : zmod 2, (phi2F F) c = ↑c :=
-begin
-  intros c,
-  cases c,
-  unfold phi2F,
-  simp,
-end
-
 
 
 variable f : F → F
@@ -74,89 +65,88 @@ include feq1 feq2
 
 
 
-lemma fn_aux :
-  ∀ (x : F) (c : zmod 2), f (x + c) = f x + c :=
-begin
-  intros x c,
-  cases (zmod2_values c) with h h,
-  all_goals { subst h; simp, },
-  exact feq2 x,
-end
-
-lemma fn_lem1 (a b : F) (h : f a = f b) :
+lemma fn_induction_intro (a b : F) (h : f a = f b) :
   ∀ P : polynomial (zmod 2),
     f (eval₂ (phi2F F) a P) = f (eval₂ (phi2F F) b P) →
-      f (eval₂ (phi2F F) a (P + X)) = f (eval₂ (phi2F F) b (P + X)) →
-        f (eval₂ (phi2F F) a (P * X)) = f (eval₂ (phi2F F) b (P * X)) :=
+      f (eval₂ (phi2F F) a (X + P)) = f (eval₂ (phi2F F) b (X + P)) →
+        f (eval₂ (phi2F F) a (X * P)) = f (eval₂ (phi2F F) b (X * P)) :=
 begin
-  simp,
-  intros P h0 h1,
-  let c := eval₂ (phi2F F) a P,
-  let d := eval₂ (phi2F F) b P,
-  have h2 := base_lemma.fn_lem2_5 F f feq1 feq2 a b h c,
-  have h3 := base_lemma.fn_lem2_5 F f feq1 feq2 c d h0 b,
-  rwa [mul_comm c b, add_comm c b, ← h2,
-       mul_comm a c, add_comm a c, h1, add_left_inj] at h3,
-end
-
-lemma fn_lem2 (a b : F) (h : f a = f b) (n : ℕ) (h0 : 1 ≤ n) :
-  (∀ P : polynomial (zmod 2), P.nat_degree ≤ n →
-    f (eval₂ (phi2F F) a P) = f (eval₂ (phi2F F) b P)) →
-  (∀ P : polynomial (zmod 2), P.nat_degree ≤ n.succ →
-    f (eval₂ (phi2F F) a P) = f (eval₂ (phi2F F) b P)) :=
-begin
-  intros M_in P h1,
-  rw [← div_X_mul_X_add P, eval₂_add, eval₂_add, eval₂_C, eval₂_C,
-      zmod2_coe, fn_aux F f feq1 feq2, fn_aux F f feq1 feq2, add_left_inj],
-  have h2 : P.div_X.nat_degree ≤ n,
-  { rw ← nat.lt_succ_iff,
-    sorry, },
-  apply fn_lem1 F f feq1 feq2 a b h,
-  { apply M_in,
-    exact h2, },
-  { apply M_in,
-    apply nat_degree_add_le_of_degree_le,
-    exact h2,
-    simp; exact h0, },
+  simp only [eval₂_X, eval₂_add, eval₂_mul]; intros P h0 h1,
+  let Pa := eval₂ (phi2F F) a P,
+  let Pb := eval₂ (phi2F F) b P,
+  calc f (a * Pa) = f (a * Pa) + f (a + Pa) - f (a + Pa) : by rw add_sub_cancel
+  ... = f (b * Pa) + f (b + Pa) - f (a + Pa) : by rw base_lemma.fn_lem2_5 F f feq1 feq2 a b h
+  ... = f (Pa * b) + f (Pa + b) - f (a + Pa) : by rw [mul_comm, add_comm b Pa]
+  ... = f (Pb * b) + f (Pb + b) - f (a + Pa) : by rw base_lemma.fn_lem2_5 F f feq1 feq2 Pa Pb h0
+  ... = f (b * Pb) + f (b + Pb) - f (a + Pa) : by rw [mul_comm, add_comm Pb b]
+  ... = f (b * Pb) : by rw [h1, add_sub_cancel],
 end
 
 theorem fn_thm (a b : F) (h : f a = f b) :
   ∀ P : polynomial (zmod 2), f (eval₂ (phi2F F) a P) = f (eval₂ (phi2F F) b P) :=
 begin
-  let M := λ n : ℕ, ∀ P : polynomial (zmod 2),
-    P.nat_degree ≤ n → f (eval₂ (phi2F F) a P) = f (eval₂ (phi2F F) b P),
-  suffices : ∀ n : ℕ, M n,
-  { intros P,
-    apply this P.nat_degree,
-    exact le_refl P.nat_degree, },
 
-  intros n,
-  induction n with n,
+  ---- Start with describing all the elements of zmod 2
+  have zmod2_elts : ∀ c : zmod 2, c = 0 ∨ c = 1 :=
+  begin
+    intros c,
+    cases c with x x_prop,
+    rcases x with _ | x | x,
+    { left; refl, },
+    { right; refl, },
+    { rw [nat.succ_lt_succ_iff, nat.add_def, add_zero, nat.succ_lt_succ_iff] at x_prop,
+      have := nat.not_lt_zero x,
+      contradiction, },
+  end,
 
-  ---- Case n = 0
-  { intros P h0,
-    rw eq_C_of_nat_degree_le_zero h0,
-    simp, },
-  
-  cases n,
-  ---- Case n = 1
-  { intros P h0,
-    rcases exists_eq_X_add_C_of_nat_degree_le_one h0 with ⟨c, d, h1⟩,
-    rw h1; simp,
-    rw [zmod2_coe, zmod2_coe, fn_aux F f feq1 feq2, fn_aux F f feq1 feq2, add_left_inj],
-    cases zmod2_values c with h2 h2; subst h2; simp,
+  ---- Some stupid result
+  have zmod2_coe : ∀ c : zmod 2, (phi2F F) c = ↑c :=
+  begin
+    intros c,
+    cases c,
+    unfold phi2F,
+    simp,
+  end,
+
+  ---- Another stupid result
+  have fn_aux : ∀ (x : F) (c : zmod 2), f (x + c) = f x + c :=
+  begin
+    intros x c,
+    cases zmod2_elts c with h h,
+    all_goals { subst h; simp, },
+    exact feq2 x,
+  end,
+
+  ---- Start of the main result
+  intros P,
+  induction h0 : P.nat_degree using nat.strong_induction_on with n n_ih generalizing P,
+  cases le_or_lt n 1 with h1 h1,
+
+  -- Case n ≤ 1
+  { rw ← h0 at h1,
+    rw eq_X_add_C_of_nat_degree_le_one h1; simp,
+    rw [zmod2_coe, zmod2_coe, fn_aux, fn_aux, add_left_inj],
+    cases zmod2_elts (P.coeff 1) with Px Px; rw Px; simp,
     exact h, },
-  
-  ---- Case n > 1
-  { have h0 := nat.succ_pos n,
-    rw [nat.lt_iff_add_one_le, zero_add] at h0,
-    exact fn_lem2 F f feq1 feq2 a b h n.succ h0 n_ih, },
 
+  -- Case 1 < n
+  { rw [← mod_by_monic_add_div P monic_X, mod_by_monic_X, add_comm, eval₂_add,
+        eval₂_add, eval₂_C, eval₂_C, zmod2_coe, fn_aux, fn_aux, add_left_inj],
+    suffices : (P /ₘ X).nat_degree < n,
+    { apply fn_induction_intro F f feq1 feq2 a b h,
+      { apply n_ih (P /ₘ X).nat_degree this; refl, },
+      { apply n_ih (X + P /ₘ X).nat_degree,
+        swap,
+        refl,
+        apply lt_of_le_of_lt (nat_degree_add_le X _),
+        simp only [nat_degree_X, max_lt_iff],
+        split; assumption, }, },
+    rw [nat_degree_div_by_monic P monic_X, nat_degree_X, h0],
+    exact nat.sub_lt (lt_trans zero_lt_one h1) zero_lt_one, },
 end
 
-
-
 end poly_result
+
 
 
 
