a173378118dafaedeb079310a48dab14306d28d9
Gian Sanjaya <mortarsanjaya@gmail.com>
2022-06-28 08:12:58 -0400

    Golf 2013 N3

--- src/IMO2013/N3/N3_general.lean
+++ src/IMO2013/N3/N3_general.lean
@@ -1,9 +1,6 @@
-import
-  IMO2013.N3.N3_extra
-  data.pnat.basic
-  data.set.basic
-  data.set.finite
+import IMO2013.N3.N3_extra data.set.finite
 
+namespace IMOSL
 namespace IMO2013N3
 
 universe u
@@ -65,12 +62,7 @@ variables {S : Type u} [linear_order S]
 
   3. In the implementation, instead of using n in the good predicate, we use n + 1.
 -/
-def good (f : ℕ+ → S) (n : ℕ+) :=
-  f (n ^ 4 + n ^ 2 + 1) = f ((n + 1) ^ 4 + (n + 1) ^ 2 + 1)
-
-
-
-
+def good (f : ℕ+ → S) (n : ℕ+) := f (n ^ 4 + n ^ 2 + 1) = f ((n + 1) ^ 4 + (n + 1) ^ 2 + 1)
 
 
 
@@ -79,97 +71,80 @@ namespace results
 variables {f : ℕ+ → S} (fmax : ∀ a b : ℕ+, f (a * b) = max (f a) (f b))
 include fmax
 
-lemma lem1 (n : ℕ+) :
-  good f (n + 1) ↔ f (T ((n + 1) ^ 2)) = f (T ((n + 1 + 1) ^ 2)) :=
-    by unfold good T; rw [nat.bit0_val, pow_mul, pow_mul]
-
-lemma lem2 (n : ℕ+) :
-  good f (n + 1) ↔ max (f (T n)) (f (T (n + 1))) = max (f (T (n + 1))) (f (T (n + 1 + 1))) :=
-    by rw [lem1 fmax, T_sq_factor, fmax, T_sq_factor, fmax]
-
-lemma lem3 (n : ℕ+) :
-  ¬good f (n + 1) → f (T n) ≤ f (T (n + 1)) → f (T (n + 1)) < f (T (n + 1 + 1)) :=
+lemma lem1 {n : ℕ+} (h : ¬good f (n + 1)) (h0 : f (T n) ≤ f (T (n + 1))) :
+  f (T (n + 1)) < f (T (n + 1 + 1)) :=
 begin
-  intros h h0,
   apply lt_of_not_le; intros h1,
-  apply h,
-  rw [lem2 fmax, max_eq_right h0, max_eq_left h1],
+  apply h; unfold good,
+  change 4 with 2 * 2,
+  repeat {rw [pow_mul, ← T_def, T_sq_factor, fmax] },
+  rw [max_eq_right h0, max_eq_left h1]
 end
 
-lemma lem4 (N : ℕ+) (h : ∀ n : ℕ+, N < n → ¬good f n) :
-  ∀ C : ℕ+, N < C → f (T C) ≤ f (T (C + 1)) → ∀ k : ℕ+, f (T (C + 1)) < f (T (C + 1 + k)) :=
+lemma lem2 {N : ℕ+} (h : ∀ n : ℕ+, N < n → ¬good f n)
+    {C : ℕ+} (h0 : N < C) (h1 : f (T C) ≤ f (T (C + 1))) :
+  ∀ k : ℕ+, f (T (C + 1)) < f (T (C + 1 + k)) :=
 begin
-  intros C h0 h1,
-  suffices : ∀ k : ℕ+, f (T (C + k)) < f (T (C + k + 1)),
-  { intros k,
-    induction k using pnat.strong_induction_on with k k_ih,
-    cases decidable.eq_or_ne k 1 with h2 h2,
-  
-    ---- Case 1: k = 1
-    rw h2; exact lem3 fmax C (h _ (lt_trans h0 (pnat.lt_add_right C 1))) h1,
+  suffices : ∀ k : ℕ+, f (T (C + k)) < f (T (C + k + 1)) ∧ f (T (C + 1)) < f (T (C + 1 + k)),
+  intros k; rcases this k with ⟨h2, h3⟩; exact h3,
+  intros k; induction k using pnat.strong_induction_on with k k_ih,
+  rcases decidable.eq_or_ne k 1 with rfl | h2,
+  split; exact lem1 fmax (h _ (lt_trans h0 (pnat.lt_add_right C 1))) h1,
+  rcases pnat.exists_eq_succ_of_ne_one h2 with ⟨k₀, rfl⟩,
+  rcases k_ih k₀ (pnat.lt_add_right k₀ 1) with ⟨h3, h4⟩,
+  have h5 : f (T (C + k₀ + 1)) < f (T (C + k₀ + 1 + 1)) :=
+    lem1 fmax (h _ (lt_trans h0 (pnat.lt_add_right C (k₀ + 1)))) (le_of_lt h3),
+  split,
+  rwa ← add_assoc,
+  apply lt_trans h4,
+  rwa [add_right_comm, add_add_add_comm]
+end
 
-    ---- Case 2: k ≠ 1
-    cases pnat.exists_eq_succ_of_ne_one h2 with k₀ h3; subst h3,
-    apply lt_trans (k_ih k₀ (pnat.lt_add_right k₀ 1)),
-    rw [← add_assoc, add_assoc]; exact this _ },
-  
-  intros k; apply pnat.rec_on k; clear k,
-  exact lem3 fmax C (h _ (lt_trans h0 (pnat.lt_add_right C 1))) h1,
-  intros k h2,
-  rw ← add_assoc; refine lem3 fmax _ (h _ _) (le_of_lt h2),
-  rw add_assoc; exact lt_trans h0 (pnat.lt_add_right C (k + 1)),
+lemma lem3 {N : ℕ+} (h : ∀ n : ℕ+, N < n → ¬good f n) {C : ℕ+} (h0 : N < C) :
+  f (T (C + 1)) < f (T C) :=
+begin
+  apply lt_of_not_le; intros h1,
+  have h2 := lem2 fmax h h0 h1 (C * (C + 1)),
+  rw [← one_add_mul, add_comm 1 C, ← sq, T_sq_factor, fmax, max_eq_right h1] at h2,
+  exact lt_irrefl _ h2
+end
+
+lemma lem4 {N : ℕ+} (h : ∀ n : ℕ+, N < n → ¬good f n) {C : ℕ+} (h0 : N < C) :
+  ∀ k : ℕ+, f (T (C + k)) < f (T C) :=
+begin
+  intros k,
+  apply pnat.rec_on k; clear k,
+  exact lem3 fmax h h0,
+  intros k h1,
+  refine lt_trans _ h1,
+  rw ← add_assoc; apply lem3 fmax h,
+  exact lt_trans h0 (pnat.lt_add_right C k)
 end
 
 lemma lem5 (N : ℕ+) (h : ∀ n : ℕ+, N < n → ¬good f n) :
-  ∀ n : ℕ+, N < n → f (T (n + 1)) < f (T n) :=
-begin
-  intros C h0,
-  apply lt_of_not_le; intros h1,
-  have h2 := lem4 fmax N h C h0 h1 (C * (C + 1)),
-  rw [← one_add_mul, add_comm 1 C, ← sq, T_sq_factor, fmax, max_eq_right h1] at h2,
-  exact lt_irrefl _ h2,
-end
-
-lemma lem6 (N : ℕ+) (h : ∀ n : ℕ+, N < n → ¬good f n) :
-  ∀ n : ℕ+, N < n → ∀ k : ℕ+, f (T (n + k)) < f (T n) :=
-begin
-  intros n h0 k,
-  apply pnat.rec_on k; clear k,
-  exact lem5 fmax N h n h0,
-  intros k h1,
-  refine lt_trans _ h1,
-  rw ← add_assoc; apply lem5 fmax N h,
-  exact lt_trans h0 (pnat.lt_add_right n k),
-end
-
-lemma lem7 (N : ℕ+) (h : ∀ n : ℕ+, N < n → ¬good f n) :
   false :=
 begin
-  have h0 := lem6 fmax N h (N + 1) (pnat.lt_add_right N 1) (N * (N + 1)),
+  have h0 := lem4 fmax h (pnat.lt_add_right N 1) (N * (N + 1)),
   rw [← one_add_mul, add_comm, ← sq, T_sq_factor, fmax, lt_iff_not_le] at h0,
-  exact h0 (le_max_right (f (T N)) (f (T (N + 1)))),
+  exact h0 (le_max_right (f (T N)) (f (T (N + 1))))
 end
   
 end results
 
 
 
-
-
-
-
+/-- Final solution -/
 theorem final_solution_general {f : ℕ+ → S} (fmax : ∀ a b : ℕ+, f (a * b) = max (f a) (f b)) :
   set.infinite (set_of (good f)) :=
 begin
   intros h,
-  have h0 := set.finite.bdd_above h,
-  cases h0 with N h0,
-  unfold upper_bounds at h0,
-  simp only [set.mem_set_of_eq] at h0,
-  apply results.lem7 fmax N,
+  cases set.finite.bdd_above h with N h0,
+  simp [upper_bounds] at h0,
+  apply results.lem5 fmax N,
   intros n h1 h2,
   rw lt_iff_not_le at h1,
-  exact h1 (h0 h2),
+  exact h1 (h0 h2)
 end
 
 end IMO2013N3
+end IMOSL
