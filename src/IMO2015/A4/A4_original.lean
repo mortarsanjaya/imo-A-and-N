31c32dacdefbe42c99de80f4abc60a4769687a0e
Gian Sanjaya <mortarsanjaya@gmail.com>
2022-06-28 20:59:48 -0400

    Minor changes to 2015 A4

--- src/IMO2015/A4/A4_original.lean
+++ src/IMO2015/A4/A4_original.lean
@@ -1,19 +1,16 @@
-import
-  IMO2015.A4.A4_general
-  data.real.basic 
-  data.set.basic
+import IMO2015.A4.A4_general data.real.basic 
 
-/-
+namespace IMOSL
+namespace IMO2015A4
+
+/--
   IMO 2015 A4 (P5), Original Version
 
   Plainly follows from the generalized version.
   See "A4_general.lean" for the generalized version.
 -/
-
-namespace IMO2015A4
-
-theorem final_solution_original :
-  set_of fn_eq = ({id, 2 - id} : set (ℝ → ℝ)) :=
-    final_solution_general
+theorem final_solution_original (f : ℝ → ℝ) : fn_eq f ↔ f = id ∨ f = 2 - id :=
+    final_solution_general f
 
 end IMO2015A4
+end IMOSL
