81b7975ed703c6ba286972d8a384ef7c775d0d9c
Gian Sanjaya <mortarsanjaya@gmail.com>
2022-06-29 07:46:42 -0400

    Add original formulation of IMO 2013 N3

--- src/IMO2013/N3/N3_general.lean
+++ src/IMO2013/N3/N3_general.lean
@@ -1,4 +1,4 @@
-import IMO2013.N3.N3_extra data.set.finite
+import data.set.finite tactic.ring
 
 namespace IMOSL
 namespace IMO2013N3
@@ -62,6 +62,18 @@ variables {S : Type*} [linear_order S]
   3. In the implementation, instead of using n in the good predicate, we use n + 1.
 -/
 def good (f : ℕ+ → S) (n : ℕ+) := f (n ^ 4 + n ^ 2 + 1) = f ((n + 1) ^ 4 + (n + 1) ^ 2 + 1)
+
+
+
+/-- A notation convenience for n² + n + 1. -/
+def T (n : ℕ+) := n ^ 2 + n + 1
+
+/-- Equation defining T -/
+lemma T_def (n : ℕ+) : T n = n ^ 2 + n + 1 := rfl
+
+/-- Proof of the identity T((n + 1)²) = T(n) T(n + 1). -/
+lemma T_sq_factor (n : ℕ+) : T ((n + 1) ^ 2) = T n * T (n + 1) :=
+  by unfold T; apply pnat.eq; simp; ring
 
 
 
---
+++ src/IMO2013/N3/N3_original.lean
@@ -0,0 +1,62 @@
+import IMO2013.N3.N3_general data.pnat.prime data.pnat.factors data.finset.lattice
+
+namespace IMOSL
+namespace IMO2013N3
+
+/-!
+# IMO 2013 N3 (Original Version)
+
+Prove that there exists infinitely many positive integers n such that the largest prime
+divisor of n^4 + n^2 + 1 is equal to the largest prime divisor of (n + 1)^4 + (n + 1) + 1.
+
+# Solution
+
+Let f : ℕ+ → ℕ+ denote the largest prime divisor function.
+From the general version, it suffices to check that f(ab) = max{f(a), f(b)} for all a, b ∈ ℕ⁺.
+This is standard.
+
+# Implementation details
+
+We still need to define the largest divisor function and prove the desired property.
+-/
+
+
+
+namespace extra
+
+/-- The maximum prime divisor of a positive integer n, as a `nat`, or 1 if n = 1. -/
+def pnat.max_prime_divisor (n : ℕ+) :=
+  (n.factor_multiset : multiset ℕ).fold linear_order.max 1
+
+lemma pnat.max_prime_divisor_one : pnat.max_prime_divisor 1 = 1 :=
+  by unfold pnat.max_prime_divisor; rw pnat.factor_multiset_one; refl
+
+lemma pnat.max_prime_divisor_prime (p : ℕ+) (h : p.prime) : pnat.max_prime_divisor p = p :=
+begin
+  unfold pnat.prime at h,
+  unfold pnat.max_prime_divisor,
+  let q : nat.primes := ⟨(p : ℕ), h⟩,
+  have h0 : p = ↑q := by ext1; rw nat.primes.coe_pnat_nat; refl,
+  rw [h0, pnat.factor_multiset_of_prime q, prime_multiset.coe_nat_of_prime,
+      multiset.fold_singleton, nat.primes.coe_pnat_nat, subtype.coe_mk],
+  exact max_eq_left (le_of_lt (nat.prime.one_lt h))
+end
+
+lemma pnat.max_prime_divisor_mul (a b : ℕ+) :
+  pnat.max_prime_divisor (a * b) = max (pnat.max_prime_divisor a) (pnat.max_prime_divisor b) :=
+begin
+  unfold pnat.max_prime_divisor,
+  rw [pnat.factor_multiset_mul, ← prime_multiset.coe_coe_nat_monoid_hom,
+      add_monoid_hom.map_add, ← multiset.fold_add max, max_self]
+end
+
+end extra
+
+
+
+/-- Final solution -/
+theorem final_solution_original : set.infinite (set_of (good extra.pnat.max_prime_divisor)) :=
+  final_solution_general extra.pnat.max_prime_divisor_mul
+
+end IMO2013N3
+end IMOSL
