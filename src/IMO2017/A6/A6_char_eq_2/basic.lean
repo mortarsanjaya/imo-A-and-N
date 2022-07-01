4be8be0749042e978b1a31d77a91909be7bdfe1e
Gian Sanjaya <mortarsanjaya@gmail.com>
2022-07-01 07:24:37 -0400

    Update basic.lean
    Slight documentation change.
    Remove dependency on the old FE.

--- src/IMO2017/A6/A6_char_eq_2/basic.lean
+++ src/IMO2017/A6/A6_char_eq_2/basic.lean
@@ -1,31 +1,23 @@
 import IMO2017.A6.A6_general algebra.char_p.two algebra.char_p.pi
 
 /-!
-  Progress of 2017 A6 for the case char(F) = 2
+## Progress of 2017 A6 for the case char(F) = 2
 
-  Here, we give an alternative FE system as follows:
-          ∀ x y : F, f(f(x) f(y)) + f(x + y) = f(xy + x + y)
-          ∀ x : F, f(x + 1) = f(x) + 1
-  We also give correspondence results between the two FE systems.
-  Then, we prove basic lemmas about the new FE system.
+Here, we deal with an alternative FE system as follows:
+  ∀ x y ∈ F, f(f(x) f(y)) + f(x + y) = f(xy + x + y)
+  ∀ x ∈ F, f(x + 1) = f(x) + 1
 -/
 
 open char_two
+open_locale classical
 
 namespace IMOSL
 namespace IMO2017A6
 
 variables {F : Type*} [field F]
 
-
 def fn_eq1 (f : F → F) := ∀ x y : F, f (f x * f y) + f (x + y) = f (x * y + x + y)
 def fn_eq2 (f : F → F) := ∀ x : F, f (x + 1) = f x + 1
-
-
-
-
-
-
 
 
 
@@ -106,99 +98,69 @@ include feq1 feq2
 
 
 
----- Results transferred from general case
-lemma fn_lem1_1 :
-  f 0 = 0 :=
+lemma fn_lem1_1 : f 0 = 0 :=
 begin
-  have h := results.fn_general3_4 (correspondence.fn_thm3 feq1 feq2),
-  simp only [] at h,
-  rwa char_two.add_self_eq_zero at h,
+  have h := feq1 0 0,
+  rw [mul_zero, add_zero, add_zero, add_left_eq_self] at h,
+  have h0 := feq1 (f 0 * f 0) 0,
+  rwa [h, zero_mul, mul_zero, zero_add, add_zero, add_left_eq_self] at h0
 end
 
-lemma fn_lem1_2 :
-  f 1 = 1 :=
-begin
-  calc f 1 = f (0 + 1) : by rw zero_add
-  ... = f 0 + 1 : by rw feq2
-  ... = 0 + 1 : by rw fn_lem1_1 feq1 feq2
-  ... = 1 : by rw zero_add,
-end
+lemma fn_lem1_2 : f 1 = 1 :=
+  by rw [← zero_add (1 : F), feq2, fn_lem1_1 feq1 feq2]
 
-lemma fn_lem1_3 :
-  ∀ x : F, f x = 0 ↔ x = 0 :=
+lemma fn_lem1_3 (x : F) : f x = 0 ↔ x = 0 :=
 begin
-  intros x,
-  calc f x = 0 ↔ f (x + 1 + 1) = 0 : by rw [← sub_eq_add, add_sub_cancel]
-  ... ↔ x + 1 = 1 : _
-  ... ↔ x = 0 : by rw add_left_eq_self,
-  { rw results.fn_general3 (correspondence.fn_thm3 feq1 feq2),
-    intros h,
-    have h0 : f (0 + 1) = (0 : F → F) 0 := by rw ← h; simp only [],
-    rw [zero_add, pi.zero_apply, fn_lem1_2 feq1 feq2] at h0,
-    exact one_ne_zero h0 },
+  rw iff.comm; split,
+  rintros rfl; exact fn_lem1_1 feq1 feq2,
+  intros h; by_contra h0,
+  have h1 := feq1 x x⁻¹,
+  rw [h, zero_mul, fn_lem1_1 feq1 feq2, zero_add, mul_inv_cancel h0,
+      add_assoc, add_comm (1 : F), feq2, eq_comm, add_right_eq_self] at h1,
+  exact one_ne_zero h1
 end
 
 
 
 ---- More results specialized for case char(F) = 2
-lemma fn_lem2_1 :
-  ∀ x : F, f (f x) = f x :=
+lemma fn_lem2_1 (x : F) : f (f x) = f x :=
 begin
-  intros x,
   have h := feq1 x 1,
   rwa [mul_one, char_two.add_self_eq_zero, zero_add, fn_lem1_2 feq1 feq2,
-       mul_one, feq2, ← add_assoc, add_left_eq_self, ← sub_eq_add, sub_eq_zero] at h,
+       mul_one, feq2, ← add_assoc, add_left_eq_self, ← sub_eq_add, sub_eq_zero] at h
 end
 
-lemma fn_lem2_2 :
-  ∀ x : F, f x = 1 ↔ x = 1 :=
-begin
-  intros x,
-  rw [← sub_eq_zero, sub_eq_add, ← feq2, fn_lem1_3 feq1 feq2, ← sub_eq_add, sub_eq_zero],
-end
+lemma fn_lem2_2 (x : F) : f x = 1 ↔ x = 1 :=
+  by rw [← sub_eq_zero, sub_eq_add, ← feq2, fn_lem1_3 feq1 feq2, ← sub_eq_add, sub_eq_zero]
 
-lemma fn_lem2_3 :
-  ∀ x : F, f (x⁻¹) = (f x)⁻¹ :=
+lemma fn_lem2_3 (x : F) : f (x⁻¹) = (f x)⁻¹ :=
 begin
-  intros x,
   by_cases h : x = 0,
   rw [h, inv_zero, fn_lem1_1 feq1 feq2, inv_zero],
   have h0 := feq1 x x⁻¹,
   rw [mul_inv_cancel h, add_assoc, (add_comm (1 : F)), feq2,
       add_comm, add_right_inj, fn_lem2_2 feq1 feq2] at h0,
-  rw inv_eq_of_mul_eq_one_right h0,
+  rw inv_eq_of_mul_eq_one_right h0
 end
 
-lemma fn_lem2_4 : 
-  ∀ x y : F, f (f x * f y) + f (x + y) + 1 = f ((x + 1) * (y + 1)) :=
-begin
-  intros x y,
-  rw [feq1, ← feq2, ← mul_add_one, add_assoc, ← add_one_mul],
-end
+lemma fn_lem2_4 (x y : F) : f (f x * f y) + f (x + y) + 1 = f ((x + 1) * (y + 1)) :=
+  by rw [feq1, ← feq2, ← mul_add_one, add_assoc, ← add_one_mul]
 
-lemma fn_lem2_5 :
-  ∀ x y : F, f ((f x + 1) * (f y + 1)) + 1 = f (x * y) + f (x + y) :=
+lemma fn_lem2_5 (x y : F) : f ((f x + 1) * (f y + 1)) + 1 = f (x * y) + f (x + y) :=
 begin
-  intros x y,
   have h := fn_lem2_4 feq1 feq2 (x + 1) (y + 1),
   rw [feq2, feq2, add_right_comm, ← eq_sub_iff_add_eq] at h,
   rw [h, sub_eq_add, add_assoc, add_assoc, add_add_add_comm,
       char_two.add_self_eq_zero, add_zero, add_zero, add_zero],
 end
 
-lemma fn_lem2_6 {a b : F} (h : f a = f b) :
-  ∀ x : F, f (a * x) + f (a + x) = f(b * x) + f (b + x) :=
-begin
-  intros x,
-  rw [← fn_lem2_5 feq1 feq2, h, fn_lem2_5 feq1 feq2],
-end
+lemma fn_lem2_6 {a b : F} (h : f a = f b) (x : F) : f (a * x) + f (a + x) = f(b * x) + f (b + x) :=
+  by rw [← fn_lem2_5 feq1 feq2, h, fn_lem2_5 feq1 feq2]
 
 lemma fn_lem2_7 {a b c d : F} (h : f a = f b) (h0 : f c = f d) :
   f (a * c) + f (a + c) = f(b * d) + f (b + d) :=
-begin
-  rw [fn_lem2_6 feq1 feq2 h, add_comm b, mul_comm,
-      fn_lem2_6 feq1 feq2 h0, add_comm d, mul_comm],
-end
+  by rw [fn_lem2_6 feq1 feq2 h, add_comm b, mul_comm,
+         fn_lem2_6 feq1 feq2 h0, add_comm d, mul_comm]
 
 
 
