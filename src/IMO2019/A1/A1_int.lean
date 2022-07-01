e1101c4fab1323224e4cf44f3b8c3a1d01f31841
Gian Sanjaya <mortarsanjaya@gmail.com>
2022-07-01 04:14:29 -0400

    Update A1_int.lean

--- src/IMO2019/A1/A1_int.lean
+++ src/IMO2019/A1/A1_int.lean
@@ -48,10 +48,7 @@ def End_int_of_int : ℤ ≃+* add_monoid.End ℤ :=
   .. int.cast_ring_hom (add_monoid.End ℤ) }
 
 lemma End_int_of_int_eq_mul (x y : ℤ) : End_int_of_int x y = x * y :=
-begin
-  simp only [End_int_of_int, int.cast_ring_hom],
-  exact coe_End_int_of_int_eq_left_mul x y
-end
+  by simp only [End_int_of_int, int.cast_ring_hom]; exact coe_End_int_of_int_eq_left_mul x y
 
 lemma End_int_eq_of_int_map_one (φ : ℤ →+ ℤ) : End_int_of_int (φ 1) = φ :=
   by change φ 1 with End_int_of_int.symm φ; rw ring_equiv.apply_symm_apply
@@ -60,10 +57,7 @@ lemma End_int_eq_map_one_mul (φ : ℤ →+ ℤ) (x : ℤ) : φ x = φ 1 * x :=
   by nth_rewrite 0 ← End_int_eq_of_int_map_one φ; rw End_int_of_int_eq_mul
 
 lemma End_int_inj_of_ne_zero {x : ℤ} (h : x ≠ 0) : injective (End_int_of_int x) :=
-begin
-  intros a b h0,
-  rwa [End_int_of_int_eq_mul, End_int_of_int_eq_mul, mul_right_inj' h] at h0
-end
+  λ a b h0, by rwa [End_int_of_int_eq_mul, End_int_of_int_eq_mul, mul_right_inj' h] at h0
 
 lemma End_int_eq_iff_eq_at_one (φ ρ : ℤ →+ ℤ) : φ = ρ ↔ φ 1 = ρ 1 :=
 begin
@@ -99,11 +93,11 @@ begin
       extra.End_int_eq_map_one_mul, mul_eq_mul_right_iff] at h1,
   cases h1 with h1 h1,
   ---- φ(1) = s; a contradiction
-  { exfalso; apply h; ext n,
-    have h3 : (φ ∘ g) n = (extra.End_int_of_int s * φ) n := by rw h0,
+  { exfalso; apply h; funext n,
+    replace h0 : (φ ∘ g) n = (extra.End_int_of_int s * φ) n := by rw h0,
     rw [comp_app, add_monoid.coe_mul, comp_app, extra.End_int_of_int_eq_mul,
-        extra.End_int_eq_map_one_mul, extra.End_int_eq_map_one_mul φ n, h1] at h3,
-    exact mul_left_cancel₀ s_ne_zero h3 },
+        extra.End_int_eq_map_one_mul, extra.End_int_eq_map_one_mul φ n, h1] at h0,
+    exact mul_left_cancel₀ s_ne_zero h0 },
   ---- φ(1) = 0; then f = 0
   { rw [extra.End_int_of_int_eq_mul, extra.End_int_eq_map_one_mul,
         h1, zero_mul, zero_eq_mul, or_iff_right s_ne_zero] at h2,
@@ -124,8 +118,7 @@ begin
         extra.End_int_of_int_eq_mul, pow_two, add_monoid.coe_mul, comp_app,
         extra.End_int_eq_map_one_mul, mul_eq_mul_right_iff] at h,
     cases h with h h,
-    { right,
-      use C; funext,
+    { right; use C; funext,
       rw [pi.add_apply, const_apply, extra.End_int_eq_map_one_mul, h] },
     { left,
       rw [extra.End_int_eq_map_one_mul, h, zero_mul, zero_eq_mul, or_iff_right s_ne_zero] at h0,
