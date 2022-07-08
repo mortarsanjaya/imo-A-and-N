66a5cbe14054438e2b81de13f168680c6e5e65dc
Gian Sanjaya <mortarsanjaya@gmail.com>
2022-07-08 07:18:00 -0400

    Slightly Generalize 2011 A3
    At the cost of having some weird pairs (f, g) satisfying the functional equation.

--- src/IMO2011/A3/A3_general.lean
+++ src/IMO2011/A3/A3_general.lean
@@ -3,24 +3,26 @@ import algebra.ring.basic tactic.ring
 /-!
 # IMO 2011 A3, Generalized Version
 
-Let R be a non-trivial domain such that 2 has an inverse in R.
+Let R be a commutative ring such that 2 has an inverse in R.
 Determine all pairs of (f, g) functions R → R such that, for all x, y ∈ R,
   g(f(x + y)) = f(x) + (2x + y) g(y).
 
 ## Answer
 
-(f, g) = (0, 0) and (f, g) = (x ↦ x² + C, id) for some C ∈ R.
+(f, g) = (x ↦ Ax² + C, x ↦ Ax) for some A, C ∈ R, such that A(A - 1) = C(A - 1) = 0.
 
 ## Solution
 
 See https://www.imo-official.org/problems/IMO2011SL.pdf.
-We will follow the official Solution.
-It adopts directly to any domain R with 2 being invertible.
+We will closely follow the official Solution.
+It adopts directly to any ring with invertible 2.
+However, after getting g = Ax + B and f = Ax^2 - Bx + C, all we can find is that
+  A^2 = A, B = 0, and AC = C; nothing more than that.
+The first equation can only be reduced to A ∈ {0, 1} when R is also a domain.
 
 # Notes
 
-1. The non-triviality is actually not required, but the trivial ring case is trivial.
-2. It seems that the case char(R) = 2 is too ugly to even consider, let alone the general case.
+It seems that the case char(R) = 2 is too ugly to even consider, let alone the general case.
 -/
 
 open function
@@ -28,7 +30,7 @@ open function
 namespace IMOSL
 namespace IMO2011A3
 
-variables {R : Type*} [comm_ring R] [is_domain R] [nontrivial R]
+variables {R : Type*} [comm_ring R] [invertible (2 : R)]
 
 def fn_eq (f g : R → R) := ∀ x y : R, g (f (x + y)) = f x + (2 * x + y) * g y
 
@@ -37,21 +39,21 @@ def fn_eq (f g : R → R) := ∀ x y : R, g (f (x + y)) = f x + (2 * x + y) * g 
 namespace extra
 
 /-- Ravi substitution -/
-lemma ravi_subst (exists_inv_2 : ∃ t : R, 2 * t = 1) (x y z : R) :
-  ∃ a b c : R, x = b + c ∧ y = c + a ∧ z = a + b :=
+lemma ravi_subst (x y z : R) : ∃ a b c : R, x = b + c ∧ y = c + a ∧ z = a + b :=
 begin
-  cases exists_inv_2 with t h,
-  use [(y + z - x) * t, (z + x - y) * t, (x + y - z) * t],
-  repeat { split }; ring_nf; rw [h, one_mul]
+  use [(y + z - x) * ⅟ 2, (z + x - y) *  ⅟ 2, (x + y - z) *  ⅟ 2],
+  repeat { split }; ring_nf; rw [mul_inv_of_self, one_mul]
 end
 
-/-- A criterion for a quadratic polynomial to be zero -/
-lemma poly_deg_2_zero (two_ne_zero : (2 : R) ≠ 0)
-    {A B C : R} (h : ∀ x : R, A * x ^ 2 + B * x + C = 0) :
+/-- A criterion for a quadratic polynomial to be zero over a ring with invertible 2 -/
+lemma poly_deg_2_zero {A B C : R} (h : ∀ x : R, A * x ^ 2 + B * x + C = 0) :
   A = 0 ∧ B = 0 ∧ C = 0 :=
 begin
   have h0 : 2 * B = (A * 1 ^ 2 + B * 1 + C) - (A * (-1) ^ 2 + B * (-1) + C) := by ring,
-  rw [h, h, sub_zero, mul_eq_zero, or_iff_right two_ne_zero] at h0,
+  rw [h, h, sub_zero] at h0,
+  replace h0 := congr_arg (λ x, ⅟ (2 : R) * x) h0,
+  simp only [] at h0,
+  rw [mul_zero, inv_of_mul_self_assoc] at h0,
   have h1 := h 0,
   rw [sq, mul_zero, mul_zero, zero_add, mul_zero, zero_add] at h1,
   have h2 := h 1,
@@ -83,11 +85,10 @@ begin
   rw [h, two_mul, ← add_assoc, neg_add_cancel_comm, add_comm, ← sub_eq_add_neg, add_comm]
 end
 
-lemma fn_lem3 (exists_inv_2 : ∃ t : R, 2 * t = 1) :
-  ∃ A B : R, ∀ x : R, g x = A * x + B :=
+lemma fn_lem3 : ∃ A B : R, ∀ x : R, g x = A * x + B :=
 begin
   use [g 1 - g 0, g 0]; intros x,
-  rcases extra.ravi_subst exists_inv_2 0 x 1 with ⟨a, b, c, h, rfl, h0⟩,
+  rcases extra.ravi_subst 0 x 1 with ⟨a, b, c, h, rfl, h0⟩,
   rw [eq_comm, add_eq_zero_iff_eq_neg] at h; subst h,
   calc g (c + a) = (a - c) * g (a + c) : by rw [sub_eq_add_neg, ← h0, one_mul, add_comm]
   ... = f (-a) - f (- -c) + (f (- -c) - f (-c)) : by rw [← fn_lem2 feq, ← sub_add_sub_cancel]
@@ -107,26 +108,21 @@ begin
 end
 
 lemma fn_lem5 {A B : R} (h : ∀ x : R, g x = A * x + B) (x : R) :
-  (A ^ 2 - A) * x ^ 2 + -(A + 1) * B * x + (A * f 0 + B - f 0) = 0 :=
+  A * (A - 1) * x ^ 2 + -(A + 1) * B * x + (f 0 * (A - 1) + B) = 0 :=
 begin
   have h0 := fn_lem1 feq x,
   rw [h, fn_lem4 feq h x, fn_lem4 feq h (-x), ← sub_eq_zero] at h0,
-  nth_rewrite 2 ← h0; ring
+  nth_rewrite 1 ← h0; ring
 end
 
-lemma fn_lem6 (two_ne_zero : (2 : R) ≠ 0) {A B : R} (h : ∀ x : R, g x = A * x + B) :
-  B = 0 ∧ (A = 1 ∨ (A = 0 ∧ f 0 = 0)) :=
+lemma fn_lem6 {A B : R} (h : ∀ x : R, g x = A * x + B) :
+  B = 0 ∧ A * (A - 1) = 0 ∧ f 0 * (A - 1) = 0 :=
 begin
-  rcases extra.poly_deg_2_zero two_ne_zero (fn_lem5 feq h) with ⟨h0, h1, h2⟩,
-  rw [sq, ← sub_one_mul, mul_eq_zero, sub_eq_zero] at h0,
-  rcases h0 with rfl | rfl,
-  { rw [one_mul, add_comm, add_sub_cancel] at h2,
-    rw and_iff_right h2,
-    left; refl },
-  { rw [zero_add, neg_one_mul, neg_eq_zero] at h1,
-    rw [zero_mul, zero_add, h1, zero_sub, neg_eq_zero] at h2,
-    rw [and_iff_right h1, and_iff_left h2],
-    right; refl }
+  rcases extra.poly_deg_2_zero (fn_lem5 feq h) with ⟨h0, h1, h2⟩,
+  rw [add_eq_zero_iff_neg_eq] at h2,
+  suffices : B = 0,
+    rw [and_iff_right this, and_iff_right h0, ← neg_eq_zero, h2, this],
+  rwa [← h2, neg_mul_neg, mul_left_comm, add_one_mul, h0, zero_add, ← neg_eq_zero, h2] at h1
 end
 
 end results
@@ -134,25 +130,21 @@ end results
 
 
 /-- Final solution -/
-theorem final_solution_general (exists_inv_2 : ∃ t : R, 2 * t = 1) (f g : R → R) :
-  fn_eq f g ↔ (f = 0 ∧ g = 0) ∨ ((∃ C : R, f = λ x, x ^ 2 + C) ∧ g = id) :=
+theorem final_solution_general (f g : R → R) : fn_eq f g ↔ ∃ A C : R,
+  (f = λ x, A * x ^ 2 + C) ∧ (g = λ x, A * x) ∧ A * (A - 1) = 0 ∧ f 0 * (A - 1) = 0 :=
 begin
   split,
   { intros feq,
-    rcases results.fn_lem3 feq exists_inv_2 with ⟨A, B, h⟩,
-    replace exists_inv_2 : (2 : R) ≠ 0,
-    { intros h0,
-      cases exists_inv_2 with t X,
-      rw [h0, zero_mul, eq_comm] at X,
-      exact one_ne_zero X },
-    rcases results.fn_lem6 feq exists_inv_2 h with ⟨rfl, rfl | ⟨rfl, h0⟩⟩,
-    { right; rw and_comm; split,
-      funext; rw [h, add_zero, one_mul, id.def],
-      use f 0; funext; rw [results.fn_lem4 feq h, zero_mul, sub_zero, one_mul] },
-    { left; split; funext,
-      rw [results.fn_lem4 feq h, zero_mul, zero_mul, sub_zero, zero_add, h0, pi.zero_apply],
-      rw [h, zero_mul, add_zero, pi.zero_apply] } },
-  { rintros (⟨rfl, rfl⟩ | ⟨⟨C, rfl⟩, rfl⟩) x y; simp,
+    rcases results.fn_lem3 feq with ⟨A, B, h⟩,
+    rcases results.fn_lem6 feq h with ⟨rfl, h0, h1⟩,
+    use [A, f 0]; rw [and_iff_left h1, and_iff_left h0]; split,
+    funext x; rw [results.fn_lem4 feq h, zero_mul, sub_zero],
+    funext x; rw [h, add_zero] },
+  { rintros ⟨A, C, rfl, rfl, h, h0⟩ x y,
+    simp only [] at h0 ⊢,
+    rw [zero_pow zero_lt_two, mul_zero, zero_add] at h0,
+    rw [mul_sub_one, sub_eq_zero, mul_comm] at h h0,
+    rw [mul_add, h0, ← mul_assoc, h],
     ring }
 end
 
--- src/IMO2011/A3/A3_original.lean
+++ src/IMO2011/A3/A3_original.lean
@@ -11,12 +11,35 @@ Follows from the generalized version since 2 is invertible in ℝ.
 namespace IMOSL
 namespace IMO2011A3
 
+namespace extra
+
+noncomputable instance invertible_two_real :
+  invertible (2 : ℝ) := invertible_of_nonzero two_ne_zero
+
+end extra
+
+
+
 theorem final_solution_original (f g : ℝ → ℝ) :
   fn_eq f g ↔ (f = 0 ∧ g = 0) ∨ ((∃ C : ℝ, f = λ x, x ^ 2 + C) ∧ g = id) :=
 begin
-  apply final_solution_general,
-  use 2⁻¹; rw mul_inv_cancel,
-  exact two_ne_zero
+  rw final_solution_general; split,
+  { rintros ⟨A, C, rfl, rfl, h, h0⟩,
+    simp only [] at h0 ⊢,
+    rw [mul_eq_zero, sub_eq_zero] at h,
+    rcases h with rfl | rfl,
+    rw [zero_mul, zero_add, zero_sub, mul_neg_one, neg_eq_zero] at h0,
+    left; rw h0; split; funext x,
+    rw [zero_mul, zero_add, pi.zero_apply],
+    rw [zero_mul, pi.zero_apply],
+    right; split,
+    use C; simp only [one_mul],
+    funext x; rw [one_mul, id.def] },
+  { rintros (⟨rfl, rfl⟩ | ⟨⟨C, rfl⟩, rfl⟩),
+    use [0, 0]; simp,
+    refl,
+    use [1, C]; simp,
+    refl },
 end
 
 end IMO2011A3
