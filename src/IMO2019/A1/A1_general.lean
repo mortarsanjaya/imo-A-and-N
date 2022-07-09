7a3f051954be5ce1b99f7eebdc8143aadf8d669d
Gian Sanjaya <mortarsanjaya@gmail.com>
2022-07-08 20:11:27 -0400

    Update A1_general.lean
    Replace nested namespace with private definitions.

--- src/IMO2019/A1/A1_general.lean
+++ src/IMO2019/A1/A1_general.lean
@@ -54,23 +54,19 @@ def fn_eq (g : G → G) (T : add_monoid.End G) (f : G → G) :=
 
 
 
-namespace results
-
 /-- Equation (1) -/
-def fn_eq1 (T : add_monoid.End G) (f : G → G) (C : G) :=
+private def fn_eq1 (T : add_monoid.End G) (f : G → G) (C : G) :=
   ∀ x : G, f (f x) = T (f x) + C
 
 /-- Equation (2) -/
-def fn_eq2 (g : G → G) (T : add_monoid.End G) (f : G → G) (C : G) :=
+private def fn_eq2 (g : G → G) (T : add_monoid.End G) (f : G → G) (C : G) :=
   ∀ x : G, f (g x) = T (f x - C) + C
 
 /-- Equation (3) -/
-def fn_eq3 (f : G → G) (C : G) :=
+private def fn_eq3 (f : G → G) (C : G) :=
   ∀ x y : G, f (x + y) = f x + f y - C
 
-
-
-theorem feq_iff_feq123 {g : G → G} (g_zero : g 0 = 0)
+private theorem feq_iff_feq123 {g : G → G} (g_zero : g 0 = 0)
     {T : add_monoid.End G} (T_inj : injective T) (f : G → G) :
   fn_eq g T f ↔ (fn_eq1 T f (f 0) ∧ fn_eq2 g T f (f 0) ∧ fn_eq3 f (f 0)) :=
 begin
@@ -95,7 +91,7 @@ begin
     rw [feq1, feq2, add_right_comm, add_left_inj, ← T.map_add, feq3, add_sub_right_comm] }
 end
 
-theorem feq3_iff_exists_hom_eq_f_sub_C (f : G → G) (C : G) :
+private theorem feq3_iff_exists_hom_eq_f_sub_C (f : G → G) (C : G) :
   fn_eq3 f C ↔ ∃ φ : add_monoid.End G, f = φ + const G C :=
 begin
   split,
@@ -112,14 +108,14 @@ begin
     rw [← add_assoc, add_sub_cancel, add_right_comm, φ.map_add] }
 end
 
-theorem feq2_hom_iff (g : G → G) (T φ : add_monoid.End G) (C : G) :
+private theorem feq2_hom_iff (g : G → G) (T φ : add_monoid.End G) (C : G) :
   fn_eq2 g T (φ + const G C) C ↔ φ ∘ g = ⇑(T * φ) :=
 begin
   rw [add_monoid.coe_mul, funext_iff],
   simp only [fn_eq2, pi.add_apply, const_apply, add_sub_cancel, add_left_inj]
 end
 
-theorem feq1_hom_iff (T φ : add_monoid.End G) (C : G) :
+private theorem feq1_hom_iff (T φ : add_monoid.End G) (C : G) :
   fn_eq1 T (φ + const G C) C ↔ (φ ^ 2 = T * φ ∧ φ C = T C) :=
 begin
   rw [pow_two, add_monoid_hom.ext_iff]; simp [fn_eq1],
@@ -132,8 +128,6 @@ begin
   rw [← add_left_inj (φ C), feq1, h]
 end
 
-end results
-
 
 
 /-- Final solution -/
@@ -142,16 +136,16 @@ theorem final_solution_general {g : G → G} (g_zero : g 0 = 0)
   fn_eq g T f ↔ ∃ (φ : add_monoid.End G) (C : G),
     f = φ + const G C ∧ φ ∘ g = ⇑(T * φ) ∧ φ ^ 2 = T * φ ∧ φ C = T C :=
 begin
-  rw [results.feq_iff_feq123 g_zero T_inj, results.feq3_iff_exists_hom_eq_f_sub_C],
+  rw [feq_iff_feq123 g_zero T_inj, feq3_iff_exists_hom_eq_f_sub_C],
   split,
   { set C := f 0 with C_val,
     rintros ⟨feq1, feq2, φ, h⟩; use [φ, C],
-    rw [h, results.feq1_hom_iff] at feq1,
+    rw [h, feq1_hom_iff] at feq1,
     rw [and_iff_right h, and_iff_left feq1],
-    rwa [h, results.feq2_hom_iff] at feq2 },
+    rwa [h, feq2_hom_iff] at feq2 },
   { rintros ⟨φ, C, rfl, h, h0, h1⟩,
-    rw [pi.add_apply, const_apply, φ.map_zero, zero_add, results.feq1_hom_iff, and_assoc,
-        and_iff_right h0, and_iff_right h1, results.feq2_hom_iff, and_iff_right h],
+    rw [pi.add_apply, const_apply, φ.map_zero, zero_add, feq1_hom_iff, and_assoc,
+        and_iff_right h0, and_iff_right h1, feq2_hom_iff, and_iff_right h],
     use φ }
 end
 
