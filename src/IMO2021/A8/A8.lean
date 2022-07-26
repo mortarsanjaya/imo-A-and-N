be5a5c2f2b28b60acdf5b350fc8d1a2ec4b5a271
Gian Sanjaya <mortarsanjaya@gmail.com>
2022-07-26 08:08:09 +0800

    Update A8.lean
    Remove redundant lemmas

--- src/IMO2021/A8/A8.lean
+++ src/IMO2021/A8/A8.lean
@@ -23,21 +23,6 @@ def fn_eq {R : Type*} [comm_ring R] [is_domain R] (f : ℝ → R) :=
 
 namespace extra
 
-lemma exists_add_eq_mul_eq (x y : ℝ) (h : 4 * y ≤ x ^ 2) : ∃ u v : ℝ, u + v = x ∧ u * v = y :=
-begin
-  use [(x + sqrt (x ^ 2 - 4 * y)) / 2, (x - sqrt (x ^ 2 - 4 * y)) / 2]; split,
-  rw [← add_div, add_add_sub_cancel, ← mul_two, mul_div_cancel x two_ne_zero],
-  rw [div_mul_div_comm, ← sq_sub_sq, sq_sqrt, sub_sub_cancel, bit0, ← two_mul, mul_div_cancel_left],
-  apply mul_ne_zero; exact two_ne_zero,
-  rwa sub_nonneg,
-end
-
-lemma exists_root_quadratic (x y : ℝ) (h : 4 * y ≤ x ^ 2) : ∃ t : ℝ, t ^ 2 + x * t + y = 0 :=
-begin
-  rcases exists_add_eq_mul_eq x y h with ⟨u, v, h, h0⟩,
-  use -u; rw [← h, ← h0, sq, ← add_mul, neg_add_cancel_left, mul_comm, neg_mul, neg_add_self]
-end
-
 lemma g_sum_zero_of_g_cube_sum_zero {R S} [add_comm_monoid R]
     [comm_ring S] [is_domain S] (char_ne_3 : ring_char S ≠ 3)
     {g : R → S} (h : ∀ x y z : R, x + y + z = 0 → (g x + g y) ^ 3 + g z ^ 3 = 0) :
