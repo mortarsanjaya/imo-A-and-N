7cbfa18982fbe3a4ad032153cae32763b324ad7c
Gian Sanjaya <mortarsanjaya@gmail.com>
2022-07-28 20:42:05 +0800

    2010 A1: Generalize problem statement

--- src/IMO2010/A1/A1.lean
+++ src/IMO2010/A1/A1.lean
@@ -1,4 +1,4 @@
-import data.real.basic
+import algebra.order.field algebra.order.floor
 
 /-! # IMO 2010 A1 (P1) -/
 
@@ -8,14 +8,17 @@ open_locale classical
 namespace IMOSL
 namespace IMO2010A1
 
-def fn_eq (f : ℝ → ℝ) := ∀ x y : ℝ, f (⌊x⌋ * y) = f x * ⌊f y⌋
+variables {F : Type*} [linear_ordered_field F] [floor_ring F]
+  {R : Type*} [linear_ordered_ring R] [floor_ring R]
+
+def fn_eq (f : F → R) := ∀ x y : F, f (⌊x⌋ * y) = f x * ⌊f y⌋
 
 
 
 namespace extra
 
-/-- For any r : ℝ with 1 < r, we have ⌊r⁻¹⌋ = 0. -/
-lemma inv_floor_eq_zero {r : ℝ} (h : 1 < r) : ⌊r⁻¹⌋ = 0 :=
+/-- For any r : F with 1 < r, we have ⌊r⁻¹⌋ = 0. -/
+lemma inv_floor_eq_zero {r : F} (h : 1 < r) : ⌊r⁻¹⌋ = 0 :=
 begin
   rw [floor_eq_iff, cast_zero, zero_add, inv_nonneg],
   exact ⟨le_of_lt (lt_trans zero_lt_one h), inv_lt_one h⟩
@@ -26,7 +29,7 @@ end extra
 
 
 /-- Final solution -/
-theorem final_solution (f : ℝ → ℝ) : fn_eq f ↔ f = 0 ∨ ∃ C : ℝ, ⌊C⌋ = 1 ∧ f = const ℝ C :=
+theorem final_solution (f : F → R) : fn_eq f ↔ f = 0 ∨ ∃ C : R, ⌊C⌋ = 1 ∧ f = const F C :=
 begin
   split,
   { intros feq,
@@ -42,12 +45,15 @@ begin
       { funext x,
         have h0 := feq 1 x,
         rwa [this, zero_mul, floor_one, cast_one, one_mul] at h0 },
-        have h0 := feq 2⁻¹ 2⁻¹,
-        rw [extra.inv_floor_eq_zero one_lt_two, cast_zero, zero_mul, h, zero_eq_mul] at h0,
-        replace h0 : (⌊f 2⁻¹⌋ : ℝ) = 0 :=
-          (or_iff_right_of_imp (by intros X; rw [X, floor_zero, cast_zero])).mp h0,
+        have h0 : ⌊f 2⁻¹⌋ = 0 :=
+        begin
+          have h0 := feq 2⁻¹ 2⁻¹,
+          rwa [extra.inv_floor_eq_zero (one_lt_two : (1 : F) < 2), cast_zero, zero_mul, h,
+               zero_eq_mul, or_iff_right_of_imp, cast_eq_zero] at h0,
+          intros h1; rw [h1, floor_zero, cast_zero]
+        end,
         have h1 := feq ↑(2 : ℤ) 2⁻¹,
-        rwa [floor_coe, cast_bit0, cast_one, h0, mul_zero, mul_inv_cancel] at h1,
+        rwa [floor_coe, cast_bit0, cast_one, h0, cast_zero, mul_zero, mul_inv_cancel] at h1,
         exact two_ne_zero } },
   { rintros (rfl | ⟨C, h, rfl⟩) x y,
     rw [pi.zero_apply, pi.zero_apply, zero_mul],
