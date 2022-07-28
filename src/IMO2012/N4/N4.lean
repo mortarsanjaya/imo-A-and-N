f12df43e1867e35610fbe6c6d2130dc3a9877295
Gian Sanjaya <mortarsanjaya@gmail.com>
2022-07-28 11:39:51 +0800

    2012 N4: Use ring tactic + Some changes

--- src/IMO2012/N4/N4.lean
+++ src/IMO2012/N4/N4.lean
@@ -1,4 +1,4 @@
-import data.int.basic data.set.finite tactic.norm_fin
+import data.int.basic data.set.finite tactic.ring
 
 /-! # IMO 2012 N4 -/
 
@@ -17,11 +17,10 @@ begin
   use [2 * k + 1, k],
   rw and_iff_right h; split,
   exact add_pos (mul_pos two_pos h) one_pos,
-  rw [add_sq, mul_pow, mul_one, ← mul_assoc, sq, ← mul_add, one_pow, add_assoc, sq, ← mul_add_one,
-      add_comm (1 : ℤ), ← mul_assoc, ← add_one_mul, mul_two, ← bit0, two_mul, add_assoc,
-      ← add_assoc, ← mul_add_one, ← add_one_mul, add_sub_cancel', ← sq, mul_assoc, ← pow_succ]
+  ring
 end
 
+/- Final solution for part 1 -/
 theorem final_solution_part1 : 500 ≤ fintype.card {a : fin 2012 // friendly (a + 1)} :=
 begin
   let f : fin 500 → {a : fin 2012 // friendly (a + 1)} := λ (n : fin 500),
@@ -40,7 +39,24 @@ end
 
 
 
-
+/- Final solution for part 2 -/
+theorem final_solution_part2 : ¬friendly 2 :=
+begin
+  rintros ⟨m, n, hm, hn, h⟩,
+  have h0 : 0 < m - n :=
+  begin
+    rw lt_iff_not_le; intros h0,
+    sorry
+  end,
+  replace h : (m ^ 2 + n + n ^ 2 + m) ^ 2 = (m - n) ^ 2 * ((m + n - 1) ^ 2 + 8 * (m - n)) :=
+  begin
+    change (8 : ℤ) with 4 + 4,
+    rw [mul_add, mul_comm _ (_ * (m - n)), mul_assoc,
+        ← pow_succ, ← mul_two, mul_assoc, ← bit1, ← h],
+    ring
+  end,
+  sorry
+end
 
 
 
