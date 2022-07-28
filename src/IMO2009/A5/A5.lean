ccd73fc5f73546e2136a4a721373612ca3d0ef56
Gian Sanjaya <mortarsanjaya@gmail.com>
2022-07-28 14:20:20 +0800

    2009 A5: Final golf

--- src/IMO2009/A5/A5.lean
+++ src/IMO2009/A5/A5.lean
@@ -22,29 +22,22 @@ begin
   rwa [add_sub_cancel, zero_mul, zero_add] at fbad
 end
 
-private lemma lem2 {y : F} (h : 0 < y) : -1 ≤ f (f y) :=
-begin
-  replace fbad := le_trans (fbad (f y) y) (add_le_add_left (lem1 fbad y) _),
-  rwa [sub_self, ← add_assoc, le_add_iff_nonneg_left, ← mul_add_one,
-       zero_le_mul_left h, ← neg_le_iff_add_nonneg] at fbad
-end
-
-private lemma lem3 {y : F} (h : 0 < y) : - f 0 - 1 ≤ f y :=
-begin
-  replace fbad := le_trans (lem2 fbad h) (lem1 fbad (f y)),
-  rwa [sub_eq_add_neg, add_comm, add_neg_le_iff_le_add]
-end
-
-private lemma lem4 (x : F) : f x ≤ 0 :=
+private lemma lem2 (x : F) : f x ≤ 0 :=
 begin
   rw ← not_lt; intros h,
   cases exists_lt (min (x - f 0) ((- f 0 - x - 1) / f x)) with y h0,
   rw lt_min_iff at h0; cases h0 with h0 h1,
+  rw [← not_le, div_le_iff h, sub_right_comm, sub_le_iff_le_add] at h1,
+  apply h1; clear h1,
+  refine le_trans _ (fbad x y),
+  rw [sub_eq_add_neg, add_comm, add_neg_le_iff_le_add],
+  refine le_trans _ (lem1 fbad _),
   rw [← sub_pos, sub_sub, add_comm] at h0,
   replace h0 := lt_of_lt_of_le h0 ((sub_le_sub_iff_left x).mpr (lem1 fbad y)),
-  replace h0 := le_trans (lem3 fbad h0) (fbad x y),
-  rw [← sub_le_iff_le_add, sub_right_comm, ← div_le_iff h, ← not_lt] at h0,
-  exact h0 h1
+  set z := x - f y,
+  replace fbad := le_trans (fbad (f z) z) (add_le_add_left (lem1 fbad z) _),
+  rwa [sub_self, ← add_assoc, le_add_iff_nonneg_left, ← mul_add_one,
+       zero_le_mul_left h0, ← neg_le_iff_add_nonneg] at fbad
 end
 
 end results
@@ -60,12 +53,12 @@ begin
   rw [sub_lt_iff_lt_add, neg_lt, ← not_le, ← zero_sub (y + 1)] at h0,
   apply h0; clear h0,
   let x := f y - 1,
-  have h0 := le_trans (lem1 fbad x) (add_le_of_nonpos_right (lem4 fbad 0)),
+  have h0 := le_trans (lem1 fbad x) (add_le_of_nonpos_right (lem2 fbad 0)),
   calc f (-1) = f (x - f y) : by rw [sub_sub_cancel_left]
   ... ≤ y * f x + x : fbad x y
   ... ≤ y * (f y - 1) + x : add_le_add_right ((mul_le_mul_left h).mpr h0) _
   ... = (y + 1) * f y - (y + 1) : by rw [mul_sub_one, sub_add_sub_comm, add_one_mul]
-  ... ≤ 0 - (y + 1) : sub_le_sub_right (mul_nonpos_of_nonneg_of_nonpos _ (lem4 fbad _)) _,
+  ... ≤ 0 - (y + 1) : sub_le_sub_right (mul_nonpos_of_nonneg_of_nonpos _ (lem2 fbad _)) _,
   exact le_of_lt (add_pos h one_pos)
 end
 
