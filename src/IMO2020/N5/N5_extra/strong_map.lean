8800fa3f035c21108c7d76629abf33f7bcb4ca2b
Gian Sanjaya <mortarsanjaya@gmail.com>
2022-08-11 10:26:14 +0800

    2020 N5: Update `strong_map.lean`

--- src/IMO2020/N5/N5_extra/strong_map.lean
+++ src/IMO2020/N5/N5_extra/strong_map.lean
@@ -1,4 +1,4 @@
-import IMO2020.N5.N5_extra.good_map data.nat.log data.nat.modeq
+import IMO2020.N5.N5_extra.good_map data.nat.log data.nat.modeq data.zmod.basic
 
 /-!
 # `p`-strong maps
@@ -7,8 +7,21 @@ We characterize `p`-strong maps.
 As it turns out, they are precisely the functions that can be described as
   `p^k t ↦ kc + χ(t mod p)` for some `c : M` and `χ : (zmod p)ˣ → M` where
   `χ` is a map with `χ(ab) = χ(a) χ(b)` for any `a b : (zmod p)ˣ` and `χ(-1) = 1`.
-Alternatively, using a bijection `(zmod (p / 2)) → additive (zmod p)ˣ/⟨-1⟩`,
-  we can replace `χ` with a map `(zmod (p / 2)) →+ M`.
+Alternatively, using a bijection `(zmod (p / 2)) → additive (zmod p)ˣ/⟨-1⟩`
+  for odd `p`, we can replace `χ` with a map `(zmod (p / 2)) →+ M`.
+
+TODO: Do the constructions!
+1. Construct the "natural" `p`-strong map `M →+ additive_map M`.
+We will call it the `p`-type map.
+[DONE]
+
+2. Construct the homomorphism `(zmod (p / 2)) → additive (zmod p)ˣ/⟨-1⟩` for `p` odd.
+3. Construct the `p`-strong map `((zmod (p / 2)) →+ M) →+ additive_map M`.
+We will call it the `coprime`-type map.
+4. Construct the `p`-strong map `(M × ((zmod (p / 2) →+ M)) →+ additive_map M`.
+The map is obtained by combining a `p`-type and `coprime`-type map.
+4. Construct the type `strong_map M p` of `p`-strong elements of `additive_map M`.
+5. Construct the bijection between `strong_map M p` and `M × (zmod (p / 2)`.
 -/
 
 namespace IMOSL
@@ -19,9 +32,95 @@ open_locale classical
 
 
 
-variables {M : Type*} [add_cancel_comm_monoid M] (f : additive_map M)
+section constructions
 
-private lemma pstrong_mod_p_induct {p : ℕ} (hp : p.prime) {a : ℕ} (h : good (p ^ a) f)
+
+/-
+section coprime_part_zmod
+
+variables {p : ℕ} (hp : p.prime)
+include hp
+
+private def coprime_part_zmod {n : ℕ} (h : n ≠ 0) : (zmod p)ˣ :=
+  zmod.unit_of_coprime (n / p ^ (n.factorization p))
+  (by rw [nat.coprime_comm, nat.prime.coprime_iff_not_dvd hp];
+    exact nat.not_dvd_div_pow_factorization hp h)
+
+private lemma coprime_part_zmod_mul {a : ℕ} (h : a ≠ 0) {b : ℕ} (h0 : b ≠ 0) :
+  coprime_part_zmod hp (mul_ne_zero h h0) = coprime_part_zmod hp h * coprime_part_zmod hp h0 :=
+begin
+  unfold coprime_part_zmod,
+  
+end
+
+end coprime_part_zmod
+-/
+
+
+
+variables {M : Type*} [add_cancel_comm_monoid M] (p : ℕ) [fact (p.prime)]
+
+/-- The pure `p`-type additive map -/
+def pstrong_pure_p_fun (c : M) : additive_map M :=
+{ to_fun := λ n, padic_val_nat p n • c,
+  map_zero' := by rw [padic_val_nat.zero, zero_smul],
+  map_mul_add' := λ x y hx hy, by rw [padic_val_nat.mul p (ne_of_gt hx) (ne_of_gt hy), add_smul] }
+
+@[simp] theorem pstrong_pure_p_zero : pstrong_pure_p_fun p (0 : M) = 0 :=
+  by ext; rw ← to_fun_eq_coe; simp only [pstrong_pure_p_fun, smul_zero, zero_apply]
+
+@[simp] theorem pstrong_pure_p_add (a b : M) :
+  pstrong_pure_p_fun p (a + b) = pstrong_pure_p_fun p a + pstrong_pure_p_fun p b :=
+  by ext; rw add_apply; repeat { rw ← to_fun_eq_coe }; simp only [pstrong_pure_p_fun, smul_add]
+
+theorem pstrong_pure_p_pstrong (c : M) : strong (fact.out p.prime) (pstrong_pure_p_fun p c) :=
+begin
+  intros k a b ha hb h,
+  rw ← to_fun_eq_coe; dsimp only [pstrong_pure_p_fun],
+  congr' 1; clear c,
+  sorry
+end
+
+/-- The above map as `M →+ additive_map M` -/
+def pstrong_pure_p_hom : M →+ additive_map M :=
+{ to_fun := pstrong_pure_p_fun p,
+  map_zero' := pstrong_pure_p_zero p,
+  map_add' := pstrong_pure_p_add p }
+
+
+
+
+/-
+/-- The pure `coprime`-part, coming from `χ : additive (zmod p)ˣ →+ M` with `χ(-1) = 0` -/
+def pstrong_pure_coprime_mul (χ : additive (zmod p)ˣ →+ M) (h : χ (additive.of_mul (-1)) = 0) :
+  additive_map M :=
+{ to_fun := λ n, dite (n = 0) (λ _, 0)
+    (λ hn0 : n ≠ 0, χ (additive.of_mul (coprime_part_zmod p hn0))),
+  map_zero' := by rw dif_pos; refl,
+  map_mul_add' := λ x y hx hy, begin
+    rw [dif_neg (ne_of_gt (mul_pos hx hy)), dif_neg (ne_of_gt hx), dif_neg (ne_of_gt hy)],
+    
+  end
+}
+-/
+
+/-
+def pstrong_pure_coprime (χ : zmod (p / 2) →+ M) : additive_map M :=
+{ to_fun := λ n, dite (n = 0) (λ _, 0) (λ h : n ≠ 0, )
+
+}
+-/
+
+end constructions
+
+
+
+section results
+
+variables {M : Type*} [add_cancel_comm_monoid M] {p : ℕ} (hp : p.prime) (f : additive_map M)
+include hp
+
+private lemma pstrong_mod_p_induct {a : ℕ} (h : good (p ^ a) f)
   {k : ℕ} (hkp : ¬(p : ℕ) ∣ k) (h0 : k < p ^ a) : f k = f (k % p) :=
 begin
   rcases a.eq_zero_or_pos with rfl | h1,
@@ -73,13 +172,10 @@ begin
   exact nat.le_mod_add_mod_of_dvd_add_of_not_dvd h3 h6
 end
 
+theorem pstrong_mod_p (h : strong hp f) {k : ℕ} (hkp : ¬(p : ℕ) ∣ k) : f k = f (k % p) :=
+  pstrong_mod_p_induct hp f (h k) hkp (nat.lt_pow_self hp.one_lt k)
 
-
-variables {p : ℕ} (hp : p.prime) (h : strong hp f)
-include hp h
-
-theorem pstrong_mod_p {k : ℕ} (hkp : ¬(p : ℕ) ∣ k) : f k = f (k % p) :=
-  pstrong_mod_p_induct f hp (h k) hkp (nat.lt_pow_self hp.one_lt k)
+end results
 
 
 
