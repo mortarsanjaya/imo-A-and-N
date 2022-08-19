95a24aac8e69f7810aa154ecb5ffbbd68a158ec2
Gian Sanjaya <mortarsanjaya@gmail.com>
2022-08-20 01:43:23 +0800

    2012 N3: Golf first lemma

--- src/IMO2012/N3/N3.lean
+++ src/IMO2012/N3/N3.lean
@@ -1,4 +1,4 @@
-import data.nat.prime data.nat.choose.basic data.nat.parity
+import data.nat.choose.basic data.nat.parity
 
 /-! # IMO 2012 N3 -/
 
@@ -11,34 +11,28 @@ def good (m : ℕ) := 2 ≤ m ∧ ∀ n : ℕ, 2 * n ≤ m → m ≤ 3 * n → n
 
 private lemma prime_implies_good (p : ℕ) (h : p.prime) : good p :=
 begin
-  split,
-  exact nat.prime.two_le h,
-  rintros n h0 h1,
-  have hn : 0 < n := pos_of_mul_pos_right (lt_of_lt_of_le h.pos h1) zero_le_three,
+  refine ⟨nat.prime.two_le h, λ n h0 h1, _⟩,
+  replace h1 : 0 < n := pos_of_mul_pos_right (lt_of_lt_of_le h.pos h1) zero_le_three,
   rcases h.eq_two_or_odd with rfl | h2,
-  { replace hn := nat.mul_le_mul_left 2 hn,
-    rw mul_one at hn,
-    replace hn := le_antisymm h0 hn,
-    rw nat.mul_right_eq_self_iff two_pos at hn,
-    rw hn; exact one_dvd _ },
+  { replace h1 := nat.mul_le_mul_left 2 h1,
+    rw mul_one at h1,
+    replace h1 := le_antisymm h0 h1,
+    rw nat.mul_right_eq_self_iff two_pos at h1,
+    rw h1; exact one_dvd _ },
   apply @nat.coprime.dvd_of_dvd_mul_right _ (p - 2 * n),
   { rw [nat.coprime, nat.gcd_rec, ← nat.add_mul_mod_self_right _ 2,
         nat.sub_add_cancel h0, ← nat.gcd_rec, nat.gcd_comm],
-    exact nat.coprime_of_lt_prime hn (lt_of_lt_of_le (lt_two_mul_self hn) h0) h },
-  { obtain ⟨k, h3⟩ : ∃ k : ℕ, p - 2 * n = k + 1 :=
-    begin
-      use (p - 2 * n).pred,
-      rw [← nat.succ_eq_add_one, nat.succ_pred_eq_of_pos],
-      apply nat.pos_of_ne_zero; intros h3,
-      replace h3 := le_antisymm (nat.sub_eq_zero_iff_le.mp h3) h0,
-      rw [h3, nat.mul_mod_right] at h2,
-      exfalso; exact zero_ne_one h2
-    end,
-    rw [h3, nat.choose_succ_right_eq],
+    exact nat.coprime_of_lt_prime h1 (lt_of_lt_of_le (lt_two_mul_self h1) h0) h },
+  { rw le_iff_exists_add at h0,
+    rcases h0 with ⟨k, rfl⟩,
+    rcases k with rfl | k,
+    rw [add_zero, nat.mul_mod_right] at h2,
+    exfalso; exact zero_ne_one h2,
+    rw [nat.add_sub_cancel_left, nat.choose_succ_right_eq],
     cases n,
-    exfalso; exact lt_irrefl 0 hn,
+    exfalso; exact lt_irrefl 0 h1,
     rw [nat.succ_eq_add_one, ← nat.choose_mul_succ_eq],
-    use (n.choose k); rw mul_comm }
+    exact dvd_mul_left (n + 1) (nat.choose n k) }
 end
 
 private lemma good_implies_prime (m : ℕ) (h : good m) : nat.prime m :=
