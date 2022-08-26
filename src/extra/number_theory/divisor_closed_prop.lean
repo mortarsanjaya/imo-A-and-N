de42d5009bbae4f0e3c0fb31d621a5a63a2b660f
Gian Sanjaya <mortarsanjaya@gmail.com>
2022-08-26 11:59:11 -0400

    extra: Finalize `divisor_closed _prop.lean`

--- src/extra/number_theory/divisor_closed_prop.lean
+++ src/extra/number_theory/divisor_closed_prop.lean
@@ -56,94 +56,99 @@ end
 
 /-- Using choice, given a `Prop` map `P` that is not `p`-strong for any `p` prime, construct a
   map `x : nat.primes → ℕ` such that `P(p^k) → k < x_p` for any `k : ℕ` and `p` prime. -/
-theorem dc_not_strong_aoc_map_iff (h0 : ∀ p : nat.primes, ¬strong p P) :
-  ∃ x : nat.primes → ℕ, ∀ (p : nat.primes) (k : ℕ), P (p ^ k) ↔ k < x p :=
+theorem dc_not_strong_aoc_map_iff (h0 : ∀ p : ℕ, p.prime → ¬strong p P) :
+  ∃ x : ℕ → ℕ, (∀ p : ℕ, x p ≠ 0 → p.prime) ∧
+    ∀ (p : ℕ) (hp : p.prime) (k : ℕ), P (p ^ k) ↔ k < x p :=
 begin
   have h1 : ∀ p : nat.primes, ∃ kp : ℕ, (∀ k : ℕ, P (p ^ k) ↔ k < kp) :=
-    λ ⟨p, hp⟩, by rw [subtype.coe_mk, ← dc_not_strong_iff h hp.ne_zero]; exact h0 ⟨p, hp⟩,
-  exact classical.axiom_of_choice h1
+    λ ⟨p, hp⟩, by rw [subtype.coe_mk, ← dc_not_strong_iff h hp.ne_zero]; exact h0 p hp,
+  replace h1 := classical.axiom_of_choice h1,
+  cases h1 with x h1,
+  refine ⟨(λ p, dite p.prime (λ hp : p.prime, x ⟨p, hp⟩) (λ _, 0)), (λ p h2, _), (λ p hp k, _)⟩,
+  contrapose! h2; rw dif_neg h2,
+  rw [dif_pos hp, ← h1, subtype.coe_mk]
 end
 
 /-- Using choice, given a `Prop` map `P` that is not `p`-strong for any `p` prime, construct a
   map `x : nat.primes → ℕ` such that `P(p^k) → k ≤ x_p` for any `k : ℕ` and `p` prime.
 This is just a weak version of `dc_not_strong_aoc_map_iff`. -/
-theorem dc_not_strong_aoc_map (h0 : ∀ p : nat.primes, ¬strong p P) :
-  ∃ x : nat.primes → ℕ, ∀ (p : nat.primes) (k : ℕ), P (p ^ k) → k ≤ x p :=
+theorem dc_not_strong_aoc_map (h0 : ∀ p : ℕ, p.prime → ¬strong p P) :
+  ∃ x : ℕ → ℕ, (∀ p : ℕ, x p ≠ 0 → p.prime) ∧
+    ∀ (p : ℕ) (hp : p.prime) (k : ℕ), P (p ^ k) → k ≤ x p :=
 begin
-  cases dc_not_strong_aoc_map_iff h h0 with x h1,
-  exact ⟨x, λ p k h2, le_of_lt ((h1 p k).mp h2)⟩
+  rcases dc_not_strong_aoc_map_iff h h0 with ⟨x, h1, h2⟩,
+  exact ⟨x, h1, λ p hp k h3, le_of_lt ((h2 p hp k).mp h3)⟩
 end
 
 /-- Using choice, given a `Prop` map `P` that is not `p`-strong for any `p` prime, under the
   condition `P(1)`, construct a map `x : nat.primes → ℕ` such that `P(p^k) ↔ k ≤ x_p` for any
   `k : ℕ` and `p` prime. -/
-theorem dc_not_strong_aoc_map_iff' (h0 : ∀ p : nat.primes, ¬strong p P) (h1 : P 1) :
-  ∃ x : nat.primes → ℕ, ∀ (p : nat.primes) (k : ℕ), P (p ^ k) ↔ k ≤ x p :=
+theorem dc_not_strong_aoc_map_iff' (h0 : ∀ p : ℕ, p.prime → ¬strong p P) (h1 : P 1) :
+  ∃ x : ℕ → ℕ, (∀ p : ℕ, x p ≠ 0 → p.prime) ∧
+    ∀ (p : ℕ) (hp : p.prime) (k : ℕ), P (p ^ k) ↔ k ≤ x p :=
 begin
-  cases dc_not_strong_aoc_map_iff h h0 with x h1,
-  refine ⟨λ p, x p - 1, λ p k, _⟩,
-  rw [h1, ← nat.lt_succ_iff, nat.succ_eq_add_one, nat.sub_add_cancel],
-  rwa [nat.succ_le_iff, ← h1, pow_zero]
+  rcases dc_not_strong_aoc_map_iff h h0 with ⟨x, h2, h3⟩,
+  refine ⟨λ p, x p - 1, λ p h4, h2 p (λ h5, _), λ p hp k, _⟩,
+  rw [h5, zero_tsub] at h4; exact h4 rfl,
+  rw [h3 p hp, ← nat.lt_succ_iff, nat.succ_eq_add_one, nat.sub_add_cancel],
+  rwa [nat.succ_le_iff, ← h3 p hp, pow_zero]
 end
 
 /-- Using choice, given a `Prop` map `P` that is neither wide nor `p`-strong for any `p` prime,
   under the condition `P(1)`, construct a finitely-supported map `x : nat.primes → ℕ` such that
   `P(p^k) ↔ k ≤ x_p` for any `k : ℕ` and `p` prime. -/
 theorem dc_not_wide_strong_aoc_finsupp
-    (h0 : ¬wide P) (h1 : ∀ p : nat.primes, ¬strong p P) (h2 : P 1) :
-  ∃ x : nat.primes →₀ ℕ, ∀ (p : nat.primes) (k : ℕ), P (p ^ k) ↔ k ≤ x p :=
+    (h0 : ¬wide P) (h1 : ∀ p : ℕ, p.prime → ¬strong p P) (h2 : P 1) :
+  ∃ x : ℕ →₀ ℕ, (∀ p : ℕ, x p ≠ 0 → p.prime) ∧
+    ∀ (p : ℕ) (hp : p.prime) (k : ℕ), P (p ^ k) ↔ k ≤ x p :=
 begin
   simp only [wide, set.not_infinite] at h0,
-  replace h1 := dc_not_strong_aoc_map_iff' h h1 h2,
-  cases h1 with x h1,
-  suffices : {p : nat.primes | x p ≠ 0}.finite,
-  { refine ⟨⟨this.to_finset, x, λ p, _⟩, h1⟩,
-    rw [set.finite.mem_to_finset, set.mem_set_of_eq] },
-  contrapose h0,
-  refine set.infinite_of_inj_on_maps_to (set.inj_on_of_injective nat.primes.coe_nat_inj _) _ h0,
-  intros p h3; simp only [set.mem_set_of_eq] at h3 ⊢; split,
-  cases p with p hp; rwa subtype.coe_mk,
-  rwa [← pow_one (p : ℕ), h1, nat.succ_le_iff, zero_lt_iff]
+  replace h2 := dc_not_strong_aoc_map_iff' h h1 h2,
+  clear h h1; rcases h2 with ⟨x, h, h1⟩,
+  refine ⟨⟨h0.to_finset, x, (λ p, _)⟩, ⟨h, h1⟩⟩,
+  rw [set.finite.mem_to_finset, set.mem_set_of_eq, iff.comm],
+  refine ⟨λ h2, ⟨h p h2, _⟩, λ h2, _⟩,
+  rwa [← pow_one p, h1 p (h p h2), nat.succ_le_iff, zero_lt_iff],
+  rw [← zero_lt_iff, ← nat.succ_le_iff, ← h1 p h2.1, pow_one],
+  exact h2.2
 end
 
 /-- Assuming choice, `P(n)` holds for infinitely many `n` iff
   `P` is either wide or `p`-strong for some `p` prime. -/
 theorem dc_infinite_iff_wide_or_strong :
-  {p : ℕ | P p}.infinite ↔ wide P ∨ ∃ p : nat.primes, strong p P :=
+  (set_of P).infinite ↔ wide P ∨ ∃ p : ℕ, p.prime ∧ strong p P :=
 begin
-  refine ⟨(λ h0, _), _⟩,
+  refine iff.symm ⟨_, λ h0, _⟩,
+  { rintros (h0 | ⟨p, hp, h0⟩),
+    refine set.infinite.mono (λ x h1, _) h0; exact h1.2,
+    exact set.infinite_of_injective_forall_mem (nat.pow_right_injective hp.two_le) h0 },
+  by_cases h1 : ¬P 1,
+  { rcases set.infinite.exists_nat_lt h0 0 with ⟨n, h2, h3⟩,
+    exfalso; exact h1 (dc_at_one h (ne_of_gt h3) h2) },
   { contrapose! h0,
-    rw set.not_infinite,
-    sorry
-    /-
-    replace h0 := dc_not_wide_strong_aoc_finsupp h h0.1 h0.2,
-    rcases h with ⟨x, h, h0⟩,
-    suffices : ∃ N : ℕ, 0 < N ∧ ∀ n : ℕ, good n f → n = 0 ∨ n ∣ N,
+    rw not_not at h1,
+    replace h0 := dc_not_wide_strong_aoc_finsupp h h0.1 h0.2 h1,
+    clear h1; rcases h0 with ⟨x, h0, h1⟩,
+    suffices : ∃ N : ℕ, 0 < N ∧ ∀ n : ℕ, P n → n = 0 ∨ n ∣ N,
     { rcases this with ⟨N, hN, this⟩,
-      refine set.finite.subset (finset.range N.succ).finite_to_set (λ n h0, _),
+      refine not_not.mpr (set.finite.subset (finset.range N.succ).finite_to_set (λ n h2, _)),
       rw [finset.mem_coe, finset.mem_range, nat.lt_succ_iff],
-      rcases this n h0 with rfl | h0,
-      exacts [N.zero_le, nat.le_of_dvd hN h0] },
+      rcases this n h2 with rfl | h2,
+      exacts [N.zero_le, nat.le_of_dvd hN h2] },
     use x.prod has_pow.pow,
     rw and_iff_left_of_imp,
-    { refine nat.prod_pow_pos_of_zero_not_mem_support (λ h1, _),
-      rw finsupp.mem_support_iff at h1,
-      exact nat.not_prime_zero (h 0 h1) },
-    { intros h1 n h2,
-      rcases eq_or_ne n 0 with rfl | h3,
+    { refine nat.prod_pow_pos_of_zero_not_mem_support (λ h2, _),
+      rw finsupp.mem_support_iff at h2,
+      exact nat.not_prime_zero (h0 0 h2) },
+    { intros h2 n h3,
+      rcases eq_or_ne n 0 with rfl | h4,
       left; refl,
-      rw [or_iff_right h3, ← nat.factorization_le_iff_dvd h3 (ne_of_gt h1),
-          nat.prod_pow_factorization_eq_self (by simp; exact h), finsupp.le_iff],
-      rintros p h4,
-      replace h4 : p.prime := nat.prime_of_mem_factorization h4,
-      rw [nat.factorization_def n h4, ← h0 p h4],
-      exact good_factor f h3 h2 pow_padic_val_nat_dvd } -/ },
-  /-
-  { rintros (h | ⟨p, hp, h⟩),
-    refine set.infinite.mono (λ x h0, _) h; exact h0.2,
-    exact set.infinite_of_injective_forall_mem (nat.pow_right_injective hp.two_le) h }
-  -/
-  sorry
+      rw [or_iff_right h4, ← nat.factorization_le_iff_dvd h4 (ne_of_gt h2),
+          nat.prod_pow_factorization_eq_self (by simp; exact h0), finsupp.le_iff],
+      rintros p h5,
+      replace h5 : p.prime := nat.prime_of_mem_factorization h5,
+      rw [nat.factorization_def n h5, ← h1 p h5],
+      exact h n h4 h3 _ pow_padic_val_nat_dvd } }
 end
 
 end results
