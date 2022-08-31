facbe74c5ca2a76eaa1e8f2b7aaf94bcd1b40671
Gian Sanjaya <mortarsanjaya@gmail.com>
2022-08-31 16:46:31 -0400

    2011 A6: Golf and generalize

--- src/IMO2011/A6/A6.lean
+++ src/IMO2011/A6/A6.lean
@@ -5,59 +5,47 @@ import algebra.order.field tactic.linarith
 namespace IMOSL
 namespace IMO2011A6
 
-variables {F : Type*} [linear_ordered_field F]
+variables {F : Type*} [linear_ordered_comm_ring F]
 
 def fn_ineq (f : F → F) := ∀ x y : F, f (x + y) ≤ y * f x + f (f x)
 
 
 
-section results
-
 variables {f : F → F} (fineq : fn_ineq f)
 include fineq
 
-private lemma fn_lem1 (t x : F) : f t ≤ t * f x - x * f x + f (f x) :=
+private lemma lem1 (x : F) : x * f x ≤ 0 :=
 begin
+  revert x; suffices : ∀ t x : F, f t ≤ (t - x) * f x + f (f x),
+    intros x; linarith [this (f (2 * f x)) x, this (f x) (2 * f x)],
+  intros t x,
   replace fineq := fineq x (t - x),
-  rwa [add_sub_cancel'_right, sub_mul] at fineq
+  rwa add_sub_cancel'_right at fineq
 end
 
-private lemma fn_lem2 (x : F) (h : x < 0) : 0 ≤ f x :=
+private lemma lem2 (x : F) : f x ≤ 0 :=
 begin
-  refine nonneg_of_mul_nonpos_right _ h,
-  linarith [(fn_lem1 fineq (f (2 * f x)) x), (fn_lem1 fineq (f x) (2 * f x))]
+  have h := fineq x 0,
+  rw [add_zero, zero_mul, zero_add] at h,
+  rw ← not_lt; intros h0,
+  refine lt_irrefl 0 (lt_of_lt_of_le h0 (le_trans h _)),
+  exact nonpos_of_mul_nonpos_right (lem1 fineq (f x)) h0
 end
-
-private lemma fn_lem3 (x : F) : f x ≤ 0 :=
-begin
-  rw ← not_lt; intros h,
-  let M := x - f (f x) / f x,
-  have h0 : ∀ t : F, t < M → f t < 0,
-  { intros t h0,
-    apply lt_of_le_of_lt (fn_lem1 fineq t x),
-    rwa [← sub_mul, ← lt_neg_iff_add_neg, ← lt_div_iff h,
-         sub_lt_iff_lt_add', neg_div, ← sub_eq_add_neg] },
-  cases exists_lt (min M 0) with C h1,
-  rw lt_min_iff at h1,
-  cases h1 with h1 h2,
-  exact not_lt_of_le (fn_lem2 fineq C h2) (h0 C h1)
-end
-
-end results
 
 
 
 /-- Final solution -/
-theorem final_solution {f : F → F} (fineq : fn_ineq f) (x : F) (h : x ≤ 0) : f x = 0 :=
+theorem final_solution (x : F) (h : x ≤ 0) : f x = 0 :=
 begin
-  apply le_antisymm (fn_lem3 fineq x),
+  have h0 : ∀ y : F, y < 0 → f y = 0 :=
+    λ y h, le_antisymm (lem2 fineq y) (nonneg_of_mul_nonpos_right (lem1 fineq y) h),
   rw le_iff_lt_or_eq at h,
   rcases h with h | rfl,
-  exact fn_lem2 fineq x h,
+  exact h0 x h,
   cases exists_lt (0 : F) with x h,
-  replace h := le_antisymm (fn_lem2 fineq x h) (fn_lem3 fineq x),
-  have h0 := fineq x 0,
-  rwa [zero_mul, zero_add, add_zero, ← h] at h0
+  have h1 := fineq x 0,
+  rw [add_zero, h0 x h, mul_zero, zero_add] at h1,
+  exact le_antisymm (lem2 fineq 0) h1
 end
 
 end IMO2011A6
