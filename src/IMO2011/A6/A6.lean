bc2d76952c5cd7b968b61e7b6ab7f8e914ab7057
Gian Sanjaya <mortarsanjaya@gmail.com>
2022-09-01 20:32:05 -0400

    2011 A6: Change variable name: F to R

--- src/IMO2011/A6/A6.lean
+++ src/IMO2011/A6/A6.lean
@@ -5,25 +5,25 @@ import algebra.order.field tactic.linarith
 namespace IMOSL
 namespace IMO2011A6
 
-variables {F : Type*} [linear_ordered_comm_ring F]
+variables {R : Type*} [linear_ordered_comm_ring R]
 
-def fn_ineq (f : F → F) := ∀ x y : F, f (x + y) ≤ y * f x + f (f x)
+def fn_ineq (f : R → R) := ∀ x y : R, f (x + y) ≤ y * f x + f (f x)
 
 
 
-variables {f : F → F} (fineq : fn_ineq f)
+variables {f : R → R} (fineq : fn_ineq f)
 include fineq
 
-private lemma lem1 (x : F) : x * f x ≤ 0 :=
+private lemma lem1 (x : R) : x * f x ≤ 0 :=
 begin
-  revert x; suffices : ∀ t x : F, f t ≤ (t - x) * f x + f (f x),
+  revert x; suffices : ∀ t x : R, f t ≤ (t - x) * f x + f (f x),
     intros x; linarith [this (f (2 * f x)) x, this (f x) (2 * f x)],
   intros t x,
   replace fineq := fineq x (t - x),
   rwa add_sub_cancel'_right at fineq
 end
 
-private lemma lem2 (x : F) : f x ≤ 0 :=
+private lemma lem2 (x : R) : f x ≤ 0 :=
 begin
   have h := fineq x 0,
   rw [add_zero, zero_mul, zero_add] at h,
@@ -34,15 +34,15 @@ end
 
 
 
-/-- Final solution -/
-theorem final_solution (x : F) (h : x ≤ 0) : f x = 0 :=
+/-- Rinal solution -/
+theorem final_solution (x : R) (h : x ≤ 0) : f x = 0 :=
 begin
-  have h0 : ∀ y : F, y < 0 → f y = 0 :=
+  have h0 : ∀ y : R, y < 0 → f y = 0 :=
     λ y h, le_antisymm (lem2 fineq y) (nonneg_of_mul_nonpos_right (lem1 fineq y) h),
   rw le_iff_lt_or_eq at h,
   rcases h with h | rfl,
   exact h0 x h,
-  cases exists_lt (0 : F) with x h,
+  cases exists_lt (0 : R) with x h,
   have h1 := fineq x 0,
   rw [add_zero, h0 x h, mul_zero, zero_add] at h1,
   exact le_antisymm (lem2 fineq 0) h1
