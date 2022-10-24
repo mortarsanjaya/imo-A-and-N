09aa056b53c3d426be3d3beede28033405ff0f68
Gian Sanjaya <mortarsanjaya@gmail.com>
2022-10-23 20:05:31 -0400

    2018 A7: Some slight change in proof skeleton

--- src/IMO2018/A7/A7.lean
+++ src/IMO2018/A7/A7.lean
@@ -9,68 +9,96 @@ open finset nnreal function
 open_locale nnreal
 
 noncomputable def target_sum (q : ℝ≥0) (a : ℕ → ℝ≥0) (n : ℕ) :=
-  (range (2 * n)).sum (λ i, (a i / (a (i + 1) + q)) ^ (3⁻¹ : ℝ))
+  (range (2 * n)).sum (λ i, (a i / (a (i + 1) + q)) ^ (1 / 3 : ℝ))
+
+
+
+section extra_lemmas
+
+private lemma cauchy_schwarz_engel {a b : ℕ → ℝ≥0} (hb : ∀ i : ℕ, 0 < b i) (n : ℕ) :
+  (range n).sum a ^ 2 / (range n).sum b ≤ (range n).sum (λ i, a i ^ 2 / b i) :=
+begin
+  apply div_le_of_le_mul,
+  rw [← le_sqrt_iff, sqrt_eq_rpow, mul_rpow],
+  convert nnreal.inner_le_Lp_mul_Lq (range n)
+    (λ i, a i / sqrt (b i)) (λ i, sqrt (b i)) _,
+  funext i; symmetry; refine div_mul_cancel _ (ne_of_gt _),
+  rw sqrt_eq_rpow; exact rpow_pos (hb i),
+  funext i; rw [div_rpow, rpow_two, rpow_two, sq_sqrt],
+  funext i; rw [rpow_two, sq_sqrt],
+  rw real.is_conjugate_exponent_iff; norm_num
+end
+
+end extra_lemmas
 
 
 
 section upper_bound
 
-private lemma AM_GM3 (a b c : ℝ≥0) :
-  (a * b * c) ^ (3⁻¹ : ℝ) ≤ 3⁻¹ * (a + b + c) :=
+variables {a : ℕ → ℝ≥0} {n : ℕ} (hn : 0 < n) (h : periodic a n) {q t s : ℝ≥0} (hq : 0 < q)
+  (ht : (range n).sum (λ i, sqrt (a i)) = n • t) (hs : (range n).sum (λ i, (a i + q)⁻¹) = n • s)
+include hn h hq ht hs
+  
+private lemma holder_target :
+  (range n).sum (λ i, (a i / (a (i + 1) + q)) ^ (1 / 3 : ℝ)) ≤
+    n • (t ^ (2 / 3 : ℝ) * s ^ (1 / 3 : ℝ)) :=
 begin
-  rw [mul_rpow, mul_rpow, mul_add, mul_add],
-  change (3⁻¹ : ℝ) with ((3⁻¹ : ℝ≥0) : ℝ),
-  apply nnreal.geom_mean_le_arith_mean3_weighted,
-  rw [← two_mul, ← add_one_mul, ← bit1],
-  exact mul_inv_cancel three_ne_zero
+  have X : (3 / 2 : ℝ).is_conjugate_exponent 3 :=
+    by rw real.is_conjugate_exponent_iff; norm_num,
+  convert nnreal.inner_le_Lp_mul_Lq (range n)
+    (λ i, a i ^ (1 / 3 : ℝ)) (λ i, (a (i + 1) + q)⁻¹ ^ (1 / 3 : ℝ)) X,
+  funext; rw [div_eq_mul_inv, mul_rpow],
+  replace X : (3 : ℝ) ≠ 0 := three_ne_zero,
+  rw [one_div_div, ← rpow_eq_rpow_iff X, nsmul_eq_mul, mul_rpow, ← rpow_mul, inv_mul_cancel X,
+      rpow_one, mul_rpow, ← rpow_mul, div_mul_cancel 2 X, ← rpow_mul, div_mul_cancel 1 X, rpow_one],
+  conv_rhs {
+    congr, congr, congr, skip, funext, rw [← rpow_mul, mul_div, inv_mul_cancel X, ← sqrt_eq_rpow],
+    skip, skip, congr, skip, funext, rw [← rpow_mul, inv_mul_cancel X, rpow_one] },
+  replace h : periodic (λ i, (a i + q)⁻¹) n := λ i, by simp only []; rw h,
+  rw [ht, extra.periodic_sum_const h, hs, nsmul_eq_mul, mul_rpow, nsmul_eq_mul,
+      mul_mul_mul_comm, ← rpow_nat_cast, nat.cast_two, bit1, rpow_add, rpow_one],
+  rw nat.cast_ne_zero; exact ne_of_gt hn
 end
 
-private lemma one_liner {q c : ℝ≥0} (hq : 0 < q) (hc : 0 < c) (a b : ℝ≥0) :
-  (a / (b + q)) ^ (3⁻¹ : ℝ) ≤
-    (c / q) ^ (3⁻¹ : ℝ) * 3⁻¹ * ((a + q) / c + a / (a + q) + q / (b + q)) :=
+variables {p : ℝ≥0} (hp : (range n).sum a = n • p)
+include hp
+
+private lemma case_q_le_p_step1 (c : ℝ≥0) :
+  c ^ 2 * q * s + 2 * c * t ≤ p + q + c ^ 2 :=
 begin
-  rw mul_assoc; convert mul_le_mul_of_nonneg_left (AM_GM3 _ _ _) (zero_le _),
-  rw [← mul_rpow, ← mul_assoc, ← mul_assoc, mul_comm (_ / q), div_mul_div_cancel _ (ne_of_gt hc),
-      mul_comm (_ / q), div_mul_div_cancel, div_mul_div_cancel _ (ne_of_gt hq)],
-  rw ← zero_lt_iff; exact add_pos_of_nonneg_of_pos (zero_le a) hq
+  sorry
 end
 
-private lemma upper_bound_non_optimized {q c : ℝ≥0} (hq : 0 < q) (hc : 0 < c)
-  {a : ℕ → ℝ≥0} {n : ℕ} (h : a n = a 0) :
-  (range n).sum (λ i, (a i / (a (i + 1) + q)) ^ (3⁻¹ : ℝ)) ≤ 
-    (c / q) ^ (3⁻¹ : ℝ) * 3⁻¹ * (((range n).sum a + n * q) / c + n) :=
+
+/-
+private lemma cauchy_schwarz_step1 : q * s + t ^ 2 / (p + q) ≤ 1 :=
 begin
-  refine le_of_le_of_eq (sum_le_sum (λ i _, one_liner hq hc _ _)) _,
-  rw [← mul_sum, sum_add_distrib, sum_add_distrib, ← finset.sum_div,
-      sum_add_distrib, add_assoc, sum_const, card_range, nsmul_eq_mul],
-  congr' 2,
-  rw [← add_left_inj (q / (a 0 + q)), add_assoc, ← sum_range_succ' (λ i, q / (a i + q))],
-  simp only [],
-  rw [sum_range_succ, ← add_assoc, ← sum_add_distrib, h, add_left_inj],
-  conv_lhs { congr, skip, funext,
-    rw [← add_div, div_self (ne_of_gt (add_pos_of_nonneg_of_pos (zero_le _) hq))] },
-  rw [sum_const, card_range, nsmul_one]
+  replace hn : 0 < (n : ℝ≥0) := nat.cast_pos.mpr hn,
+  suffices : q * (n • s) + (n • t) ^ 2 / (range n).sum (λ i, a i + q) ≤ n • 1,
+    rwa [nsmul_eq_mul, sum_add_distrib, sum_const, card_range, hp, ← nsmul_add,
+      nsmul_eq_mul, nsmul_eq_mul, nsmul_eq_mul, mul_pow, sq, mul_assoc, mul_left_comm,
+      mul_div_mul_left _ _ (ne_of_gt hn), ← mul_div, ← mul_add, mul_le_mul_left hn] at this,
+  have X : ∀ i, 0 < a i + q := λ i, add_pos_of_nonneg_of_pos (zero_le (a i)) hq,
+  rw ← ht; refine le_of_le_of_eq (add_le_add_left (cauchy_schwarz_engel X n) _) _,
+  nth_rewrite 2 ← card_range n,
+  rw [← hs, mul_sum, ← sum_add_distrib, ← sum_const],
+  refine sum_congr rfl (λ i _, _),
+  rw [sq_sqrt, ← div_eq_mul_inv, ← add_div, add_comm, div_self (ne_of_gt (X i))]
 end
 
-private lemma upper_bound {q p : ℝ≥0} (hq : 0 < q) (hqp : q ≤ p)
-  {a : ℕ → ℝ≥0} {n : ℕ} (h : periodic a n) (h0 : (range n).sum a = n * p) :
-  (range n).sum (λ i, (a i / (a (i + 1) + q)) ^ (3⁻¹ : ℝ)) ≤ 
-    (n / 2) * (2 * (1 + p / q)) ^ (3⁻¹ : ℝ) :=
+private lemma cauchy_schwarz_step2 : t ^ 2 * s ≤ p / (p + q) :=
 begin
-  have X : 0 < p + q := add_pos (lt_of_lt_of_le hq hqp) hq,
-  have X0 : a n = a 0 := by rw [← zero_add n, h],
-  convert upper_bound_non_optimized hq (mul_pos two_pos X) X0 using 1; clear X0,
-  rw [mul_comm, mul_assoc]; congr,
-  rw [nnreal.add_div' _ _ _ (ne_of_gt hq), one_mul, add_comm, mul_div],
-  rw [h0, ← mul_add, mul_div_mul_right _ _ (ne_of_gt X), nnreal.div_add' _ _ _ two_ne_zero,
-      ← mul_one_add, add_comm, ← bit1, mul_div, mul_comm, mul_assoc, mul_inv_cancel, mul_one],
-  exact three_ne_zero
+  sorry
 end
+
+private lemma special_ineq_step1 : 
+-/
 
 end upper_bound
 
 
 
+/-
 section construction
 
 private def good_seq (u v : ℝ≥0) (n : ℕ) : ℝ≥0 := ite (even n) u v
@@ -104,52 +132,26 @@ begin
 end
 
 end construction
+-/
 
 
 
-/-- Final solution -/
-theorem final_solution {q p : ℝ≥0} (hq : 0 < q) (hqp : q ≤ p) (n : ℕ) :
-  is_lub ((λ a : ℕ → ℝ≥0, target_sum q a n) '' {a | (range (2 * n)).sum a = (2 * n) * p ∧ periodic a (2 * n)})
-    (n * (2 * (1 + p / q)) ^ (3⁻¹ : ℝ)) :=
+/-- Final solution, case `p ≤ q` -/
+theorem final_solution_case_p_le_q {q p : ℝ≥0} (hq : 0 < q) (h : p ≤ q) (n : ℕ) :
+  is_greatest ((λ a : ℕ → ℝ≥0, target_sum q a n) ''
+    {a | (range n).sum a = n • p ∧ periodic a n})
+  (n • (p / (p + q)) ^ (3⁻¹ : ℝ)) :=
 begin
-  refine ⟨λ a h, _, λ a h, h _⟩,
-  { rcases h with ⟨a, ⟨h, h0⟩, rfl⟩,
-    unfold target_sum; convert upper_bound hq hqp h0 _ using 2,
-    rw [nat.cast_mul, nat.cast_two, mul_div_cancel_left],
-    exact two_ne_zero,
-    rw [h, nat.cast_mul, nat.cast_two] },
+  sorry
+end
 
-  clear h a; suffices : ∃ u v : ℝ≥0, u + v = 2 * p ∧ u * v = q ^ 2,
-  { rcases this with ⟨u, v, h, h0⟩,
-    refine ⟨good_seq u v, ⟨_, good_seq_periodic u v n⟩, _⟩,
-    rw [good_seq_sum, h, mul_left_comm, mul_assoc],
-    simp only [good_seq_target_sum]; congr,
-
-    obtain ⟨x, rfl⟩ : ∃ x : ℝ≥0, x ^ 2 = u := ⟨sqrt u, by rw sq_sqrt⟩,
-    obtain ⟨y, rfl⟩ : ∃ y : ℝ≥0, y ^ 2 = v := ⟨sqrt v, by rw sq_sqrt⟩,
-    rw [← mul_pow, sq_eq_sq (zero_le (x * y)) (zero_le q)] at h0,
-    rw [← add_left_inj (2 * x * y), ← add_sq', mul_assoc, ← mul_add, h0] at h,
-    rw [one_add_div (ne_of_gt hq), add_comm q, mul_div, ← h, ← h0],
-    clear hq hqp h h0 p q n,
-
-    suffices : ∀ a b : ℝ≥0, (a ^ 2 / b) ^ (3⁻¹ : ℝ) = a / (a * b) ^ (3⁻¹ : ℝ),
-      rw [this, this, this, sq, sq, ← mul_add, ← add_mul, add_comm y,
-          mul_left_comm x, mul_comm y, mul_right_comm, mul_comm, ← add_div],
-    clear x y; intros a b,
-    have X : (3 : ℝ) ≠ 0 := three_ne_zero,
-    rw [← rpow_eq_rpow_iff X, inv_eq_one_div, rpow_self_rpow_inv X,
-        div_rpow, rpow_self_rpow_inv X, ← div_div],
-    congr; rcases eq_or_ne a 0 with rfl | ha,
-    rw [sq, zero_mul, div_zero],
-    rw [eq_div_iff ha, ← pow_succ', ← rpow_nat_cast, ← bit1, nat.cast_bit1, nat.cast_one] },
-
-  { let x := sqrt (p ^ 2 - q ^ 2),
-    have hx : x ≤ p := by rw sqrt_le_iff; exact tsub_le_self,
-    refine ⟨p + x, p - x, _, _⟩,
-    rw [add_assoc, add_tsub_cancel_of_le hx, two_mul],
-    rw [← nnreal.coe_eq, nnreal.coe_mul, nnreal.coe_add, nnreal.coe_sub hx, ← sq_sub_sq,
-        ← nnreal.coe_pow, ← nnreal.coe_pow, sq_sqrt, nnreal.coe_sub, sub_sub_cancel],
-    rwa [← sqrt_le_iff, sqrt_sq] }
+/-- Final solution, case `q ≤ p` -/
+theorem final_solution_case_q_le_p {q p : ℝ≥0} (hq : 0 < q) (h : q ≤ p) (n : ℕ) :
+  is_greatest ((λ a : ℕ → ℝ≥0, target_sum q a n) ''
+    {a | (range (2 * n)).sum a = (2 * n) • p ∧ periodic a (2 * n)})
+  ((2 * n) • ((p + q) / (4 * q)) ^ (3⁻¹ : ℝ)) :=
+begin
+  sorry
 end
 
 end IMO2018A7
