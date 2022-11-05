c47fc2b35eff455ebe8fcb4404686f17751796ec
Gian Sanjaya <mortarsanjaya@gmail.com>
2022-11-05 04:05:59 -0400

    2010 A3: Simplify .lean file

--- src/IMO2010/A3/A3.lean
+++ src/IMO2010/A3/A3.lean
@@ -1,4 +1,4 @@
-import data.real.nnreal algebra.big_operators.intervals algebra.periodic extra.nonneg_semifield
+import data.real.nnreal algebra.big_operators.intervals algebra.periodic
 
 /-! # IMO 2010 A3, Generalized Version -/
 
@@ -9,55 +9,41 @@ open finset function
 open_locale nnreal
 
 def good (x : ℕ → ℝ≥0) := ∀ j : ℕ, x j + x (j + 1) + x (j + 2) ≤ 1
-
-
-
-section extra
-
 def target_sum (x : ℕ → ℝ≥0) (n : ℕ) := (range (2 * n)).sum (λ i, x i * x (i + 2))
 
-private lemma target_sum_zero (x : ℕ → ℝ≥0) : target_sum x 0 = 0 :=
-  by rw [target_sum, mul_zero, range_zero, sum_empty]
-
-private lemma target_sum_succ (x : ℕ → ℝ≥0) (n : ℕ) : target_sum x n.succ =
-    target_sum x n + (x (2 * n) * x (2 * n + 2) + x (2 * n + 1) * x (2 * n + 1 + 2)) :=
-  by rw [target_sum, target_sum, nat.succ_eq_add_one, mul_add_one,
-    finset.sum_range_add, sum_range_succ, sum_range_one, add_zero]
-
-end extra
 
 
-
-/-- Upper bound on the target sum over all good sequences -/
-private lemma good_up_bound {x : ℕ → ℝ≥0} (h : good x) (n : ℕ) : target_sum x n ≤ (n : ℝ≥0) / 4 :=
+private lemma lem1 {a b c d : ℝ≥0} (h : a + b + c ≤ 1) (h0 : b + c + d ≤ 1) :
+  a * c + b * d ≤ 1 / 4 :=
 begin
-  induction n with n n_ih,
-  rw [target_sum_zero, nat.cast_zero, zero_div],
-  rw [target_sum_succ, nat.succ_eq_add_one, nat.cast_add, nat.cast_one, add_div],
-  apply add_le_add n_ih; clear n_ih,
-  rw [← add_le_add_iff_left ((x (2 * n + 1) + x (2 * n + 2)) * x (2 * n + 2)),
-      ← add_assoc, ← add_mul, add_comm _ (x (2 * n)), ← add_assoc],
-  refine le_trans (add_le_add_right (mul_le_mul_of_nonneg_right (h _) (zero_le _)) _) _,
-  rw [← add_le_add_iff_right (x (2 * n + 1) * (x (2 * n + 1) + x (2 * n + 1 + 1))),
-      one_mul, add_assoc, ← mul_add, add_comm (x (2 * n + 1 + 2))],
-  refine le_trans (add_le_add_left (mul_le_mul_of_nonneg_left (h _) (zero_le _)) _) _,
-  rw [mul_one, add_right_comm, mul_comm _ (x _), add_assoc _ 1, ← add_mul, add_comm, ← sq],
-  generalize : x (2 * n + 1) + x (2 * n + 2) = c,
-  rcases c with ⟨c, h⟩,
-  rw [← nnreal.coe_le_coe, nonneg.coe_add, nonneg.mk_pow, subtype.coe_mk, subtype.coe_mk,
-      nonneg.coe_div, nnreal.coe_bit0, nnreal.coe_bit0, nonneg.coe_one, one_div, ← sub_nonneg],
-  convert (sq_nonneg (c - 2⁻¹)),
-  rw [sub_sq', mul_right_comm, mul_inv_cancel, one_mul, sq (2 : ℝ)⁻¹, ← mul_inv, two_mul, ← bit0],
-  exact two_ne_zero
+  rw add_assoc at h,
+  generalize_hyp hx : b + c = x at h h0,
+  rw [← add_le_add_iff_left (x * c), ← add_assoc, ← add_mul, add_comm x],
+  refine le_trans (add_le_add_right (mul_le_mul_of_nonneg_right h (zero_le _)) _) _,
+  rw [one_mul, ← add_le_add_iff_right (b * x), add_assoc, ← mul_add, add_comm d],
+  refine le_trans (add_le_add_left (mul_le_mul_of_nonneg_left h0 (zero_le _)) _) _,
+  rw [mul_one, add_right_comm, mul_comm, ← add_mul, add_comm, hx, ← sq],
+  clear h h0 hx a b c d,
+
+  rcases x with ⟨x, h⟩,
+  rw [← nnreal.coe_le_coe, nonneg.coe_add, nonneg.mk_pow, subtype.coe_mk, subtype.coe_mk],
+  convert two_mul_le_add_sq x (1 / 2),
+  rw [mul_right_comm, mul_div_cancel' 1 (two_ne_zero : (2 : ℝ) ≠ 0), one_mul],
+  norm_num
 end
 
-
-
-private noncomputable def good_sup (j : ℕ) : ℝ≥0 := ite (even j) (1 / 2) 0
-
-private lemma good_sup1 : good good_sup :=
+private lemma target_sum_good_bound {x : ℕ → ℝ≥0} (h : good x) (n : ℕ) :
+  target_sum x n ≤ (n : ℝ≥0) / 4 :=
 begin
-  intros j; simp only [good_sup, nat.even_add_one, not_not],
+  unfold target_sum; induction n with n n_ih,
+  rw [mul_zero, sum_range_zero, nat.cast_zero, zero_div],
+  rw [nat.mul_succ, sum_range_succ, sum_range_succ, add_assoc, nat.cast_succ, add_div],
+  exact add_le_add n_ih (lem1 (h _) (h _))
+end
+
+private lemma bin_even_good : good (λ j, ite (even j) (1 / 2) 0) :=
+begin
+  intros j; simp only [nat.even_add_one, not_not],
   rw [ite_not, apply_ite2 has_add.add, add_zero, zero_add, apply_ite2 has_add.add,
       add_zero, ← add_div, ← bit0, div_self (two_ne_zero : (2 : ℝ≥0) ≠ 0)],
   by_cases h : even j,
@@ -66,18 +52,15 @@ begin
   exact one_le_two
 end
 
-private lemma good_sup2 (n : ℕ) : target_sum good_sup n = (n : ℝ≥0) / 4 :=
+private lemma bin_even_target_sum (n : ℕ) :
+  target_sum (λ j, ite (even j) (1 / 2) 0) n = (n : ℝ≥0) / 4 :=
 begin
-  induction n with n n_ih,
-  rw [target_sum_zero, nat.cast_zero, zero_div],
-  rw [target_sum_succ, nat.succ_eq_add_one, nat.cast_add, nat.cast_one, add_div, n_ih],
-  simp only [good_sup, nat.even_add_one, not_not, add_right_inj],
-  rw [ite_not, apply_ite2 has_mul.mul, apply_ite2 has_mul.mul, apply_ite2 has_add.add,
-      mul_zero, add_zero, zero_add, if_t_t, ← mul_div_mul_comm, mul_one, two_mul, ← bit0]
+  unfold target_sum; induction n with n n_ih,
+  rw [mul_zero, sum_range_zero, nat.cast_zero, zero_div],
+  rw [nat.mul_succ, sum_range_succ, sum_range_succ, n_ih, add_assoc, nat.cast_succ, add_div],
+  simp only [nat.even_add_one, not_not, even.mul_right even_two]; simp only [not_true],
+  rw [if_true, if_false, zero_mul, add_zero, one_div_mul_one_div, bit0, ← two_mul]
 end
-
-private lemma good_sup3 (n : ℕ) : periodic good_sup (2 * n) :=
-  λ x, by simp only [good_sup, nat.even_add, two_mul, iff_self, iff_true]
 
 
 
@@ -86,9 +69,10 @@ theorem final_solution (n : ℕ) :
   is_greatest ((λ x : ℕ → ℝ≥0, target_sum x n) '' {x | good x ∧ periodic x (2 * n)})
     ((n : ℝ≥0) / 4) :=
 begin
-  refine ⟨⟨good_sup, ⟨good_sup1, good_sup3 n⟩, good_sup2 n⟩, λ a h, _⟩,
+  refine ⟨⟨_, ⟨bin_even_good, λ k, _⟩, bin_even_target_sum n⟩, λ a h, _⟩,
+  simp only [nat.even_add, even_two_mul, iff_true],
   rcases h with ⟨x, ⟨h, -⟩, rfl⟩,
-  exact good_up_bound h n
+  exact target_sum_good_bound h n
 end
 
 end IMO2010A3
