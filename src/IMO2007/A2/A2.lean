89acb53a8279c5b5a2b4e7293524421d815df57e
Gian Sanjaya <mortarsanjaya@gmail.com>
2022-11-05 03:04:50 -0400

    2007 A2: Update .lean and TeX file

--- src/IMO2007/A2/A2.lean
+++ src/IMO2007/A2/A2.lean
@@ -13,6 +13,7 @@ def good (f : ℕ+ → ℕ+) := ∀ m n : ℕ+, f m + f (f n) ≤ f (m + n) + 1
 
 
 
+/-! ## Correspondence between `good` and `good'` -/
 section correspondence
 
 private lemma good_iff_good' (f : ℕ+ → ℕ+) : good f ↔ good' (λ x, (f x.succ_pnat).nat_pred) :=
@@ -52,6 +53,8 @@ end correspondence
 
 
 
+/-! ## Main results -/
+
 private lemma good'_monotone {f : ℕ → ℕ} (h : good' f) : monotone f :=
 begin
   intros x y h0,
@@ -61,6 +64,67 @@ begin
   rw add_zero,
   rw [nat.succ_eq_add_one, ← add_assoc],
   exact le_trans le_self_add (h x c)
+end
+
+private lemma good'_val_bound {f : ℕ → ℕ} (h : good' f) (N : ℕ) : f N ≤ N + 1 :=
+begin
+  generalize hK : N + 1 = K,
+  rw [← not_lt, ← nat.add_one_le_iff]; intros h0,
+
+  -- We prove that `f(mK) ≥ mf(K)` for all `m : ℕ`.
+  have h1 : ∀ m : ℕ, m * f K ≤ f (m * K) :=
+  begin
+    intros m; induction m with m m_ih,
+    rw [zero_mul, zero_mul]; exact zero_le _,
+    rw [nat.succ_mul, nat.succ_mul, ← hK, ← add_assoc, hK],
+    exact le_trans (add_le_add m_ih (good'_monotone h (nat.le_of_succ_le h0))) (h _ _)
+  end,
+
+  -- For the case `m = K`, get some `d ≥ N` such that `f(K^2) = K^2 + d + 1`
+  replace h1 : ∃ d : ℕ, N ≤ d ∧ f (K * K) = K * K + d + 1 :=
+  begin
+    replace h0 := le_trans h0 (good'_monotone h N.le_succ),
+    rw [nat.succ_eq_add_one, hK] at h0,
+    replace h1 := le_trans (nat.mul_le_mul_left _ h0) (h1 K),
+    rw [mul_add_one, le_iff_exists_add] at h1,
+    cases h1 with c h1,
+    refine ⟨N + c, le_self_add, _⟩,
+    rw [add_assoc, add_right_comm, hK, ← add_assoc, ← h1]
+  end,
+
+  -- Finishing
+  rcases h1 with ⟨d, h2, h3⟩,
+  replace hK := h d (K * K),
+  rw [add_comm d, h3, add_le_iff_nonpos_left, le_zero_iff] at hK,
+  revert hK; exact ne_of_gt (lt_of_lt_of_le K.succ_pos (le_trans h0 (good'_monotone h h2)))
+end
+
+private lemma sub_right_good' (c : ℕ) : good' (λ n, n - c) :=
+begin
+  intros m n; dsimp only [],
+  cases le_total n c with h h,
+  rw [nat.sub_eq_zero_of_le h, nat.zero_sub, add_zero, add_assoc],
+  exact nat.sub_le_sub_right le_self_add _,
+  rw le_iff_exists_add at h; rcases h with ⟨d, rfl⟩,
+  rw [nat.add_sub_cancel_left, add_right_comm, add_comm c, ← add_assoc, nat.add_sub_cancel],
+  exact add_le_add (le_trans (nat.sub_le m c) m.le_succ) (nat.sub_le d c)
+end
+
+private lemma add_bin_div_good' {K : ℕ} (h : 1 < K) : good' (λ n, n + ite (K ∣ n + 1) 1 0) :=
+begin
+  intros m n; dsimp only [],
+  rw [add_add_add_comm, ← add_assoc m, add_assoc, add_assoc (m + n), add_le_add_iff_left],
+  by_cases h0 : K ∣ n + 1,
+  { rw [if_pos h0, add_le_add_iff_left, add_assoc m, add_right_comm m],
+    simp only [nat.dvd_add_left h0, add_le_iff_nonpos_right, le_zero_iff],
+    refine if_neg (λ h1, _),
+    rw [nat.dvd_add_right h0, nat.dvd_one] at h1,
+    exact ne_of_gt h h1 },
+  { rw [if_neg h0, zero_add, add_zero, if_neg h0, add_zero],
+    refine le_trans _ le_self_add,
+    by_cases h1 : K ∣ m + 1,
+    rw if_pos h1,
+    rw if_neg h1; exact zero_le_one }
 end
 
 
@@ -86,69 +150,12 @@ theorem final_solution_nat_ne_zero {N : ℕ} (h : 0 < N) (k : ℕ) :
   (∃ f : ℕ → ℕ, good' f ∧ f N = k) ↔ k ≤ N + 1 :=
 begin
   refine ⟨λ h0, _, λ h0, _⟩,
-
-  ---- `f(N) ≤ N + 1` for all `N : ℕ`.
-  { clear h; rcases h0 with ⟨f, h, rfl⟩,
-    generalize hK : N + 1 = K,
-    rw [← not_lt, ← nat.add_one_le_iff]; intros h0,
-
-    -- We prove that `f(mK) ≥ mf(K)` for all `m : ℕ` here.
-    have h1 : ∀ m : ℕ, m * f K ≤ f (m * K) :=
-    begin
-      intros m; induction m with m m_ih,
-      rw [zero_mul, zero_mul]; exact zero_le _,
-      rw [nat.succ_mul, nat.succ_mul, ← hK, ← add_assoc, hK],
-      exact le_trans (add_le_add m_ih (good'_monotone h (nat.le_of_succ_le h0))) (h _ _)
-    end,
-
-    -- For the case `m = K`, get some `d ≥ N` such that `f(K^2) = K^2 + d + 1`
-    replace h1 : ∃ d : ℕ, N ≤ d ∧ f (K * K) = K * K + d + 1 :=
-    begin
-      replace h0 := le_trans h0 (good'_monotone h N.le_succ),
-      rw [nat.succ_eq_add_one, hK] at h0,
-      replace h1 := le_trans (nat.mul_le_mul_left _ h0) (h1 K),
-      rw [mul_add_one, le_iff_exists_add] at h1,
-      cases h1 with c h1,
-      refine ⟨N + c, le_self_add, _⟩,
-      rw [add_assoc, add_right_comm, hK, ← add_assoc, ← h1]
-    end,
-
-    -- Finishing
-    rcases h1 with ⟨d, h2, h3⟩,
-    replace hK := h d (K * K),
-    rw [add_comm d, h3, add_le_iff_nonpos_left, le_zero_iff] at hK,
-    revert hK; exact ne_of_gt (lt_of_lt_of_le K.succ_pos (le_trans h0 (good'_monotone h h2))) },
-  
+  rcases h0 with ⟨f, h0, rfl⟩,
+  exact good'_val_bound h0 N,
   rw [le_iff_lt_or_eq, nat.lt_succ_iff, le_iff_exists_add] at h0,
   rcases h0 with ⟨c, rfl⟩ | rfl,
-
-  ---- Construction of good `f` with `f(N) = k`, `k ≤ N`.
-  -- We have already substituted `N = k + c` here, and we do not need `N > 0`.
-  { refine ⟨λ n, n - c, λ m n, _, nat.add_sub_cancel k c⟩,
-    clear h k; dsimp only []; cases le_total n c with h h,
-    rw [nat.sub_eq_zero_of_le h, nat.zero_sub, add_zero, add_assoc],
-    exact nat.sub_le_sub_right le_self_add _,
-    rw le_iff_exists_add at h; rcases h with ⟨d, rfl⟩,
-    rw [nat.add_sub_cancel_left, add_right_comm, add_comm c, ← add_assoc, nat.add_sub_cancel],
-    exact add_le_add (le_trans (nat.sub_le m c) m.le_succ) (nat.sub_le d c) },
-
-  ---- Construction of good `f` with `f(N) = N + 1`, `N > 0`.
-  { refine ⟨λ n, n + ite (N + 1 ∣ n + 1) 1 0, λ m n, _, by rw if_pos (dvd_refl _)⟩,
-    rw [← add_lt_add_iff_right 1, zero_add] at h,
-    generalize_hyp : N + 1 = M at h ⊢,
-    dsimp only []; clear N,
-    rw [add_add_add_comm, ← add_assoc m, add_assoc, add_assoc (m + n), add_le_add_iff_left],
-    by_cases h0 : M ∣ n + 1,
-    { rw [if_pos h0, add_le_add_iff_left, add_assoc m, add_right_comm m],
-      simp only [nat.dvd_add_left h0, add_le_iff_nonpos_right, le_zero_iff],
-      refine if_neg (λ h1, _),
-      rw [nat.dvd_add_right h0, nat.dvd_one] at h1,
-      exact ne_of_gt h h1 },
-    { rw [if_neg h0, zero_add, add_zero, if_neg h0, add_zero],
-      refine le_trans _ le_self_add,
-      by_cases h1 : M ∣ m + 1,
-      rw if_pos h1,
-      rw if_neg h1; exact zero_le_one } } 
+  exact ⟨λ n, n - c, sub_right_good' c, nat.add_sub_cancel k c⟩,
+  exact ⟨_, add_bin_div_good' (nat.succ_lt_succ h), by rw if_pos (dvd_refl _)⟩
 end
 
 /-- Final solution, `pnat` version, `N = 1` -/
