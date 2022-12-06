cdc02fcef745a9d73e0bda004b2c531602efd91e
Gian Sanjaya <43656481+mortarsanjaya@users.noreply.github.com>
2022-12-06 03:59:44 -0500

    2017 A8: Simplify proofs

--- src/IMO2017/A8/A8.lean
+++ src/IMO2017/A8/A8.lean
@@ -72,75 +72,78 @@ end
 
 
 
-/-- Explicit construction of the good non-monotone function -/
-theorem good_of_discrete {g : G} (h : 0 < g) (h0 : ∀ x : G, 0 < x → g ≤ x) :
-  good (λ x : G, ite (x = 0) (2 • g) (ite (x = g) 0 (2 • x))) :=
-begin
-  intros x y h1; dsimp only [] at h1 ⊢,
-  rcases eq_or_ne 0 x with rfl | hx0,
-
-  ---- Case 1: `x = 0`
-  { rw if_pos rfl at h1 ⊢,
-    rcases eq_or_ne 0 y with rfl | hy0,
-    rw if_pos rfl at h1 ⊢,
-    exfalso; exact ne_of_lt h1 rfl,
-    rcases eq_or_ne g y with rfl | hyg,
-    rw [if_neg hy0.symm, if_pos rfl] at h1 ⊢,
-    exfalso; exact lt_asymm h1 (smul_pos two_pos h),
-    rw [if_neg hy0.symm, if_neg hyg.symm] at h1 ⊢,
-    rw [zero_add, and_comm, two_nsmul, le_add_iff_nonneg_left, two_nsmul, ← le_sub_iff_add_le],
-    rw smul_lt_smul_iff_of_pos at h1,
-    exacts [⟨le_of_lt (lt_trans h h1), h0 _ (sub_pos_of_lt h1)⟩, two_pos] },
-  rw if_neg hx0.symm at h1 ⊢,
-  rcases eq_or_ne g x with rfl | hxg,
-
-  ---- Case 2: `x = g`
-  { rw if_pos rfl at h1 ⊢,
-    rcases eq_or_ne 0 y with rfl | hy0,
-    rw if_pos rfl at h1 ⊢,
-    rw add_zero; exact ⟨le_of_lt h, h0 _ h1⟩,
-    rcases eq_or_ne g y with rfl | hyg,
-    rw [if_neg hy0.symm, if_pos rfl] at h1 ⊢,
-    exfalso; exact lt_irrefl 0 h1,
-    rw [if_neg hy0.symm, if_neg hyg.symm] at h1 ⊢,
-    rw [two_nsmul, add_le_add_iff_right],
-    rw smul_pos_iff_of_pos at h1,
-    exacts [⟨le_of_lt (add_pos h h1), h0 y h1⟩, two_pos] },
-
-  ---- Case 3: `x ∉ {0, g}`
-  { rw if_neg hxg.symm at h1 ⊢,
-    rcases eq_or_ne 0 y with rfl | hy0,
-    rw if_pos rfl at h1 ⊢,
-    rw [add_zero, two_nsmul, add_le_iff_nonpos_left, and_iff_left_of_imp],
-    contrapose! h1; exact nsmul_le_nsmul_of_le_right (h0 x h1) 2,
-    intros h2; exact le_trans h2 (nsmul_nonneg (le_of_lt h) 2),
-    rcases eq_or_ne g y with rfl | hyg,
-    rw [if_neg hy0.symm, if_pos rfl] at h1 ⊢,
-    rw [two_nsmul, add_le_add_iff_left, ← le_neg_iff_add_nonpos_left],
-    rw left.nsmul_neg_iff at h1,
-    exact ⟨le_of_lt (lt_trans h1 h), h0 _ (neg_pos.mpr h1)⟩,
-    exact two_pos,
-    rw [if_neg hy0.symm, if_neg hyg.symm] at h1 ⊢,
-    rw [two_nsmul, add_le_add_iff_left, two_nsmul, add_le_add_iff_right, and_self],
-    rw smul_lt_smul_iff_of_pos at h1,
-    exacts [le_of_lt h1, two_pos] }
-end
-
-
-
 /-- Final solution, part 2 -/
 theorem final_solution_part2 (h : ¬densely_ordered G) :
-  ∃ f : G → G, good f ∧ ¬monotone f :=
+  ∃ f : G → G, ¬monotone f ∧ good f :=
 begin
-  contrapose! h; refine ⟨λ a b h0, _⟩,
-  obtain ⟨g, rfl⟩ : ∃ g : G, g + a = b := ⟨b - a, sub_add_cancel b a⟩,
-  rw lt_add_iff_pos_left at h0,
-  contrapose! h; refine ⟨_, good_of_discrete h0 (λ x h1, _), λ h1, _⟩,
-  exact le_of_add_le_add_right (h (x + a) (lt_add_of_pos_left a h1)),
-  replace h1 := h1 (le_of_lt h0),
-  revert h1; simp only [if_true, eq_self_iff_true],
-  rw [if_neg (ne_of_gt h0), imp_false, not_le],
-  exact smul_pos two_pos h0
+  ---- Change non-dense-ordering criterion with existence of a minimal positive element
+  replace h : ∃ g : G, 0 < g ∧ ∀ x : G, 0 < x → g ≤ x :=
+  begin
+    contrapose! h; refine ⟨λ x y h0, _⟩,
+    replace h := h (y - x) (sub_pos_of_lt h0),
+    clear h0; rcases h with ⟨g, h, h0⟩,
+    exact ⟨g + x, lt_add_of_pos_left x h, lt_sub_iff_add_lt.mp h0⟩
+  end,
+  
+  ---- Given a minimal positive element `g`, construct a good non-monotone function
+  rcases h with ⟨g, h, h0⟩,
+  refine ⟨λ x : G, ite (x = 0) (2 • g) (ite (x = g) 0 (2 • x)), _, λ x y h1, _⟩,
+
+  ---- Show that the function is not monotone
+  { simp_rw [monotone, not_forall],
+    refine ⟨0, g, le_of_lt h, _⟩,
+    rw [if_pos rfl, if_neg (ne_of_gt h), if_pos rfl, not_le],
+    exact smul_pos two_pos h },
+
+  ---- Show that the function is good
+  { dsimp only [] at h1 ⊢,
+    rcases eq_or_ne 0 x with rfl | hx0,
+
+    -- Case 1: `x = 0`
+    { rw if_pos rfl at h1 ⊢,
+      rcases eq_or_ne 0 y with rfl | hy0,
+      rw if_pos rfl at h1 ⊢,
+      exfalso; exact ne_of_lt h1 rfl,
+      rcases eq_or_ne g y with rfl | hyg,
+      rw [if_neg hy0.symm, if_pos rfl] at h1 ⊢,
+      exfalso; exact lt_asymm h1 (smul_pos two_pos h),
+      rw [if_neg hy0.symm, if_neg hyg.symm] at h1 ⊢,
+      rw [zero_add, and_comm, two_nsmul, le_add_iff_nonneg_left, two_nsmul, ← le_sub_iff_add_le],
+      rw smul_lt_smul_iff_of_pos at h1,
+      exacts [⟨le_of_lt (lt_trans h h1), h0 _ (sub_pos_of_lt h1)⟩, two_pos] },
+    rw if_neg hx0.symm at h1 ⊢,
+    rcases eq_or_ne g x with rfl | hxg,
+
+    -- Case 2: `x = g`
+    { rw if_pos rfl at h1 ⊢,
+      rcases eq_or_ne 0 y with rfl | hy0,
+      rw if_pos rfl at h1 ⊢,
+      rw add_zero; exact ⟨le_of_lt h, h0 _ h1⟩,
+      rcases eq_or_ne g y with rfl | hyg,
+      rw [if_neg hy0.symm, if_pos rfl] at h1 ⊢,
+      exfalso; exact lt_irrefl 0 h1,
+      rw [if_neg hy0.symm, if_neg hyg.symm] at h1 ⊢,
+      rw [two_nsmul, add_le_add_iff_right],
+      rw smul_pos_iff_of_pos at h1,
+      exacts [⟨le_of_lt (add_pos h h1), h0 y h1⟩, two_pos] },
+
+    -- Case 3: `x ∉ {0, g}`
+    { rw if_neg hxg.symm at h1 ⊢,
+      rcases eq_or_ne 0 y with rfl | hy0,
+      rw if_pos rfl at h1 ⊢,
+      rw [add_zero, two_nsmul, add_le_iff_nonpos_left, and_iff_left_of_imp],
+      contrapose! h1; exact nsmul_le_nsmul_of_le_right (h0 x h1) 2,
+      intros h2; exact le_trans h2 (nsmul_nonneg (le_of_lt h) 2),
+      rcases eq_or_ne g y with rfl | hyg,
+      rw [if_neg hy0.symm, if_pos rfl] at h1 ⊢,
+      rw [two_nsmul, add_le_add_iff_left, ← le_neg_iff_add_nonpos_left],
+      rw left.nsmul_neg_iff at h1,
+      exact ⟨le_of_lt (lt_trans h1 h), h0 _ (neg_pos.mpr h1)⟩,
+      exact two_pos,
+      rw [if_neg hy0.symm, if_neg hyg.symm] at h1 ⊢,
+      rw [two_nsmul, add_le_add_iff_left, two_nsmul, add_le_add_iff_right, and_self],
+      rw smul_lt_smul_iff_of_pos at h1,
+      exacts [le_of_lt h1, two_pos] } }
 end
 
 
@@ -151,7 +154,8 @@ begin
   refine ⟨λ h, _, λ h, _⟩,
   haveI : densely_ordered G := h,
   exact final_solution_part1,
-  contrapose! h; exact final_solution_part2 h
+  contrapose! h; simp_rw [and_comm (good _)],
+  exact final_solution_part2 h
 end
 
 
