f3d8493cf220cc0a9af31fc4089b74154f83505c
Gian Sanjaya <43656481+mortarsanjaya@users.noreply.github.com>
2022-12-08 16:37:14 -0500

    2012 N1: Generalize to commutative rings

--- src/IMO2012/N1/N1.lean
+++ src/IMO2012/N1/N1.lean
@@ -1,43 +1,44 @@
-import ring_theory.int.basic
+import ring_theory.coprime.lemmas ring_theory.ideal.basic
 
 /-! # IMO 2012 N1 -/
 
 namespace IMOSL
 namespace IMO2012N1
 
-def admissible (A : set ℤ) := ∀ x y : ℤ, x ∈ A → y ∈ A → ∀ k : ℤ, x ^ 2 + k * x * y + y ^ 2 ∈ A
+def admissible {R : Type*} [semiring R] (A : set R) := ∀ x y : R, x ∈ A → y ∈ A → ∀ r : R, x ^ 2 + r * x * y + y ^ 2 ∈ A
 
 
 
 /-- Final solution -/
-theorem final_solution (m n : ℤ) :
-  (∀ A : set ℤ, admissible A → m ∈ A → n ∈ A → ∀ x : ℤ, x ∈ A) ↔ is_coprime m n :=
+theorem final_solution {R : Type*} [comm_ring R] (x y : R) :
+  (∀ A : set R, admissible A → x ∈ A → y ∈ A → ∀ z : R, z ∈ A) ↔ is_coprime x y :=
 begin
   refine ⟨λ h, _, λ h A h0 h1 h2, _⟩,
-  
-  ---- If the only admissible set containing `m` and `n` is `ℤ`, then `gcd(m, n) = 1`
-  { rw [← int.gcd_eq_one_iff_coprime, ← int.coe_nat_eq_coe_nat_iff, nat.cast_one],
-    refine int.eq_one_of_dvd_one (nat.cast_nonneg _)
-      (h {k : ℤ | (int.gcd m n : ℤ) ∣ k} _ (int.gcd_dvd_left m n) (int.gcd_dvd_right m n) 1),
-    generalize : (int.gcd m n : ℤ) = a,
-    clear h m n; intros x y h h0 k,
-    refine dvd_add (dvd_add (dvd_pow h two_ne_zero) _) (dvd_pow h0 two_ne_zero),
-    exact dvd_mul_of_dvd_right h0 _ },
 
-  ---- If `gcd(m, n) = 1`, then the only admissible set containing `m` and `n` is `ℤ`
-  { have h3 : ∀ x : ℤ, x ∈ A → (∀ k : ℤ, k * x ^ 2 ∈ A) :=
+  ---- If the only admissible set containing `x` and `y` is `R`, then `x` and `y` are coprime.
+  { have h0 : ∀ I : ideal R, admissible (I : set R) :=
+      λ I u v h h0 r, by rw [sq, mul_right_comm, ← add_mul, sq];
+        exact I.add_mem (I.mul_mem_left _ h) (I.mul_mem_left _ h0),
+    replace h := h _ (h0 (ideal.span {x, y})) (ideal.subset_span (set.mem_insert x _))
+      (ideal.subset_span (set.mem_insert_of_mem _ (set.mem_singleton y))) 1,
+    simp_rw [set_like.mem_coe, ideal.mem_span_insert, ideal.mem_span_singleton] at h,
+    rcases h with ⟨a, z, ⟨b, rfl⟩, h⟩,
+    rw mul_comm y at h; exact ⟨a, b, h.symm⟩ },
+
+  ---- If `x` and `y` are coprime, the only admissible set `x` and `y` is `R`
+  { have h3 : ∀ z : R, z ∈ A → (∀ k : R, k * z ^ 2 ∈ A) :=
       λ x h3 k, by replace h3 := h0 x x h3 h3 (k - (1 + 1));
-        rwa [mul_assoc, ← sq, ← one_add_mul, add_comm (1 : ℤ),
+        rwa [mul_assoc, ← sq, ← one_add_mul, add_comm (1 : R),
              ← add_one_mul, add_assoc, sub_add_cancel] at h3,
-    suffices : (1 : ℤ) ∈ A,
+    suffices : (1 : R) ∈ A,
       intros x; convert h3 1 this x; rw [one_pow, mul_one],
-    replace h : is_coprime (m ^ 2) (n ^ 2) := is_coprime.pow h,
+    replace h : is_coprime (x ^ 2) (y ^ 2) := is_coprime.pow h,
     rcases h with ⟨a, b, h⟩,
-    replace h1 := h3 m h1 a,
-    replace h2 := h3 n h2 b,
+    replace h1 := h3 x h1 a,
+    replace h2 := h3 y h2 b,
     replace h0 := h0 _ _ h1 h2 2,
-    rwa [← add_sq, h, sq] at h0 }
-end
+    rwa [← add_sq, h, one_pow] at h0 }
+end 
 
 end IMO2012N1
 end IMOSL
