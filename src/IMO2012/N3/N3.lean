d0a7cad2b33bafb340c4bfff039e9c0eef5fefec
Gian Sanjaya <43656481+mortarsanjaya@users.noreply.github.com>
2022-12-07 22:33:31 -0500

    2012 N3: Shorten proof

--- src/IMO2012/N3/N3.lean
+++ src/IMO2012/N3/N3.lean
@@ -5,88 +5,81 @@ import data.nat.choose.basic data.nat.parity
 namespace IMOSL
 namespace IMO2012N3
 
-def good (m : ℕ) := 1 < m ∧ ∀ n : ℕ, 2 * n ≤ m → m ≤ 3 * n → n ∣ n.choose (m - 2 * n)
-
-
-
-private theorem prime_implies_good (p : ℕ) (h : p.prime) : good p :=
-begin
-  refine ⟨nat.prime.one_lt h, λ n h0 h1, _⟩,
-  replace h1 : 0 < n := pos_of_mul_pos_right (lt_of_lt_of_le h.pos h1) zero_le_three,
-  rcases h.eq_two_or_odd with rfl | h2,
-  { replace h1 := nat.mul_le_mul_left 2 h1,
-    rw mul_one at h1,
-    replace h1 := le_antisymm h0 h1,
-    rw nat.mul_right_eq_self_iff two_pos at h1,
-    rw h1; exact one_dvd _ },
-  apply @nat.coprime.dvd_of_dvd_mul_right _ (p - 2 * n),
-  { rw [nat.coprime, nat.gcd_rec, ← nat.add_mul_mod_self_right _ 2,
-        nat.sub_add_cancel h0, ← nat.gcd_rec, nat.gcd_comm],
-    exact nat.coprime_of_lt_prime h1 (lt_of_lt_of_le (lt_two_mul_self h1) h0) h },
-  { rw le_iff_exists_add at h0,
-    rcases h0 with ⟨k, rfl⟩,
-    rcases k with rfl | k,
-    rw [add_zero, nat.mul_mod_right] at h2,
-    exfalso; exact zero_ne_one h2,
-    rw [nat.add_sub_cancel_left, nat.choose_succ_right_eq],
-    cases n,
-    exfalso; exact lt_irrefl 0 h1,
-    rw [nat.succ_eq_add_one, ← nat.choose_mul_succ_eq],
-    exact dvd_mul_left (n + 1) (nat.choose n k) }
-end
-
-private lemma lem1 {m : ℕ} (h : 1 < m) : 2 ∣ m ∨ ∃ p k : ℕ, p.prime ∧ m = p * (2 * k + 1) :=
-begin
-  let p := m.min_fac,
-  cases eq_or_ne p 2 with h0 h0,
-  left; rwa ← nat.min_fac_eq_two_iff,
-  rw [ne.def, nat.min_fac_eq_two_iff, nat.two_dvd_ne_zero, ← nat.odd_iff] at h0,
-  obtain ⟨c, h1⟩ : p ∣ m := nat.min_fac_dvd m,
-  rw h1 at h0; replace h0 := nat.odd.of_mul_right h0,
-  rcases h0 with ⟨k, rfl⟩,
-  right; exact ⟨p, k, nat.min_fac_prime (ne_of_gt h), h1⟩
-end
-
-private lemma lem2 {p : ℕ} (h : p.prime) (k r : ℕ) (h0 : r < p) :
-  ¬p ∣ (p * k + r).desc_factorial r :=
-begin
-  induction r with r r_ih; intros h1,
-  rw [nat.desc_factorial_zero, nat.dvd_one] at h1,
-  rw h1 at h; exact nat.not_prime_one h,
-  rw [nat.succ_eq_add_one r, ← add_assoc, nat.succ_desc_factorial_succ, add_assoc,
-      ← nat.succ_eq_add_one, nat.prime.dvd_mul h, ← nat.dvd_add_iff_right ⟨k, rfl⟩,
-      or_iff_left (r_ih (lt_trans (lt_add_one r) h0))] at h1,
-  exact nat.not_dvd_of_pos_of_lt r.succ_pos h0 h1
-end
-
-private theorem good_implies_prime (m : ℕ) (h : good m) : nat.prime m :=
-begin
-  cases h with h0 h,
-  replace h0 := lem1 h0,
-  rcases h0 with ⟨k, rfl⟩ | ⟨p, k, hp, rfl⟩,
-  { replace h := h k (le_refl _) (nat.mul_le_mul_right k (by norm_num)),
-    rw [nat.sub_self, nat.choose_zero_right, nat.dvd_one] at h,
-    rw [h, mul_one]; exact nat.prime_two },
-  { rcases k.eq_zero_or_pos with rfl | h0,
-    rwa [mul_zero, zero_add, mul_one],
-    replace h := h (p * k),
-    rw [mul_add_one, bit1, add_one_mul 2, mul_left_comm, nat.add_sub_cancel_left] at h,
-    replace h := h le_self_add (by rw [add_le_add_iff_left]; exact nat.le_mul_of_pos_right h0),
-    exfalso; rw [← nat.succ_le_iff, le_iff_exists_add] at h0,
-    rcases h0 with ⟨k, rfl⟩,
-    apply lem2 hp k (p - 1) (nat.sub_lt hp.pos one_pos),
-    rw [← nat.mul_dvd_mul_iff_left (mul_pos hp.pos k.succ_pos), nat.mul_succ],
-    nth_rewrite 4 ← nat.sub_add_cancel hp.pos,
-    rw [← add_assoc, ← nat.succ_desc_factorial_succ, add_assoc, nat.sub_add_cancel hp.pos,
-        nat.desc_factorial_eq_factorial_mul_choose, ← mul_add_one, add_comm, mul_comm],
-    exact mul_dvd_mul (nat.dvd_factorial hp.pos (le_refl p)) h }
-end
-
-
-
 /-- Final solution -/
-theorem final_solution : good = nat.prime :=
-  by ext n; exact ⟨good_implies_prime n, prime_implies_good n⟩
+theorem final_solution {m : ℕ} (h : 1 < m) :
+  (∀ n : ℕ, 2 * n ≤ m → n ∣ n.choose (m - 2 * n)) ↔ m.prime :=
+begin
+  symmetry; refine ⟨λ h0 n h1, _, λ h0, _⟩,
+
+  ---- If `m` is prime, then `n ∣ C(n, m - 2n)` for any `n ≤ m/2`
+  { -- Case 1: `n = 0`
+    clear h; rcases n.eq_zero_or_pos with rfl | h,
+    rw [mul_zero, nat.sub_zero, zero_dvd_iff, nat.choose_eq_zero_iff],
+    exact h0.pos,
+
+    -- Case 2: `m = 2`, `n = 1`
+    rcases h0.eq_two_or_odd with rfl | h2,
+    replace h1 : n = 1 := le_antisymm ((mul_le_iff_le_one_right two_pos).mp h1) h,
+    rw [h1, mul_one, nat.sub_self, nat.choose_succ_self_right],
+  
+    -- Case 3: `m` is an odd prime
+    rw le_iff_exists_add at h1,
+    rcases h1 with ⟨k, rfl⟩,
+    rw [add_comm, nat.add_mul_mod_self_left] at h2,
+    replace h2 : ∃ q : ℕ, q + 1 = k :=
+      ⟨2 * (k / 2), by nth_rewrite 2 ← h2; exact nat.div_add_mod k 2⟩,
+    rcases h2 with ⟨q, rfl⟩,
+    replace h0 := nat.coprime_of_lt_prime q.succ_pos
+      ((lt_add_iff_pos_left _).mpr (mul_pos two_pos h)) h0,
+    rw nat.coprime_add_self_left at h0; replace h0 := h0.coprime_mul_left,
+    rw [← nat.succ_le_iff, le_iff_exists_add'] at h,
+    rcases h with ⟨n, rfl⟩,
+    rw [nat.add_sub_cancel_left, ← h0.dvd_mul_right,
+        nat.choose_succ_right_eq, ← nat.choose_mul_succ_eq],
+    exact dvd_mul_left (n + 1) (n.choose q) },
+
+  ---- If `n ∣ C(n, m - 2n)` for any `n ≤ m/2`, then `m` is prime
+  { -- First, solve the case where `m` is even
+    by_cases h1 : 2 ∣ m,
+    rcases h1 with ⟨r, rfl⟩,
+    replace h0 := h0 r (le_refl _),
+    rw [nat.sub_self, nat.choose_zero_right, nat.dvd_one] at h0,
+    rw [h0, mul_one]; exact nat.prime_two,
+
+    -- Some setup for the case where `m` is odd
+    rcases nat.min_fac_dvd m with ⟨r, h2⟩,
+    rw h2 at h1; replace h1 : ¬2 ∣ r := λ h3,
+      by exfalso; exact h1 (dvd_mul_of_dvd_right h3 (nat.min_fac m)),
+    rw [nat.prime_def_min_fac, nat.succ_le_iff, and_iff_right h],
+    replace h := nat.min_fac_prime (ne_of_gt h),
+    generalize_hyp : m.min_fac = p at h h2 ⊢,
+    subst h2; rw [eq_comm, mul_right_eq_self₀, or_iff_left h.ne_zero],
+    replace h1 : ∃ k : ℕ, 2 * k + 1 = r :=
+      ⟨r / 2, by nth_rewrite 2 ← nat.two_dvd_ne_zero.mp h1; exact nat.div_add_mod r 2⟩,
+    rcases h1 with ⟨k, rfl⟩,
+    rw [add_left_eq_self, nat.mul_eq_zero, or_iff_right (two_ne_zero : 2 ≠ 0)],
+    
+    -- Now choose `n = pk` and reduce to showing that `p ∤ ∏_{i ≤ j} (pk + i)` for all `j < p`
+    replace h0 := h0 (p * k),
+    rw [mul_left_comm, mul_add_one, nat.add_sub_cancel_left] at h0,
+    replace h0 := dvd_mul_of_dvd_right (h0 le_self_add) (p - 1).factorial,
+    contrapose! h0; rw [← zero_lt_iff, ← nat.succ_le_iff, le_iff_exists_add'] at h0,
+    rcases h0 with ⟨k, rfl⟩,
+    rw [← nat.mul_dvd_mul_iff_left (p - 1).succ_pos, nat.mul_succ, ← mul_assoc,
+        ← nat.factorial_succ, nat.succ_eq_add_one, nat.sub_add_cancel h.pos,
+        ← nat.desc_factorial_eq_factorial_mul_choose],
+    suffices : ∀ j : ℕ, j < p → ¬p ∣ (p * k + j).desc_factorial j,
+      replace this := this (p - 1) (nat.sub_lt h.pos one_pos);
+        rwa [← nat.mul_dvd_mul_iff_left (p * k + (p - 1)).succ_pos, ← nat.succ_desc_factorial_succ,
+             nat.succ_eq_add_one, add_assoc, nat.sub_add_cancel h.pos, mul_comm] at this,
+
+    -- Prove that `p ∤ ∏_{i ≤ j} (pk + i)` for `j < p` by induction on `j`
+    intros j h0; induction j with j h1,
+    rw nat.desc_factorial_zero; exact h.not_dvd_one,
+    rw [nat.add_succ, nat.succ_desc_factorial_succ, add_assoc, h.dvd_mul,
+        nat.dvd_add_right ⟨k, rfl⟩, or_iff_right (nat.not_dvd_of_pos_of_lt j.succ_pos h0)],
+    exact h1 (lt_trans j.lt_succ_self h0) }
+end
 
 end IMO2012N3
 end IMOSL
