fe9767b2fea3ae58a0150eecf24ed2f67aaa2f6b
Gian Sanjaya <43656481+mortarsanjaya@users.noreply.github.com>
2022-12-26 19:45:24 -0500

    2018 A5: Update proofs

--- src/IMO2018/A5/A5.lean
+++ src/IMO2018/A5/A5.lean
@@ -1,4 +1,4 @@
-import algebra.order.positive.field algebra.module.basic algebra.module.submodule.basic
+import algebra.order.positive.field algebra.module.submodule.basic
 
 /-! # IMO 2018 A5 -/
 
@@ -16,22 +16,77 @@ private def good_space (F V : Type*) [linear_ordered_field F] [add_comm_group V]
   submodule F ({x : F // 0 < x} → V) :=
 { carrier := set_of good,
   add_mem' := λ f g h h0 x y,
-    by simp_rw [pi.add_apply, smul_add, h x y, h0 x y, add_add_add_comm _ (g (x * y))],
-  zero_mem' := λ x y, by simp_rw [pi.zero_apply, smul_zero, zero_add],
+    by simp_rw [pi.add_apply, smul_add]; rw [h x y, h0 x y, add_add_add_comm],
+  zero_mem' := λ x y, by simp_rw pi.zero_apply; rw [smul_zero, zero_add],
   smul_mem' := λ c f h x y, by simp_rw [pi.smul_apply, smul_comm _ c, h x, ← smul_add] }
 
 private lemma smul_vec_is_good (v : V) :
-  (λ x : {x : F // 0 < x}, (x : F) • v) ∈ good_space F V :=
+  good (λ x : {x : F // 0 < x}, (x : F) • v) :=
   λ x y, by simp_rw [positive.coe_mul, positive.coe_inv, ← smul_smul, ← add_smul]
 
 private lemma inv_smul_vec_is_good (v : V) :
-    (λ x : {x : F // 0 < x}, (x⁻¹ : F) • v) ∈ good_space F V :=
+  good (λ x : {x : F // 0 < x}, (x⁻¹ : F) • v) :=
   λ x y, by simp_rw [positive.coe_mul, mul_inv, positive.coe_inv, inv_inv, ← smul_smul];
     rw [← add_smul, add_comm (x : F)]
     
-private lemma smul_vec_add_inv_smul_vec_is_good (v1 v2 : V) :
-    good (λ x : {x : F // 0 < x}, (x : F) • v1 + (x⁻¹ : F) • v2) :=
+private lemma smul_add_inv_smul_is_good (v1 v2 : V) :
+  good (λ x : {x : F // 0 < x}, (x : F) • v1 + (x⁻¹ : F) • v2) :=
   (good_space F V).add_mem (smul_vec_is_good v1) (inv_smul_vec_is_good v2)
+
+private lemma exists_smul_add_inv_smul_two_eq {a b : {x : F // 0 < x}} (h : a ≠ b) (v1 v2 : V) :
+ ∃ w1 w2 : V, (a : F) • w1 + (a : F)⁻¹ • w2 = v1 ∧ (b : F) • w1 + (b : F)⁻¹ • w2 = v2 :=
+begin
+  set S := {x : F // 0 < x},
+  let c : F := a * b⁻¹ - b * a⁻¹,
+  rsuffices ⟨h0, h1⟩ : c ≠ 0 ∧ ∃ w1 w2 : V,
+    (a : F) • w1 + (a : F)⁻¹ • w2 = c • v1 ∧ (b : F) • w1 + (b : F)⁻¹ • w2 = c • v2,
+  { rcases h1 with ⟨w1, w2, h1, h2⟩,
+    refine ⟨c⁻¹ • w1, c⁻¹ • w2, _, _⟩;
+      simp_rw [smul_smul, mul_comm _ c⁻¹, ← smul_smul];
+      rwa [← smul_add, inv_smul_eq_iff₀ h0] },
+  dsimp only [c]; clear c; split,
+
+  ---- `ab⁻¹ ≠ ba⁻¹`
+  { simp_rw [← positive.coe_inv, ← positive.coe_mul],
+    rw [ne.def, sub_eq_zero, set_coe.ext_iff, ← mul_inv_eq_one, mul_inv_rev, inv_inv, ← sq],
+    revert h; rw [ne.def, ← mul_inv_eq_one, not_imp_not],
+    exact (pow_eq_one_iff two_ne_zero).mp },
+
+  ---- `f` good with `f(a) = (ab⁻¹ - ba⁻¹) v₁` and `f(b) = (ab⁻¹ - ba⁻¹) v₂`
+  { refine ⟨(b : F)⁻¹ • v1 - (a : F)⁻¹ • v2, (a : F) • v2 - (b : F) • v1, _⟩,
+    replace h : (a : F) ≠ 0 := ne_of_gt a.2,
+    generalize_hyp : (a : F) = x at h ⊢,
+    have h0 : (b : F) ≠ 0 := ne_of_gt b.2,
+    generalize_hyp : (b : F) = y at h0 ⊢,
+    clear a b S; simp_rw [smul_sub, smul_smul]; split,
+    rw [mul_inv_cancel h, inv_mul_cancel h, sub_add_sub_cancel, ← sub_smul, mul_comm y],
+    rw [mul_inv_cancel h0, inv_mul_cancel h0, sub_add_sub_cancel', ← sub_smul, mul_comm x] }
+end
+
+private lemma good_two_eq_zero {f : {x : F // 0 < x} → V} (h : good f)
+  {a b : {x : F // 0 < x}} (h0 : a ≠ b) (h1 : f a = 0) (h2 : f b = 0) :
+  f = 0 :=
+begin
+  have h3 : ∀ u : {x : F // 0 < x}, f u = 0 → ∀ x : {x : F // 0 < x}, f (u ^ 2 * x) = -f x⁻¹ :=
+    λ u h3 x, by replace h := h (u * x) u; rwa [h3, smul_zero, mul_inv_rev,
+      inv_mul_cancel_right, mul_right_comm, ← sq, eq_comm, add_eq_zero_iff_eq_neg] at h,
+  replace h3 : ∀ x : {x : F // 0 < x}, f ((a * b⁻¹) ^ 2 * x) = f x :=
+    λ x, by rw [mul_pow, mul_assoc, h3 a h1, ← h3 b h2, inv_pow, mul_inv_cancel_left],
+  rw [ne.def, ← mul_inv_eq_one] at h0,
+  replace h0 : (a * b⁻¹) ^ 2 ≠ 1 := λ h4, h0 ((pow_eq_one_iff two_ne_zero).mp h4),
+  clear h1 h2; generalize_hyp : (a * b⁻¹) ^ 2 = c at h0 h3,
+
+  funext x; replace h := h c (c * x),
+  rw [h3 (c * x), h3, inv_mul_cancel_left, ← sub_eq_zero,
+      ← two_smul F, ← sub_smul, smul_eq_zero, sub_eq_zero] at h,
+  revert h; clear h3; refine (or_iff_right (λ h3, _)).mp,
+  apply_fun has_mul.mul (c : F) at h3,
+  have h1 : 0 < (c : F) := c.2,
+  rw [mul_add, mul_inv_cancel (ne_of_gt h1), mul_two, ← eq_sub_iff_add_eq,
+      add_sub_assoc, ← sub_eq_iff_eq_add', ← mul_sub_one, mul_left_eq_self₀,
+      sub_eq_zero, or_self, ← positive.coe_one, subtype.coe_inj] at h3,
+  exact h0 h3
+end
 
 
 
@@ -39,46 +94,15 @@ private lemma smul_vec_add_inv_smul_vec_is_good (v1 v2 : V) :
 theorem final_solution {F V : Type*} [linear_ordered_field F] [add_comm_group V] [module F V] :
   ∀ f : {x : F // 0 < x} → V, good f ↔ ∃ v1 v2 : V, f = λ x, (x : F) • v1 + (x⁻¹ : F) • v2 :=
 begin
-  ---- First solve the `←` direction.
   intros f; symmetry; refine ⟨λ h, _, λ h, _⟩,
   rcases h with ⟨v1, v2, rfl⟩,
-  exact smul_vec_add_inv_smul_vec_is_good v1 v2,
-
-  ---- Now reduce to showing that, if `f(1) = f(2) = 0`, then `f = 0`
-  use [-(1 / 3 : F) • f 1 + (2 / 3 : F) • f 2, ((2 + 2) / 3 : F) • f 1 + -(2 / 3 : F) • f 2],
-  rw ← sub_eq_zero; revert h f,
-  suffices : ∀ f : {x : F // 0 < x} → V, good f → (f 1 = 0 ∧ f 2 = 0) → f = 0,
-  { intros f h,
-    refine this _ ((good_space F V).sub_mem h (smul_vec_add_inv_smul_vec_is_good _ _)) _,
-    set g := λ x : {x : F // 0 < x}, (x : F) • (-(1 / 3 : F) • f 1 + (2 / 3 : F) • f 2) +
-      (x : F)⁻¹ • (((2 + 2) / 3 : F) • f 1 + (-(2 / 3 : F) • f 2)) with hg,
-    simp_rw [smul_add, smul_smul, add_add_add_comm _ (_ • f 2), ← add_smul, mul_neg,
-      add_comm (- (_ * (1 / 3 : F))), ← sub_eq_add_neg, ← mul_div_assoc, ← sub_div, mul_one] at hg,
-    have h0 : (2 + 2 - 1 : F) / 3 = 1 := by norm_num,
-    simp_rw [hg, pi.sub_apply, sub_eq_zero]; split,
-    rw [positive.coe_one, inv_one, sub_self, zero_div, zero_smul, add_zero, one_mul, h0, one_smul],
-    change ((2 : {x : F // 0 < x}) : F) with (2 : F),
-    rw [← two_mul, ← mul_assoc, inv_mul_cancel (two_ne_zero : (2 : F) ≠ 0), one_mul,
-        sub_self, zero_div, zero_smul, zero_add, two_mul, h0, one_smul] },
-
-  ---- Finally, show that, if `f(1) = f(2) = 0`, then `f = 0`
-  rintros f h h0,
-  replace h0 : ∀ t : {x : F // 0 < x}, f (2 * 2 * t) = f t :=
-  begin
-    intros t,
-    have h2 := h (2 * t) 2,
-    rw [h0.2, smul_zero, mul_inv_rev, inv_mul_cancel_right, mul_right_comm] at h2,
-    replace h := h t 1,
-    rwa [h0.1, smul_zero, mul_one, mul_one, h2, add_left_inj] at h
-  end,
-
-  funext t; replace h := h (2 * 2) t,
-  rw [h0, ← h0 ((2 * 2)⁻¹ * t), mul_inv_cancel_left, ← sub_eq_zero,
-      ← two_smul F, ← sub_smul, smul_eq_zero, positive.coe_mul, sub_eq_zero] at h,
-  revert h; refine (or_iff_right (ne_of_gt _)).mp,
-  rw ← sq; refine lt_add_of_lt_of_nonneg _ (inv_nonneg.mpr (sq_nonneg _)),
-  change (2 : F) < (2 : F) ^ 2,
-  norm_num
+  exact smul_add_inv_smul_is_good v1 v2,
+  obtain ⟨a, b, h0⟩ : ∃ a b : {x : F // 0 < x}, a ≠ b :=
+    ⟨⟨2, two_pos⟩, ⟨1, one_pos⟩, ne_of_gt (by rw subtype.mk_lt_mk; exact one_lt_two)⟩,
+  obtain ⟨v1, v2, h1, h2⟩ := exists_smul_add_inv_smul_two_eq h0 (f a) (f b),
+  rw [eq_comm, ← sub_eq_zero] at h1 h2,
+  refine ⟨v1, v2, sub_eq_zero.mp (good_two_eq_zero _ h0 h1 h2)⟩,
+  exact (good_space F V).sub_mem h (smul_add_inv_smul_is_good v1 v2)
 end
 
 end IMO2018A5
