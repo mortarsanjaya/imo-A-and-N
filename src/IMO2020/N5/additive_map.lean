aef118052558915b88c9eb297d9407eab437cad0
Gian Sanjaya <43656481+mortarsanjaya@users.noreply.github.com>
2023-01-05 11:09:50 -0500

    2020 N5: Optimize

--- src/IMO2020/N5/additive_map.lean
+++ src/IMO2020/N5/additive_map.lean
@@ -45,7 +45,7 @@ instance : has_coe_to_fun (additive_map M) (λ _, ℕ → M) := ⟨additive_map.
 instance fun_like {M : out_param Type*} [has_zero M] [has_add M] :
   fun_like (additive_map M) ℕ (λ _, M) :=
 { coe := λ f, f.to_fun,
-  coe_injective' := λ f g h, by cases f; cases g; congr' }
+  coe_injective' := λ f g h, by cases f; cases g; simp only []; exact h }
 
 theorem coe_inj {f g : additive_map M} : (f : ℕ → M) = g ↔ f = g :=
 ⟨(λ h, fun_like.coe_injective h), (λ h, by rw h)⟩
@@ -57,13 +57,13 @@ theorem ext_iff {f g : additive_map M} : f = g ↔ ∀ x, f x = g x := ⟨λ h x
 theorem ext_iff_primes {f g : additive_map M} : f = g ↔ ∀ p : ℕ, p.prime → f p = g p :=
 begin
   refine ⟨λ h _ _, by rw h, λ h, ext (nat.rec_on_mul _ _ h (λ a b ha hb, _))⟩,
-  rw [additive_map.map_zero, additive_map.map_zero],
-  rw [additive_map.map_one, additive_map.map_one],
+  rw [map_zero, map_zero],
+  rw [map_one, map_one],
   rcases eq_or_ne a 0 with rfl | ha0,
   rw [zero_mul, ha],
   rcases eq_or_ne b 0 with rfl | hb0,
   rw [mul_zero, hb],
-  rw [additive_map.map_mul f ha0 hb0, additive_map.map_mul g ha0 hb0, ha, hb]
+  rw [f.map_mul ha0 hb0, g.map_mul ha0 hb0, ha, hb]
 end
 
 end basic
@@ -82,26 +82,22 @@ instance : has_add (additive_map M) :=
 ⟨λ f g, ⟨(λ n, f n + g n),
   by rw [map_zero, map_zero, add_zero],
   by rw [map_one, map_one, add_zero],
-  λ x y hx hy, by rw [map_mul f hx hy, map_mul g hx hy, add_add_add_comm]⟩⟩
+  λ x y hx hy, by rw [f.map_mul hx hy, g.map_mul hx hy, add_add_add_comm]⟩⟩
 
 @[simp] theorem add_apply {f g : additive_map M} {n : ℕ} : (f + g) n = f n + g n := rfl
 
 instance : add_comm_monoid (additive_map M) :=
-{ add_comm := λ f g, by ext; simp only [add_apply, add_comm],
-  add_assoc := λ f g h, by ext; simp only [add_apply, add_assoc],
+{ add_comm := λ f g, by ext; rw [add_apply, add_apply, add_comm],
+  add_assoc := λ f g h, by ext; iterate 4 { rw add_apply }; rw add_assoc,
   zero_add := λ f, by ext; rw [add_apply, zero_apply, zero_add],
   add_zero := λ f, by ext; rw [add_apply, zero_apply, add_zero],
   .. additive_map.has_zero, .. additive_map.has_add }
 
-@[simp] theorem map_pow (f : additive_map M) (x n : ℕ) :
-  f (x ^ n) = n • f x :=
-begin
-  induction n with n n_ih,
-  rw [pow_zero, zero_smul, map_one],
-  by_cases hx : x = 0,
-  rw [hx, pow_succ, zero_mul, map_zero, smul_zero],
-  rw [pow_succ', map_mul f (pow_ne_zero _ hx) hx, nat.succ_eq_add_one, n_ih, add_smul, one_smul]
-end
+@[simp] theorem map_pow (f : additive_map M) : ∀ x n : ℕ, f (x ^ n) = n • f x
+| _ 0 := by rw [pow_zero, zero_nsmul, map_one]
+| 0 (n+1) := by rw [zero_pow n.succ_pos, map_zero, nsmul_zero]
+| (x+1) (n+1) := by rw [pow_succ', succ_nsmul', ← map_pow,
+  f.map_mul (pow_ne_zero _ x.succ_ne_zero) x.succ_ne_zero]
 
 theorem map_prod_finset (f : additive_map M) {α : Type*} [decidable_eq α] {S : finset α} {g : α → ℕ}
   (hS : ∀ x, x ∈ S → g x ≠ 0) : f (S.prod g) = S.sum (λ x, f (g x)) :=
@@ -114,26 +110,25 @@ begin
   rw finset.prod_ne_zero_iff; exact hS.2
 end
 
-theorem map_factorization (f : additive_map M) (n : ℕ) :
-  f n = n.factorization.sum (λ p k, k • f p) :=
+theorem map_factorization (f : additive_map M) :
+  ∀ n : ℕ, f n = n.factorization.sum (λ p k, k • f p) :=
 begin
-  revert n; refine nat.rec_on_mul _ _ (λ p hp, _) (λ a b ha hb, _),
-  rw [nat.factorization_zero, finsupp.sum_zero_index, additive_map.map_zero],
-  rw [nat.factorization_one, finsupp.sum_zero_index, additive_map.map_one],
+  refine nat.rec_on_mul _ _ (λ p hp, _) (λ a b ha hb, _),
+  rw [nat.factorization_zero, finsupp.sum_zero_index, map_zero],
+  rw [nat.factorization_one, finsupp.sum_zero_index, map_one],
   rw [hp.factorization, finsupp.sum_single_index, one_nsmul],
-  rw zero_smul,
+  rw zero_nsmul,
   rcases eq_or_ne a 0 with rfl | ha0,
   rw [zero_mul, ha],
   rcases eq_or_ne b 0 with rfl | hb0,
   rw [mul_zero, hb],
-  rw [additive_map.map_mul f ha0 hb0, nat.factorization_mul ha0 hb0,
-      finsupp.sum_add_index, ← ha, ← hb],
-  rintros c -; rw zero_smul,
-  rintros c - d1 d2; rw add_smul
+  rw [f.map_mul ha0 hb0, nat.factorization_mul ha0 hb0, finsupp.sum_add_index, ← ha, ← hb],
+  rintros c -; exact zero_nsmul (f c),
+  rintros c - d1 d2; rw add_nsmul
 end
 
 theorem zero_iff_primes {f : additive_map M} : f = 0 ↔ ∀ p : ℕ, p.prime → f p = 0 :=
-  by rw ext_iff_primes; simp only [additive_map.zero_apply]
+  by rw ext_iff_primes; simp_rw additive_map.zero_apply
 
 end add_comm_monoid
 
@@ -147,16 +142,16 @@ instance : has_smul α (additive_map M) :=
 ⟨λ a f, ⟨λ x, a • f x,
   by rw [map_zero, smul_zero],
   by rw [map_one, smul_zero],
-  λ x y hx hy, by rw [map_mul f hx hy, smul_add]⟩⟩
+  λ x y hx hy, by rw [f.map_mul hx hy, smul_add]⟩⟩
 
 @[simp] theorem smul_def {a : α} {f : additive_map M} {x : ℕ} : (a • f) x = a • f x := rfl
 
 instance : mul_action α (additive_map M) :=
 { one_smul := λ f, by ext; rw [smul_def, one_smul],
-  mul_smul := λ x y f, by ext; simp only [smul_def]; rw mul_smul }
+  mul_smul := λ x y f, by ext; iterate 3 { rw smul_def }; rw mul_smul }
 
 instance : distrib_mul_action α (additive_map M) :=
-{ smul_add := λ x f g, by ext; simp only [smul_def, add_apply]; rw smul_add,
+{ smul_add := λ x f g, by ext; rw [smul_def, add_apply, add_apply, smul_add, smul_def, smul_def],
   smul_zero := λ x, by ext; rw [smul_def, zero_apply, smul_zero] }
 
 end mul_action
@@ -171,15 +166,15 @@ instance : has_neg (additive_map G) :=
 ⟨λ f, ⟨(λ n, -(f n)),
   by rw [map_zero, neg_zero],
   by rw [map_one, neg_zero],
-  λ x y hx hy, by rw [map_mul f hx hy, neg_add]⟩⟩
+  λ x y hx hy, by rw [f.map_mul hx hy, neg_add]⟩⟩
 
 @[simp] theorem neg_apply {f : additive_map G} {n : ℕ} : (-f) n = -(f n) := rfl
 
 instance : has_sub (additive_map G) :=
 ⟨λ f g, ⟨(λ n, f n - g n),
-  by simp only [map_zero, sub_zero],
-  by simp only [map_one, sub_zero],
-  λ x y hx hy, by rw [map_mul f hx hy, map_mul g hx hy, add_sub_add_comm]⟩⟩
+  by rw [map_zero, map_zero, sub_zero],
+  by rw [map_one, map_one, sub_zero],
+  λ x y hx hy, by rw [f.map_mul hx hy, g.map_mul hx hy, add_sub_add_comm]⟩⟩
 
 @[simp] theorem sub_apply {f g : additive_map G} {n : ℕ} : (f - g) n = f n - g n := rfl
 
