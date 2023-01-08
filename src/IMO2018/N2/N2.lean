1079eb5edd6f3a97211f6f40bf3daec219a3bd56
Gian Sanjaya <43656481+mortarsanjaya@users.noreply.github.com>
2023-01-08 08:30:00 -0500

    2020 N2: Generalize

--- src/IMO2018/N2/N2.lean
+++ src/IMO2018/N2/N2.lean
@@ -1,4 +1,4 @@
-import data.int.modeq algebra.big_operators.fin
+import data.int.modeq algebra.big_operators.basic
 
 /-! # IMO 2018 N2 -/
 
@@ -7,53 +7,59 @@ namespace IMO2018N2
 
 open finset
 
-private lemma prod_one_mod_eq_one_mod {n : ℤ} {k : ℕ}
-    {f : fin k → ℤ} (h : ∀ i : fin k, f i ≡ 1 [ZMOD n]) :
-  univ.prod f ≡ 1 [ZMOD n] :=
+private lemma add_modeq_mul_add_one {n a b : ℤ} (h : a ≡ 1 [ZMOD n]) (h0 : b ≡ 1 [ZMOD n]) :
+  a + b ≡ a * b + 1 [ZMOD n ^ 2] :=
+  by { rw [int.modeq_iff_dvd, sq, add_comm, ← sub_sub_sub_eq, ← one_sub_mul, ← mul_one_sub],
+    exact mul_dvd_mul h.dvd h0.dvd }
+
+
+variables {ι : Type*} [decidable_eq ι] {s : finset ι}
+
+private lemma prod_one_modeq_one_mod {n : ℤ} {f : ι → ℤ} (h : ∀ i ∈ s, f i ≡ 1 [ZMOD n]) :
+  s.prod f ≡ 1 [ZMOD n] :=
 begin
-  induction k with k k_ih,
-  rw fin.prod_univ_zero,
-  rw [fin.prod_univ_cast_succ, ← mul_one (1 : ℤ)],
-  exact int.modeq.mul (k_ih (λ i, h (fin.cast_succ i))) (h _)
+  induction s using finset.induction with i s h0 h1, refl,
+  rw [prod_insert h0, ← int.mul_one 1],
+  exact (h i $ mem_insert_self i s).mul (h1 $ λ j h2, h j $ mem_insert_of_mem h2)
 end
 
-private lemma prod_one_mod_add_size_eq_sum_add_one_mod_sq {n : ℤ} {k : ℕ}
-    {f : fin k → ℤ} (h : ∀ i : fin k, f i ≡ 1 [ZMOD n]) :
-  univ.prod f + k ≡ univ.sum f + 1 [ZMOD n^2] :=
+private lemma prod_one_mod_add_card_modeq_sum_add_one
+  {n : ℤ} {f : ι → ℤ} (h : ∀ i ∈ s, f i ≡ 1 [ZMOD n]) :
+  s.prod f + s.card ≡ s.sum f + 1 [ZMOD n ^ 2] :=
 begin
-  induction k with k k_ih,
-  rw [fin.prod_univ_zero, fin.sum_univ_zero, nat.cast_zero, add_comm],
-  rw [fin.prod_univ_cast_succ, fin.sum_univ_cast_succ, nat.cast_succ, add_right_comm],
-  have h0 : ∀ i : fin k, f (fin.cast_succ i) ≡ 1 [ZMOD n] := λ i, h (fin.cast_succ i),
-  refine int.modeq.trans _ (int.modeq.add_right _ (k_ih h0)),
-  replace h0 := prod_one_mod_eq_one_mod h0,
-  replace h := h (fin.last k),
-  generalize_hyp : univ.prod (λ (i : fin k), f (fin.cast_succ i)) = X at h0 ⊢,
-  generalize_hyp : f (fin.last k) = Y at h ⊢,
-  rw [int.modeq_iff_dvd, ← neg_sub, dvd_neg] at h h0 ⊢,
-  rw [add_right_comm, ← add_assoc, add_right_comm, add_sub_add_right_eq_sub, ← sub_sub,
-      add_sub_right_comm, ← mul_sub_one, add_sub_right_comm, sub_add, ← sub_one_mul, sq],
-  exact mul_dvd_mul h0 h
+  induction s using finset.induction with i s h0 h1, refl,
+  rw [prod_insert h0, sum_insert h0, card_insert_of_not_mem h0,
+      nat.cast_succ, ← add_assoc, add_right_comm],
+  have h2 : ∀ j : ι, j ∈ s → f j ≡ 1 [ZMOD n] := λ j h2, h j (mem_insert_of_mem h2),
+  refine ((add_modeq_mul_add_one (h i $ mem_insert_self i s)
+    (prod_one_modeq_one_mod h2)).symm.add_right _).trans _,
+  rw [add_assoc, add_assoc]; exact (h1 h2).add_left _
 end
+
+private lemma prod_one_mod_modeq_one_mod_sq
+  {n : ℤ} {f : ι → ℤ} (h : ∀ i ∈ s, f i ≡ 1 [ZMOD n]) (h0 : s.sum f ≡ s.card [ZMOD n ^ 2]) :
+  s.prod f ≡ 1 [ZMOD n ^ 2] :=
+  ((prod_one_mod_add_card_modeq_sum_add_one h).trans
+    (by rw add_comm; exact h0.add_left 1)).add_right_cancel' s.card
+
+
+
+
 
 
 
 /-- Final solution -/
-theorem final_solution {n : ℕ} {f : fin n → fin n → ℤ} (h : ∀ i j : fin n, f i j ≡ 1 [ZMOD n])
-    (h0 : ∀ i : fin n, univ.sum (f i) ≡ n [ZMOD n^2])
-    (h1 : ∀ j : fin n, univ.sum (λ i, f i j) ≡ n [ZMOD n^2]) :
-  univ.sum (λ i, univ.prod (f i)) ≡ univ.sum (λ j, univ.prod (λ i, f i j)) [ZMOD n^4] :=
+theorem final_solution {n : ℤ} {f : ι → ι → ℤ}
+  (h : ∀ (i ∈ s) (j ∈ s), f i j ≡ 1 [ZMOD n])
+  (h0 : ∀ i ∈ s, s.sum (f i) ≡ s.card [ZMOD n ^ 2])
+  (h1 : ∀ j ∈ s, s.sum (λ i, f i j) ≡ s.card [ZMOD n ^ 2]) :
+  s.sum (λ i, s.prod (f i)) ≡ s.sum (λ j, s.prod (λ i, f i j)) [ZMOD n ^ 4] :=
 begin
-  replace h0 : ∀ i : fin n, univ.prod (f i) ≡ 1 [ZMOD n^2] :=
-    λ i, int.modeq.add_left_cancel (h0 i).symm (by rw add_comm;
-      exact prod_one_mod_add_size_eq_sum_add_one_mod_sq (h i)),
-  replace h1 : ∀ j : fin n, univ.prod (λ i, f i j) ≡ 1 [ZMOD n^2] :=
-    λ j, int.modeq.add_left_cancel (h1 j).symm (by rw add_comm;
-      exact prod_one_mod_add_size_eq_sum_add_one_mod_sq (λ i, h i j)),
-  rw [bit0, ← mul_two, pow_mul],
-  apply int.modeq.add_right_cancel' 1,
-  refine int.modeq.trans (prod_one_mod_add_size_eq_sum_add_one_mod_sq h0).symm _,
-  rw finset.prod_comm; exact prod_one_mod_add_size_eq_sum_add_one_mod_sq h1
+  rw [bit0, ← two_mul, pow_mul],
+  refine ((prod_one_mod_add_card_modeq_sum_add_one _).symm.trans _).add_right_cancel' 1,
+  exact (λ i h2, prod_one_mod_modeq_one_mod_sq (h i h2) (h0 i h2)),
+  rw prod_comm; apply prod_one_mod_add_card_modeq_sum_add_one,
+  exact (λ j h2, prod_one_mod_modeq_one_mod_sq (λ i h3, h i h3 j h2) (h1 j h2))
 end
 
 end IMO2018N2
