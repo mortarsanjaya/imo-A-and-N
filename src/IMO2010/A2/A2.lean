8afabbd5cbed24709945d851cffc11ba777ff504
Gian Sanjaya <43656481+mortarsanjaya@users.noreply.github.com>
2023-01-08 12:56:51 -0500

    2010 A2: Simplify code

--- src/IMO2010/A2/A2.lean
+++ src/IMO2010/A2/A2.lean
@@ -7,8 +7,6 @@ namespace IMO2010A2
 
 open finset
 
-
-
 section comm_ring_ineq
 
 variables {R : Type*} [linear_ordered_comm_ring R]
@@ -39,11 +37,13 @@ begin
   rw [sum_insert s_ih1, sum_insert s_ih1, card_insert_of_not_mem s_ih1, succ_nsmul,
       add_sq, add_assoc, add_assoc, add_le_add_iff_left, nsmul_add, ← add_assoc,
       ← sum_const, ← sum_add_distrib, mul_sum],
-  refine add_le_add (sum_le_sum (λ i junk, _)) s_ih2; clear s_ih1 s_ih2 junk,
+  refine add_le_add (sum_le_sum (λ i _, _)) s_ih2,
   rw mul_right_comm; exact two_mul_le_add_sq (x i) (x j)
 end
 
 end comm_ring_ineq
+
+
 
 
 
@@ -76,6 +76,8 @@ begin
   rw [div_le_iff' h, ← nsmul_eq_mul]; exact QM_AM s _
 end
 
+
+
 /-- Final solution, general version -/
 theorem final_solution_general {ι : Type*} [decidable_eq ι] {s : finset ι} {a : ι → F}
     {σ₁ σ₂ : F} (h : s.sum a = σ₁) (h0 : s.sum (λ i, a i ^ 2) = σ₂) :
@@ -87,14 +89,7 @@ final_solution_general' h h0 rfl rfl rfl
 /-- Final solution -/
 theorem final_solution {a : fin 4 → F} (h : univ.sum a = 6) (h0 : univ.sum (λ i, a i ^ 2) = 12) :
   let S := 4 * univ.sum (λ i, a i ^ 3) - univ.sum (λ i, a i ^ 4) in 36 ≤ S ∧ S ≤ 48 :=
-begin
-  intros S,
-  have h1 : (36 : F) = 52 - 16 := by norm_num,
-  have h2 : (48 : F) = 52 - 16 / (univ : finset (fin 4)).card := by norm_num,
-  have h3 : (52 : F) = 6 * 12 - 4 * 6 + (univ : finset (fin 4)).card := by norm_num,
-  have h4 : (16 : F) = (12 - 2 * 6 + (univ : finset (fin 4)).card) ^ 2 := by norm_num,
-  rw [h1, h2, h3, h4]; exact final_solution_general h h0
-end
+  by intros S; convert final_solution_general h h0; norm_num
 
 end IMO2010A2
 end IMOSL
