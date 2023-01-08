d38cec1e5c0f0f1056257d5ef55386b976359ecf
Gian Sanjaya <43656481+mortarsanjaya@users.noreply.github.com>
2023-01-08 13:44:16 -0500

    2012 N1: Optimization

--- src/IMO2012/N1/N1.lean
+++ src/IMO2012/N1/N1.lean
@@ -5,7 +5,8 @@ import ring_theory.coprime.lemmas ring_theory.ideal.basic
 namespace IMOSL
 namespace IMO2012N1
 
-def admissible {R : Type*} [semiring R] (A : set R) := ∀ x y : R, x ∈ A → y ∈ A → ∀ r : R, x ^ 2 + r * x * y + y ^ 2 ∈ A
+def admissible {R : Type*} [semiring R] (A : set R) :=
+  ∀ x y : R, x ∈ A → y ∈ A → ∀ r : R, x ^ 2 + r * x * y + y ^ 2 ∈ A
 
 
 
@@ -19,10 +20,11 @@ begin
   { have h0 : ∀ I : ideal R, admissible (I : set R) :=
       λ I u v h h0 r, by rw [sq, mul_right_comm, ← add_mul, sq];
         exact I.add_mem (I.mul_mem_left _ h) (I.mul_mem_left _ h0),
-    replace h := h _ (h0 (ideal.span {x, y})) (ideal.subset_span (set.mem_insert x _))
-      (ideal.subset_span (set.mem_insert_of_mem _ (set.mem_singleton y))) 1,
-    simp_rw [set_like.mem_coe, ideal.mem_span_insert, ideal.mem_span_singleton] at h,
-    clear h0; rcases h with ⟨a, z, ⟨b, rfl⟩, h⟩,
+    replace h := h _ (h0 $ ideal.span {x, y}) (ideal.subset_span $ set.mem_insert x _)
+      (ideal.subset_span $ set.mem_insert_of_mem _ $ set.mem_singleton y) 1,
+    rw [set_like.mem_coe, ideal.mem_span_insert] at h,
+    clear h0; rcases h with ⟨a, z, h0, h⟩,
+    rw ideal.mem_span_singleton at h0; rcases h0 with ⟨b, rfl⟩,
     rw mul_comm y at h; exact ⟨a, b, h.symm⟩ },
 
   ---- If `x` and `y` are coprime, the only admissible set `x` and `y` is `R`
@@ -31,13 +33,11 @@ begin
         rwa [mul_assoc, ← sq, ← one_add_mul, add_comm (1 : R),
              ← add_one_mul, add_assoc, sub_add_cancel] at h3,
     suffices : (1 : R) ∈ A,
-      intros x; convert h3 1 this x; rw [one_pow, mul_one],
+      intros x; rw [← mul_one x, ← one_pow 2]; exact h3 1 this x,
     replace h : is_coprime (x ^ 2) (y ^ 2) := is_coprime.pow h,
     rcases h with ⟨a, b, h⟩,
-    replace h1 := h3 x h1 a,
-    replace h2 := h3 y h2 b,
-    replace h0 := h0 _ _ h1 h2 2,
-    clear h1 h2 h3; rwa [← add_sq, h, one_pow] at h0 }
+    replace h0 := h0 _ _ (h3 x h1 a) (h3 y h2 b) 2,
+    rwa [← add_sq, h, one_pow] at h0 }
 end 
 
 end IMO2012N1
