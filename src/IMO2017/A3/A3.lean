c42aac15f6fe82dd97bd549655a9ef090e7cd2b1
Gian Sanjaya <43656481+mortarsanjaya@users.noreply.github.com>
2023-04-20 21:58:58 -0400

    2017 A3: Update

--- src/IMO2017/A3/A3.lean
+++ src/IMO2017/A3/A3.lean
@@ -1,53 +1,70 @@
-import data.fintype.basic data.set.finite
+import data.fintype.card data.set.finite
 
 /-! # IMO 2017 A3 -/
 
 namespace IMOSL
 namespace IMO2017A3
 
-open function set
+section finite_npow
+
+variables {M : Type*} [has_pow M ℕ] [fintype M] (x : M)
+
+private lemma npow_not_inj_of_finite : ∃ a b : ℕ, a ≠ b ∧ x ^ a = x ^ b := 
+  finite.exists_ne_map_eq_of_infinite (λ n : ℕ, x ^ n)
+
+private lemma npow_not_inj_of_finite2 : ∃ a b : ℕ, a < b ∧ x ^ a = x ^ b :=
+  by rcases npow_not_inj_of_finite x with ⟨a, b, h, h0⟩;
+    exact (lt_or_gt_of_ne h).elim (λ h, ⟨a, b, h, h0⟩) (λ h, ⟨b, a, h, h0.symm⟩)
+
+private lemma npow_not_inj_of_finite3 : ∃ n k : ℕ, 0 < k ∧ x ^ (n + k) = x ^ n :=
+begin
+  rcases npow_not_inj_of_finite2 x with ⟨a, b, h, h0⟩,
+  rw lt_iff_exists_add at h; rcases h with ⟨k, h, rfl⟩,
+  exact ⟨a, k, h, h0.symm⟩
+end
+
+end finite_npow
+
+
+section monoid_npow
+
+variables {M : Type*} [monoid M] {x : M} {n k : ℕ} (h : x ^ (n + k) = x ^ n)
+include h
+
+private lemma npow_mul_add_eq_of_npow_add_eq : ∀ t : ℕ, x ^ (n + t * k) = x ^ n
+| 0 := congr_arg (λ c, x ^ c) $ (congr_arg (has_add.add n) (zero_mul k)).trans $ add_zero n
+| (t+1) := by rw [add_one_mul, ← add_assoc, pow_add, npow_mul_add_eq_of_npow_add_eq, ← pow_add, h]
+
+private lemma npow_add_eq_of_npow_add_eq_of_le {n0 : ℕ} (h0 : n ≤ n0) : x ^ (n0 + k) = x ^ n0 :=
+  by rw le_iff_exists_add at h0; rcases h0 with ⟨c, rfl⟩; rw [add_right_comm, pow_add, h, pow_add]
+
+end monoid_npow
+
+
 
 /-- Final solution, monoid version (the claim) -/
 theorem final_solution_monoid {M : Type*} [monoid M] [fintype M]
   {x : M} (h : ∀ y : M, x * y * x = y * x * y → y = x) :
   ∃ m : ℕ, 1 < m ∧ x ^ m = x :=
 begin
+  ---- Take some `k > 0` and `n ≥ 0` such that `x^{n + k} = x^n`.
+  obtain ⟨n, k, h0, h1⟩ := npow_not_inj_of_finite3 x,
 
-  ---- get some `k > n` such that `x ^ k = x ^ n`
-  obtain ⟨n, k, h0, h1⟩ : ∃ n k : ℕ, n < k ∧ x ^ n = x ^ k :=
-  begin
-    have h0 := not_injective_infinite_finite (λ n : ℕ, x ^ n),
-    simp_rw [injective, not_forall] at h0,
-    rcases h0 with ⟨n, k, h0, h1⟩,
-    rw [← ne.def, ne_iff_lt_or_gt, gt_iff_lt] at h1,
-    wlog h1 : n < k := h1 using [n k, k n],
-    exact ⟨n, k, h1, h0⟩
-  end,
-
-  ---- Now write `k = n + t` and get rid of the case `n = 0`
+  ---- Remove the case `n = 0`.
   rcases n.eq_zero_or_pos with rfl | h2,
-  refine ⟨k + 1, nat.succ_lt_succ h0, by rw [pow_succ, ← h1, pow_zero, mul_one]⟩,
-  rw lt_iff_exists_add at h0,
-  rcases h0 with ⟨t, h0, rfl⟩,
+  refine ⟨k + 1, nat.succ_lt_succ h0, _⟩,
+  rw zero_add at h1,
+  rw [pow_succ, h1, pow_zero, mul_one],
 
-  ---- Choose `m = nt + 1` and prove the equality
-  refine ⟨n * t + 1, nat.succ_lt_succ (mul_pos h2 h0), h _ _⟩; clear h2,
-  rw [pow_succ, ← mul_assoc (_ * x), ← pow_succ, ← pow_succ,
-      ← pow_succ', ← pow_succ', ← pow_succ', ← pow_add],
-  change n * t + 1 + 1 + 1 with n * t + 3,
-  replace h0 : n ≤ n * t + 3 := le_trans (nat.le_mul_of_pos_right h0) le_self_add,
-  generalize_hyp : n * t + 3 = d at h0 ⊢,
-  generalize_hyp : n = k,
+  ---- Claim that `m = nk + 1` works by reducing to `x^{2nk + 3} = x^{nk + 3}`.
+  refine ⟨n * k + 1, nat.succ_lt_succ (nat.mul_pos h2 h0), h _ _⟩,
 
-  ---- More generally, we have `x ^ d = x ^ (d + kt)` for all `d ≥ n` and `k ≥ 0` by induction
-  revert k; suffices : x ^ d = x ^ (d + t),
-  { intros k; induction k with k k_ih,
-    rw [zero_mul, add_zero],
-    rw [nat.succ_eq_one_add, one_add_mul, ← add_assoc, pow_add, ← this, ← pow_add, ← k_ih] },
-  revert h0 d; apply nat.le_induction,
-  exact h1,
-  rintros d - h0,
-  rw [add_right_comm, pow_succ, pow_succ, ← h0]
+  ---- Now prove that `x^{2nk + 3} = x^{nk + 3}`.
+  replace h := npow_add_eq_of_npow_add_eq_of_le
+    (npow_mul_add_eq_of_npow_add_eq h1 n) (nat.le_mul_of_pos_right h0),
+  generalize_hyp : n * k = d at h ⊢; clear h0 h1 h2 n k,
+  rw [mul_assoc, pow_mul_comm', mul_assoc, ← pow_succ, ← pow_add,
+      add_add_add_comm, pow_add x (d + d), h, ← pow_add]
 end
 
 
@@ -55,16 +72,16 @@ end
 /-- Final solution -/
 theorem final_solution {S : Type*} [fintype S] [decidable_eq S]
   {f : function.End S} (h : ∀ g : S → S, f * g * f = g * f * g → g = f) :
-  range (f^[2]) = range f :=
-begin
-  refine subset.antisymm (range_comp_subset_range f f) _,
-  rcases final_solution_monoid h with ⟨m, h1, h0⟩,
-  rw [← nat.succ_le_iff, le_iff_exists_add] at h1,
-  rcases h1 with ⟨c, rfl⟩,
-  nth_rewrite 0 ← h0; clear h0,
-  rw [pow_add, pow_succ, pow_one, iterate_succ, iterate_one],
-  exact range_comp_subset_range _ (f ∘ f)
-end
+  set.range (f ∘ f) = set.range f :=
+  (set.range_comp_subset_range f f).antisymm
+(begin
+  replace h := final_solution_monoid h,
+  rcases h with ⟨m, h, h0⟩,
+  nth_rewrite 0 ← h0,
+  rw [← nat.succ_le_iff, le_iff_exists_add] at h,
+  rcases h with ⟨c, rfl⟩,
+  rw pow_add; exact set.range_comp_subset_range _ (f ∘ f)
+end)
 
 end IMO2017A3
 end IMOSL
