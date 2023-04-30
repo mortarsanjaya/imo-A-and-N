c7a82bd045e6bd71eeafb571d6ed03e6c3d462b6
Gian Sanjaya <43656481+mortarsanjaya@users.noreply.github.com>
2023-04-30 18:06:51 -0400

    2010 A4: Small code change

--- src/IMO2010/A4/A4.lean
+++ src/IMO2010/A4/A4.lean
@@ -1,4 +1,4 @@
-import algebra.big_operators.basic data.nat.digits
+import data.nat.digits
 
 /-! # IMO 2010 A4 -/
 
@@ -7,28 +7,23 @@ namespace IMO2010A4
 
 open finset
 
-def x : ℕ → bool := nat.binary_rec ff (λ odd k, bxor (bor odd k.bodd))
+def x : ℕ → bool := nat.binary_rec ff (λ odd k, bxor $ bor odd k.bodd)
 def S (n : ℕ) : ℤ := (range n).sum (λ k, cond (x k) (-1) 1)
 
 
 
 section x_prop
 
-private lemma x_zero : x 0 = ff := nat.binary_rec_zero ff _
+private lemma x_zero : x 0 = ff :=
+  nat.binary_rec_zero ff _
 
 private lemma x_mul2 (k : ℕ) : x (2 * k) = bxor k.bodd (x k) :=
-begin
-  rw [x, ← nat.bit0_val],
-  refine nat.binary_rec_eq _ ff k,
-  rw [ff_bor, bxor_ff, nat.bodd_zero]
-end
+  (congr_arg x (nat.bit0_val k).symm).trans $
+    nat.binary_rec_eq (by rw [ff_bor, bxor_ff, nat.bodd_zero]) ff k
 
 private lemma x_mul2_add1 (k : ℕ) : x (2 * k + 1) = !(x k) :=
-begin
-  rw [x, ← nat.bit1_val, ← tt_bxor],
-  refine nat.binary_rec_eq _ tt k,
-  rw [ff_bor, bxor_ff, nat.bodd_zero]
-end
+  (congr_arg x (nat.bit1_val k).symm).trans $
+    (nat.binary_rec_eq (by rw [ff_bor, bxor_ff, nat.bodd_zero]) tt k).trans (tt_bxor _)
 
 private lemma x_mul4_lem1 (k : ℕ) : x (4 * k + 1) = !(x (4 * k)) :=
   by rw [bit0, ← two_mul, mul_assoc, x_mul2, x_mul2_add1, ← nat.bit0_val, nat.bodd_bit0, ff_bxor]
@@ -53,11 +48,8 @@ private lemma S_succ (a : ℕ) : S a.succ = S a + cond (x a) (-1) 1 :=
   sum_range_succ _ a
 
 private lemma S_mul4_add2 (k : ℕ) : S (4 * k + 2) = S (4 * k) :=
-begin
-  rw [S_succ, S_succ, add_assoc, add_right_eq_self, x_mul4_lem1],
-  generalize : x (4 * k) = b,
-  cases b; refl
-end
+  by rw [S_succ, S_succ, add_assoc, add_right_eq_self, x_mul4_lem1];
+    exact bool.rec rfl rfl (x (4 * k))
 
 private lemma S_mul4 : ∀ k : ℕ, S (4 * k) = 2 * S k
 | 0 := rfl
@@ -66,20 +58,13 @@ private lemma S_mul4 : ∀ k : ℕ, S (4 * k) = 2 * S k
 
 private lemma S_parity : ∀ k : ℕ, (S k).bodd = k.bodd
 | 0 := rfl
-| (k+1) := begin
-  rw [nat.bodd_succ, S_succ, int.bodd_add, S_parity, ← bxor_tt],
-  generalize : x k = b,
-  cases b; refl
-end
+| (k+1) := by rw [nat.bodd_succ, S_succ, int.bodd_add, S_parity, ← bxor_tt];
+  exact bool.rec rfl rfl (x k)
 
 private lemma S_four_mul_add_eq_zero_iff (q : ℕ) {r : ℕ} (h : r < 4) :
   S (4 * q + r) = 0 ↔ S q = 0 ∧ (r = 0 ∨ r = 2) :=
 begin
-  ---- If `S_q = 0` and `r ∈ {0, 2}`, then `S_{4q + r} = 0`
-  symmetry; refine ⟨λ h0, _, λ h0, (and_iff_right_of_imp _).mpr _⟩,
-  rcases h0 with ⟨h0, rfl | rfl⟩,
-  rw [add_zero, S_mul4, h0, mul_zero],
-  rw [S_mul4_add2, S_mul4, h0, mul_zero],
+  refine ⟨λ h0, (and_iff_right_of_imp _).mpr _, λ h0, _⟩,
 
   ---- If `S_{4q + r} = 0` and `r ∈ {0, 2}`, then `S_q = 0`
   replace h : (2 : ℤ) ≠ 0 := two_ne_zero,
@@ -93,10 +78,17 @@ begin
   iterate 3 { rw nat.lt_succ_iff_lt_or_eq at h },
   rw [nat.lt_one_iff, or_assoc, or_or_or_comm] at h,
   revert h; refine (or_iff_left _).mp,
-  rintros (rfl | rfl); exact tt_eq_ff_eq_false h0
+  rintros (rfl | rfl); exact tt_eq_ff_eq_false h0,
+
+  ---- If `S_q = 0` and `r ∈ {0, 2}`, then `S_{4q + r} = 0`
+  rcases h0 with ⟨h0, rfl | rfl⟩,
+  rw [add_zero, S_mul4, h0, mul_zero],
+  rw [S_mul4_add2, S_mul4, h0, mul_zero]
 end
 
 end S_prop
+
+
 
 
 
@@ -111,12 +103,9 @@ begin
     rw S_zero,
     rw [le_iff_lt_or_eq, int.lt_iff_add_one_le, zero_add, or_comm] at k_ih,
     rw S_succ; cases k_ih with h h,
-    rw [← h, zero_add, this k h.symm, bool.cond_ff]; exact zero_le_one,
-    rw ← add_neg_self (1 : ℤ); refine add_le_add h _,
-    generalize : x k = b,
-    clear this h k; cases b,
-    rw [bool.cond_ff, neg_le_self_iff]; exact zero_le_one,
-    rw bool.cond_tt },
+    rw [← h, zero_add, this k h.symm]; exact zero_le_one,
+    rw ← add_neg_self (1 : ℤ); exact add_le_add h
+      (bool.rec (neg_le_self int.one_nonneg) (le_refl (-1)) (x k)) },
   
   ---- Now show that `x_k = ff` whenever `S_k = 0`, using strong induction
   intros k h; induction k using nat.strong_induction_on with k k_ih,
@@ -155,8 +144,10 @@ begin
   simp_rw list.mem_singleton; rw forall_eq,
 
   ---- Case 2: `0 < q`
-  replace k_ih := k_ih q (nat.lt_add_right q (4 * q) r (lt_mul_left h0 (by norm_num : 1 < 4))),
-  rw [k_ih, add_comm, nat.digits_add 4 (nat.succ_lt_succ $ nat.succ_pos 2) r q h $ or.inr $ ne_of_gt h0],
+  replace k_ih := k_ih q (nat.lt_add_right q (4 * q) r $
+    lt_mul_left h0 $ nat.succ_lt_succ $ nat.succ_pos 2),
+  rw [k_ih, add_comm,
+    nat.digits_add 4 (nat.succ_lt_succ $ nat.succ_pos 2) r q h $ or.inr $ ne_of_gt h0],
   simp_rw list.mem_cons_iff; rw [forall_eq_or_imp, and_comm]
 end
 
