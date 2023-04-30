bde1ce4ad211c87df06a2b5df3644f82c692a4b6
Gian Sanjaya <43656481+mortarsanjaya@users.noreply.github.com>
2023-04-30 18:37:49 -0400

    2010 A5: Code change

--- src/IMO2010/A5/A5.lean
+++ src/IMO2010/A5/A5.lean
@@ -1,15 +1,14 @@
-import extra.number_theory.pos_rat_primes
+import ring_theory.int.basic
 
 /-! # IMO 2010 A5 -/
 
 namespace IMOSL
 namespace IMO2010A5
 
+open function
+
 def good' {G : Type*} [add_monoid G] (f : G → G) :=
   ∀ x y : G, f (2 • f x + y) = 3 • x + f (x + y)
-
-def good {G : Type*} [monoid G] (f : G → G) :=
-  ∀ x y : G, f (f x ^ 2 * y) = x ^ 3 * f (x * y)
 
 
 
@@ -17,14 +16,14 @@ private lemma neg_is_good' {G : Type*} [add_comm_group G] : good' (has_neg.neg :
   λ x y, by rw [eq_add_neg_iff_add_eq, add_comm, ← sub_eq_add_neg,
     add_sub_add_right_eq_sub, smul_neg, sub_neg_eq_add, eq_comm, succ_nsmul]
 
-private lemma good'_iff {G : Type*} [add_comm_group G] [no_zero_smul_divisors ℕ G] :
-  ∀ f : G → G, good' f ↔ ∃ φ : G →+ G, f = φ ∧ ∀ x : G, 2 • φ (φ x) = 3 • x + φ x :=
+private lemma good'_iff {G : Type*} [add_comm_group G] [no_zero_smul_divisors ℕ G] (f : G → G) :
+  good' f ↔ ∃ φ : G →+ G, f = φ ∧ ∀ x : G, 2 • φ (φ x) = 3 • x + φ x :=
 begin
   ---- First, solve the `←` direction.
-  intros f; symmetry; refine ⟨λ h x y, _, λ h, _⟩,
+  symmetry; refine ⟨λ h x y, _, λ h, _⟩,
   rcases h with ⟨φ, rfl, h⟩; rw [map_add, map_add, ← add_assoc, ← h, map_nsmul],
 
-  ---- Now solve the `→` direction
+  ---- Now solve the `→` direction.
   -- First reduce to just `f` being additive.
   have h0 := λ x, h x 0,
   simp_rw add_zero at h0,
@@ -35,19 +34,17 @@ begin
     rw [add_monoid_hom.coe_mk, ← h0, two_nsmul, ← this, ← two_nsmul] },
 
   -- Reduce to showing that `f` is injective.
-  revert h; suffices : function.injective f,
+  revert h; suffices : injective f,
   { intros h x y,
     have h1 := h x (2 • f y),
     rw [← smul_add, add_comm x, h, ← add_assoc, ← smul_add, add_comm y, ← h0] at h1,
-    replace h1 := this h1,
-    rwa [smul_right_inj, eq_comm] at h1,
-    exact two_ne_zero },
+    exact (smul_right_injective G (nat.succ_ne_zero 1) (this h1)).symm },
 
   -- Now show that `f` is injective.
   intros x y h,
   have h1 := h0 y,
-  rwa [← h, h0, add_left_inj, smul_right_inj] at h1,
-  exact three_ne_zero
+  rwa [← h, h0, add_left_inj] at h1,
+  exact smul_right_injective G (nat.succ_ne_zero 2) h1,
 end
 
 
@@ -58,12 +55,12 @@ end
 
 /-- Final solution, additive version -/
 theorem final_solution_additive {G : Type*} [add_comm_group G]
-  {S : Type*} {ρ : G →+ S → ℤ} (h : function.injective ρ) :
+  {S : Type*} {ρ : G →+ S → ℤ} (h : injective ρ) :
   ∀ f : G → G, good' f ↔ f = has_neg.neg :=
 begin
   ---- A characterization of zeroes in `G` using `ρ`
   refine λ f, ⟨λ h0, _, λ h0, by subst h0; exact neg_is_good'⟩,
-  simp_rw [injective_iff_map_eq_zero', function.funext_iff, pi.zero_apply] at h,
+  simp_rw [injective_iff_map_eq_zero', funext_iff, pi.zero_apply] at h,
 
   ---- Set up `no_zero_smul_divisors ℕ G` instance
   haveI _inst_2 : no_zero_smul_divisors ℕ G :=
@@ -83,7 +80,7 @@ begin
       add_left_inj, sub_sub, ← add_nsmul, sub_eq_iff_eq_add, ← succ_nsmul, ← sub_eq_zero] at h0,
   simp_rw [← h, map_sub, pi.sub_apply, sub_eq_zero, map_nsmul, pi.smul_apply,
     nsmul_eq_mul, nat.cast_bit1, nat.cast_bit0, nat.cast_one] at h0,
-  simp_rw [function.funext_iff, add_monoid_hom.sub_apply,
+  simp_rw [funext_iff, add_monoid_hom.sub_apply,
     add_monoid_hom.id_apply, sub_eq_neg_self, ← h],
   clear h,
 
@@ -104,21 +101,6 @@ begin
   refine (is_coprime.pow_left ⟨(-2 : ℤ), 1, _⟩).dvd_of_dvd_mul_left ⟨c, h0.symm⟩,
   norm_num
 end
-
-
-
-/-- Final solution, multiplicative version -/
-theorem final_solution_multiplicative {G : Type*} [comm_group G]
-  {S : Type*} {ρ : additive G →+ S → ℤ} (h : function.injective ρ) :
-  ∀ f : G → G, good f ↔ f = has_inv.inv :=
-  final_solution_additive h
-
-
-
-/-- Final solution, original (`ℚ+`) version -/
-theorem final_solution_original :
-  ∀ f : {q : ℚ // 0 < q} → {q : ℚ // 0 < q}, good f ↔ f = has_inv.inv :=
-  final_solution_multiplicative extra.pos_rat_factor_hom.inj
 
 end IMO2010A5
 end IMOSL
---
+++ src/IMO2010/A5/A5_pos_rat.lean
@@ -0,0 +1,14 @@
+import IMO2010.A5.A5 extra.number_theory.pos_rat_primes
+
+/-! # IMO 2010 A5, `ℚ+` version -/
+
+namespace IMOSL
+namespace IMO2010A5
+
+/-- Final solution, original (`ℚ+`) version -/
+theorem final_solution_original : ∀ f : {q : ℚ // 0 < q} → {q : ℚ // 0 < q},
+  (∀ x y : {q : ℚ // 0 < q}, f (f x ^ 2 * y) = x ^ 3 * f (x * y)) ↔ f = has_inv.inv :=
+  final_solution_additive extra.pos_rat_factor_hom.inj
+
+end IMO2010A5
+end IMOSL
