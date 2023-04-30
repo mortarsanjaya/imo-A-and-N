9a047186d338df418374ce2d0aa5843ef51e75fe
Gian Sanjaya <43656481+mortarsanjaya@users.noreply.github.com>
2023-04-30 19:51:29 -0400

    2011 A6: Code update

--- src/IMO2011/A6/A6.lean
+++ src/IMO2011/A6/A6.lean
@@ -11,22 +11,22 @@ theorem final_solution {R : Type*} [linear_ordered_comm_ring R]
   ∀ x : R, x ≤ 0 → f x = 0 :=
 begin
   ---- A sequence of results
-  have h0 : ∀ t x : R, f (f t) ≤ (f t - x) * f x + f (f x) := λ t x,
-    by replace h := h x (f t - x); rwa add_sub_cancel'_right at h,
-  replace h0 : ∀ x : R, x * f x ≤ 0 := λ x,
-    by linarith [h0 (2 * f x) x, h0 x (2 * f x)],
-  have h1 : ∀ x : R, f x ≤ f (f x) := λ x,
-    by replace h := h x 0; rwa [add_zero, zero_mul, zero_add] at h,
-  have h2 : ∀ x : R, f x ≤ 0 := λ x,
-    by contrapose! h1; exact ⟨x, lt_of_le_of_lt (nonpos_of_mul_nonpos_right (h0 (f x)) h1) h1⟩,
-  replace h0 : ∀ x : R, x < 0 → f x = 0 := λ x hx,
-    le_antisymm (h2 x) (nonneg_of_mul_nonpos_right (h0 x) hx),
+  have h0 : ∀ t x : R, f (f t) ≤ (f t - x) * f x + f (f x) :=
+    λ t x, le_of_eq_of_le (congr_arg f (add_sub_cancel'_right x _).symm) (h x _),
+  replace h0 : ∀ x : R, x * f x ≤ 0 :=
+    λ x, by linarith [h0 (2 * f x) x, h0 x (2 * f x)],
+  have h1 : ∀ x : R, f x ≤ f (f x) :=
+    λ x, by replace h := h x 0; rwa [add_zero, zero_mul, zero_add] at h,
+  have h2 : ∀ x : R, f x ≤ 0 :=
+    λ x, le_of_not_lt (λ h2, not_lt_of_le (h0 $ f x) $
+      mul_pos h2 $ lt_of_lt_of_le h2 $ h1 x),
+  replace h0 : ∀ x : R, x < 0 → f x = 0 :=
+    λ x h3, le_antisymm (h2 x) (nonneg_of_mul_nonpos_right (h0 x) h3),
 
   ---- Finishing
-  intros x h,
-  rw le_iff_lt_or_eq at h; rcases h with h | rfl,
-  exact h0 x h,
-  refine le_antisymm (h2 0) _,
+  intros x h3,
+  refine (lt_or_eq_of_le h3).elim (h0 x)
+    (λ h4, (congr_arg f h4).trans $ le_antisymm (h2 0) _),
   rw ← h0 (-1) neg_one_lt_zero; exact h1 (-1)
 end
 
