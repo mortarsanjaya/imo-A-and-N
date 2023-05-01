95a4a30cd6ff606b604e95544e4381be4d24f37b
Gian Sanjaya <43656481+mortarsanjaya@users.noreply.github.com>
2023-05-01 01:30:45 -0400

    extra: Optimization

--- src/extra/real_prop/real_quadratic_sol.lean
+++ src/extra/real_prop/real_quadratic_sol.lean
@@ -9,16 +9,17 @@ open real
 namespace IMOSL
 namespace extra
 
-lemma exists_add_eq_mul_eq {x y : ℝ} (h : 4 * y ≤ x ^ 2) : ∃ u v : ℝ, u + v = x ∧ u * v = y :=
-begin
-  use [(x + sqrt (x ^ 2 - 4 * y)) / 2, (x - sqrt (x ^ 2 - 4 * y)) / 2]; split,
-  rw [← add_div, add_add_sub_cancel, ← mul_two, mul_div_cancel x two_ne_zero],
-  rw [div_mul_div_comm, ← sq_sub_sq, sq_sqrt, sub_sub_cancel, bit0, ← two_mul, mul_div_cancel_left],
-  apply mul_ne_zero; exact two_ne_zero,
-  rwa sub_nonneg
-end
+lemma exists_add_eq_mul_eq {x y : ℝ} (h : 4 * y ≤ x ^ 2) :
+  ∃ u v : ℝ, u + v = x ∧ u * v = y :=
+  ⟨(x + sqrt (x ^ 2 - 4 * y)) / 2,
+  (x - sqrt (x ^ 2 - 4 * y)) / 2,
+  (add_div _ _ _).symm.trans $ (congr_arg (/ (2 : ℝ)) $
+    add_add_sub_cancel x x _).trans (half_add_self x),
+  by rw [div_mul_div_comm, ← sq_sub_sq, sq_sqrt (sub_nonneg.mpr h), sub_sub_cancel,
+    bit0, ← two_mul, mul_div_cancel_left y (mul_ne_zero two_ne_zero two_ne_zero)]⟩
 
-lemma add_mul_root_quadr {u v x y : ℝ} (h : u + v = x) (h0 : u * v = y) : u ^ 2 - x * u + y = 0 :=
+lemma add_mul_root_quadr {u v x y : ℝ} (h : u + v = x) (h0 : u * v = y) :
+  u ^ 2 - x * u + y = 0 :=
   by rw [← h, ← h0, sq, ← sub_mul, sub_add_cancel', mul_comm, mul_neg, neg_add_self]
 
 lemma exists_root_quadr {x y : ℝ} (h : 4 * y ≤ x ^ 2) : ∃ t : ℝ, t ^ 2 + x * t + y = 0 :=
