bf3dc653ce15a0b6cc3efdf5a05f385c572d7119
Gian Sanjaya <43656481+mortarsanjaya@users.noreply.github.com>
2023-05-04 05:02:54 -0400

    2012 A5: Update basic file
    1. Good map from quotient rings
    2. Very greedy optimization

--- src/IMO2012/A5/A5_basic.lean
+++ src/IMO2012/A5/A5_basic.lean
@@ -1,4 +1,4 @@
-import algebra.ring.regular algebra.hom.ring
+import algebra.ring.regular ring_theory.congruence
 
 /-! # IMO 2012 A5, Basic File -/
 
@@ -12,13 +12,27 @@ def good {R S : Type*} [ring R] [ring S] (f : R → S) :=
 
 
 
+/-- The zero map is always good. -/
 lemma zero_is_good {R S : Type*} [ring R] [ring S] : good (λ _ : R, (0 : S)) :=
   λ _ _, (sub_self 0).trans (mul_zero 0).symm
 
-lemma good_map_comp_hom {R R0 S : Type*} [ring R] [ring R0] [ring S]
-  {f : R → S} (h : good f) (φ : R0 →+* R) : good (f ∘ φ) :=
-  λ x y, (congr_arg2 has_sub.sub (congr_arg f $ by rw [φ.map_add, φ.map_mul, φ.map_one])
-    (congr_arg f $ φ.map_add x y)).trans (h (φ x) (φ y))
+/-- Given `f : R → S` good and `φ : R₀ →+* R`, `f ∘ φ` is good as well. -/
+lemma good_map_comp_hom {R R₀ S : Type*} [ring R] [ring R₀] [ring S]
+  {f : R → S} (h : good f) (φ : R₀ →+* R) : good (f ∘ φ) :=
+  λ x y, (congr_arg2 (λ u v, f v - f u) (φ.map_add x y) $
+    (φ.map_add _ _).trans $ congr_arg2 has_add.add (φ.map_mul x y) φ.map_one).trans
+      (h (φ x) (φ y))
+
+/-- An auxiliary definition to state the next lemma. -/
+def ring_con_lift {R S : Type*} [ring R] [ring S] {f : R → S} {c : ring_con R}
+  (h0 : ∀ x y : R, c x y → f x = f y) : c.quotient → S := 
+  quot.lift f h0
+
+/-- Given `f : R → S` good and a ring congruence `∼` on `R` such that `x ∼ y`
+  implies `f(x) = f(y)`, the induced map `R/∼ → S` is also good. -/
+lemma good_ring_con_lift {R S : Type*} [ring R] [ring S] {f : R → S} (h : good f)
+  {c : ring_con R} (h0 : ∀ x y : R, c x y → f x = f y) : good (ring_con_lift h0) :=
+  λ x y, quot.induction_on₂ x y h
 
 variables {R S : Type*} [ring R] [ring S]
 
@@ -28,13 +42,17 @@ variables {f : R → S} (h : good f)
 include h
 
 lemma map_comm_of_comm {x y : R} (h0 : commute x y) : commute (f x) (f y) :=
-  by have h1 := h y x; rwa [← h0.eq, add_comm y, h] at h1
+  (h x y).symm.trans $ (congr_arg2 (λ u v, f (u + 1) - f v) h0 (add_comm x y)).trans (h y x)
 
 lemma map_neg_sub_map1 (x : R) : f (1 - x) - f (x - 1) = f x * f (-1) :=
-  by rw [← h, mul_neg_one, neg_add_eq_sub, ← sub_eq_add_neg]
+  (congr_arg2 (λ u v, f v - f u) (sub_eq_add_neg x 1)
+    ((congr_arg (λ u : R, u + 1) $ mul_neg_one x).trans $ neg_add_eq_sub x 1).symm).trans
+  (h x (-1))
 
 lemma map_neg_sub_map2 (x : R) : f (-x) - f x = f (x + 1) * f (-1) :=
-  by rw [← map_neg_sub_map1 h, add_sub_cancel, ← sub_sub, sub_sub_cancel_left]
+  (congr_arg2 (λ u v, f v - f u) (add_sub_cancel x 1).symm
+    ((sub_add_cancel' 1 x).symm.trans $ congr_arg (has_sub.sub 1) (add_comm 1 x))).trans
+  (map_neg_sub_map1 h $ x + 1)
 
 end non_domain
 
@@ -44,7 +62,8 @@ variables [is_domain S] {f : R → S} (h : good f)
 include h
 
 lemma good_map_one : f 1 = 0 :=
-  by replace h := h 1 1; rwa [mul_one, sub_self, zero_eq_mul_self] at h
+  mul_self_eq_zero.mp $ (h 1 1).symm.trans $ sub_eq_zero.mpr $
+    congr_arg f $ (add_left_inj 1).mpr (mul_one 1)
 
 lemma eq_zero_of_map_zero_ne_neg_one (h0 : f 0 ≠ -1) : f = λ _, 0 :=
   funext (λ x, by have h1 := h x 0; rwa [mul_zero, zero_add, good_map_one h, zero_sub,
