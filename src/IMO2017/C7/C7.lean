af5da40c6b56dffe3e69ae472d42309faca389d1
Gian Sanjaya <43656481+mortarsanjaya@users.noreply.github.com>
2023-05-18 15:38:54 -0400

    2017 C7: Code clean-up
    Instead of defining another multiplication on finset \N, we define a new type as a copy of finset \N and then define multiplication on this new type.

--- src/IMO2017/C7/C7.lean
+++ src/IMO2017/C7/C7.lean
@@ -7,17 +7,11 @@ namespace IMO2017C7
 
 open finset function
 
-noncomputable def nth_notin (X : finset ℕ) := nat.nth (λ n, n ∉ X)
-noncomputable def cup_mul (X Y : finset ℕ) := X ∪ image (nth_notin X) Y
-noncomputable def cup_pow (X : finset ℕ) (k : ℕ) := nat.iterate (λ Y, cup_mul X Y) k ∅
-local infix ` ** `:80 := cup_mul
-local infix ` ^^ `:100 := cup_pow
+/-! Extra lemmas -/
+section extra_lemmas
 
-
-
-section strict_mono
-
-private lemma strict_mono_eq_at_large_comm {f g : ℕ → ℕ} (hf : strict_mono f) (hg : strict_mono g)
+lemma strict_mono_eq_at_large_comm {f g : ℕ → ℕ}
+  (hf : strict_mono f) (hg : strict_mono g)
   (h : ∃ N : ℕ, ∀ n : ℕ, N ≤ n → f n = g n) (h0 : f ∘ g = g ∘ f) : f = g :=
 begin
   cases h with N h,
@@ -28,82 +22,107 @@ begin
   exact h1 n h2,
   cases eq_or_lt_of_le (hf.id_le k) with h3 h3,
   cases eq_or_lt_of_le (hg.id_le k) with h4 h4,
-  rw [← h3, ← h4],
-  { replace h1 := h1 (g k) h4,
-    rw [← comp_app f, h0, comp_app] at h1,
-    exact hg.injective h1 },
-  { replace h1 := h1 (f k) h3,
-    rw [← comp_app g, ← h0, comp_app] at h1,
-    exact hf.injective h1 }
+  { rw [← h3, ← h4] },
+  { exact hg.injective ((congr_fun h0.symm k).trans $ h1 (g k) h4) },
+  { exact hf.injective ((h1 (f k) h3).trans $ congr_fun h0.symm k) }
 end
 
-end strict_mono
+lemma count_true : ∀ n : ℕ, nat.count (λ _, true) n = n
+| 0 := rfl
+| (n+1) := (nat.count_succ _ n).trans $ 
+    congr_arg2 has_add.add (count_true n) (if_true 1 0)
 
+lemma nth_true : nat.nth (λ _, true) = id :=
+  funext $ λ n, (congr_arg (nat.nth $ λ _, true) (count_true n).symm).trans $
+    nat.nth_count (λ _, true) trivial
 
+section classical
+open_locale classical
 
-section prop_lemmas
+lemma count_prop_inj {p q : ℕ → Prop}
+  (h : ∀ n : ℕ, nat.count p n = nat.count q n) : p = q :=
+  funext $ λ n, by rw [← @nat.count_succ_eq_succ_count_iff p, h,
+    h, nat.count_succ_eq_succ_count_iff]
 
-private lemma count_true : nat.count (λ _, true) = id :=
-  funext (λ n, match n with
-    | 0 := rfl
-    | (n+1) := (nat.count_succ _ n).trans $ 
-        congr_arg2 has_add.add (by exact _match n) (if_true 1 0)
-  end)
-
-private lemma nth_true : nat.nth (λ _, true) = id :=
-  funext (λ n, by have h := nat.nth_count (λ _, true) (trivial : (λ _, true) n);
-    rwa count_true at h)
-
-variables {p q : ℕ → Prop} [decidable_pred p] [decidable_pred q]
-
-private lemma count_prop_inj (h : ∀ n : ℕ, nat.count p n = nat.count q n) : p = q :=
-  funext (λ n, by rw [← @nat.count_succ_eq_succ_count_iff p, h,
-    h, nat.count_succ_eq_succ_count_iff])
-
-private lemma nth_prop_inj (hp : (set_of p).infinite) (hq : (set_of q).infinite) :
+lemma nth_prop_inj {p q : ℕ → Prop}
+  (hp : (set_of p).infinite) (hq : (set_of q).infinite) :
   nat.nth p = nat.nth q ↔ p = q :=
   ⟨λ h, count_prop_inj $ λ n, galois_connection.l_unique
     (nat.count_nth_gc _ hp) (nat.count_nth_gc _ hq) (congr_fun h),
   congr_arg nat.nth⟩
 
-end prop_lemmas
+end classical
 
-
-
-section finset_lemmas
-
-variables (X Y : finset ℕ)
-
-private lemma nat_finset_infinite_compl : {n : ℕ | n ∉ X}.infinite :=
+lemma nat_finset_infinite_compl (X : finset ℕ) : {n : ℕ | n ∉ X}.infinite :=
   by have h := (X : set ℕ).to_finite.infinite_compl; rwa set.compl_def at h
 
-private lemma range_nth_notin_eq_compl : set.range (nth_notin X) = Xᶜ :=
-begin
-  ext n; rw [set.mem_range, set.mem_compl_iff, mem_coe],
-  refine ⟨_, λ h, ⟨nat.count (λ n, n ∉ X) n, nat.nth_count (λ n, n ∉ X) h⟩⟩,
-  rintros ⟨y, rfl⟩,
-  exact nat.nth_mem_of_infinite (λ n, n ∉ X) (nat_finset_infinite_compl X) y
-end
+end extra_lemmas
 
-private lemma range_nth_notin_eq_univ_diff : set.range (nth_notin X) = set.univ \ X :=
-  (range_nth_notin_eq_compl X).trans (set.compl_eq_univ_diff ↑X)
 
-private lemma nth_notin_inj : nth_notin X = nth_notin Y ↔ X = Y :=
-  by rw [nth_notin, nth_notin,
-    nth_prop_inj (nat_finset_infinite_compl X) (nat_finset_infinite_compl Y)];
-  exact ⟨λ h, ext $ λ n, not_iff_not.mp $ eq_iff_iff.mp $ congr_fun h n,
-    λ h, funext $ λ n, congr_arg not $ congr_arg (has_mem.mem n) h⟩
 
-private lemma nth_notin_strict_mono : strict_mono (nth_notin X) :=
-  nat.nth_strict_mono _ (nat_finset_infinite_compl X)
 
-private lemma nth_notin_fn_inj : injective (nth_notin X) :=
-  strict_mono.injective (nth_notin_strict_mono X)
 
-private lemma nth_notin_empty : nth_notin ∅ = id :=
+
+
+/-! `ℕset` and its properties -/
+
+/-- `finset ℕ` but with the special multiplication operation -/
+def ℕset := finset ℕ
+
+
+
+namespace ℕset
+
+instance : has_emptyc ℕset := finset.has_emptyc
+instance : has_mem ℕ ℕset := finset.has_mem
+instance : has_union ℕset := finset.has_union
+instance : has_inter ℕset := finset.has_inter
+instance : has_coe_t ℕset (set ℕ) := finset.set.has_coe_t
+
+instance : has_one ℕset := ⟨∅⟩
+instance : inhabited ℕset := ⟨∅⟩
+
+lemma one_eq_empty : (1 : ℕset) = ∅ := rfl
+
+
+
+/-- `f_X(n)` is the `n`th element not in `X`, indexed from `0`. -/
+noncomputable def nth_notin (X : ℕset) : function.End ℕ := nat.nth (λ n, n ∉ X)
+
+/-- `nth_notin` is an embedding to `End(ℕ)`; we see later that it is injective. -/
+noncomputable instance : has_coe_t ℕset (function.End ℕ) := ⟨nth_notin⟩
+
+lemma coe_fun_eq_nth_notin (X : ℕset) : (X : function.End ℕ) = nth_notin X := rfl
+
+lemma coe_fun_one : ((1 : ℕset) : function.End ℕ) = id :=
   (congr_arg nat.nth $ funext $ λ n, eq_true_iff.mpr (not_mem_empty n : n ∉ ∅)).trans nth_true
 
-private lemma count_notin_large {n : ℕ} (h : X.sup id < n) :
+private lemma range_coe_fun_eq_compl (X : ℕset) : set.range (X : function.End ℕ) = Xᶜ :=
+  set.ext $ λ n, ⟨λ h, exists.elim h $ λ y h0, by rw ← h0;
+      exact nat.nth_mem_of_infinite (λ n, n ∉ X) (nat_finset_infinite_compl X) y,
+    λ h, ⟨nat.count (λ n, n ∉ X) n, nat.nth_count (λ n, n ∉ X) h⟩⟩
+
+private lemma range_coe_fun_eq_univ_diff (X : ℕset) :
+  set.range (X : function.End ℕ) = set.univ \ X :=
+  (range_coe_fun_eq_compl X).trans (set.compl_eq_univ_diff ↑X)
+
+private lemma coe_fun_inj {X Y : ℕset} : (X : function.End ℕ) = Y ↔ X = Y :=
+  (nth_prop_inj (nat_finset_infinite_compl X) (nat_finset_infinite_compl Y)).trans
+  ⟨λ h, ext $ λ n, not_iff_not.mp $ iff_of_eq $ congr_fun h n,
+    λ h, funext $ λ n, congr_arg not $ congr_arg (has_mem.mem n) h⟩
+
+private lemma coe_fun_strict_mono (X : ℕset) : strict_mono (X : function.End ℕ) :=
+  nat.nth_strict_mono _ (nat_finset_infinite_compl X)
+
+private lemma coe_fun_map_inj (X : ℕset) : injective (X : function.End ℕ) :=
+  strict_mono.injective (coe_fun_strict_mono X)
+
+
+
+/-- Plain supremum of an `ℕset` -/
+def sup (X : ℕset) := X.sup id
+
+private lemma count_notin_large {X : ℕset} {n : ℕ} (h : X.sup < n) :
   nat.count (λ x, x ∉ X) (n + X.card) = n :=
 begin
   have h0 := congr_arg card (filter_union_filter_neg_eq (λ x, x ∈ X) (range (n + X.card))),
@@ -120,109 +139,105 @@ begin
   exact ne_empty_of_mem h0
 end
 
-private lemma nth_notin_large {n : ℕ} (h : X.sup id < n) : nth_notin X n = n + X.card :=
+private lemma coe_fun_map_large {X : ℕset} {n : ℕ} (h : X.sup < n) :
+  (X : function.End ℕ) n = n + X.card :=
 begin
   have h0 := nat_finset_infinite_compl X,
-  rw [nth_notin, eq_comm, eq_iff_le_not_lt]; split,
-  rw [← nat.count_le_iff_le_nth _ h0, count_notin_large X h],
-  rw [← nat.succ_le_iff, ← nat.count_le_iff_le_nth _ h0, nat.count_succ, count_notin_large X h,
+  rw [coe_fun_eq_nth_notin, nth_notin, eq_comm, eq_iff_le_not_lt]; split,
+  rw [← nat.count_le_iff_le_nth _ h0, count_notin_large h],
+  rw [← nat.succ_le_iff, ← nat.count_le_iff_le_nth _ h0, nat.count_succ, count_notin_large h,
       add_le_iff_nonpos_right, nonpos_iff_eq_zero, ← ne.def, ite_ne_right_iff, and_comm],
   refine ⟨one_ne_zero, λ h1, _⟩,
-  replace h1 : id (n + X.card) ≤ X.sup id := le_sup h1,
+  replace h1 : id (n + X.card) ≤ X.sup := le_sup h1,
   rw [id.def, ← not_lt] at h1,
   exact h1 (lt_of_lt_of_le h le_self_add)
 end
 
-end finset_lemmas
 
 
+/-- Multiplication based on `nth_notin` -/
+noncomputable def mul (X Y : ℕset) := X ∪ image (X : function.End ℕ) Y
 
-section cup_mul_lemmas
+noncomputable instance : has_mul ℕset := ⟨mul⟩
 
-lemma cup_mul_empty (X : finset ℕ) : X ** ∅ = X :=
-  by rw [cup_mul, image_empty, union_empty]
-  
-lemma empty_cup_mul (X : finset ℕ) : ∅ ** X = X :=
-  by rw [cup_mul, empty_union, nth_notin_empty, image_id]
+lemma mul_def (X Y : ℕset) : X * Y = X ∪ image (X : function.End ℕ) Y := rfl
 
-/-- For any `X Y : finset ℕ`, `|X ** Y| = |X| + |Y|`. -/
-lemma cup_mul_card (X Y : finset ℕ) : (X ** Y).card = X.card + Y.card :=
+lemma mul_one (X : ℕset) : X * 1 = X :=
+  union_empty X
+
+lemma one_mul (X : ℕset) : 1 * X = X :=
+  (empty_union _).trans $ (congr_arg2 image coe_fun_one rfl).trans image_id
+
+lemma card_mul (X Y : ℕset) : (X * Y).card = X.card + Y.card :=
 begin
-  rw [cup_mul, card_disjoint_union, card_image_of_injective Y (nth_notin_fn_inj X)],
+  rw [mul_def, card_disjoint_union, card_image_of_injective Y (coe_fun_map_inj X)],
   rw disjoint_right; intros a h1 h0,
   rw ← mem_coe at h1,
-  replace h1 : a ∈ set.range (nth_notin X) := coe_image_subset_range h1,
-  rw [range_nth_notin_eq_compl, set.mem_compl_iff, finset.mem_coe] at h1,
+  replace h1 : a ∈ set.range (X : function.End ℕ) := coe_image_subset_range h1,
+  rw [range_coe_fun_eq_compl, set.mem_compl_iff, finset.mem_coe] at h1,
   exact h1 h0
 end
 
-/-- Lemma 1 in the official solution: `f_{X ** Y} = f_X ∘ f_Y` -/
-lemma cup_mul_range (X Y : finset ℕ) : nth_notin (X ** Y) = nth_notin X ∘ nth_notin Y :=
-begin
-  rw ← well_founded.eq_strict_mono_iff_eq_range is_well_founded.wf (nth_notin_strict_mono _),
-  work_on_goal 2 { exact strict_mono.comp (nth_notin_strict_mono _) (nth_notin_strict_mono _) },
-  rw [range_nth_notin_eq_compl, set.range_comp, range_nth_notin_eq_univ_diff, set.image_diff,
-      set.image_univ, range_nth_notin_eq_univ_diff, set.diff_diff, ← set.compl_eq_univ_diff,
-      compl_inj_iff, cup_mul, coe_union, coe_image],
-  exact nth_notin_fn_inj X
-end
+lemma range_mul (X Y : ℕset) : set.range ((X * Y : ℕset) : function.End ℕ) =
+  set.range ((X : function.End ℕ) ∘ (Y : function.End ℕ)) :=
+  by rw [range_coe_fun_eq_compl, set.range_comp (X : function.End ℕ),
+    range_coe_fun_eq_univ_diff, set.image_diff (coe_fun_map_inj X),
+    set.image_univ, range_coe_fun_eq_univ_diff, set.diff_diff,
+    ← set.compl_eq_univ_diff, compl_inj_iff, mul_def, coe_union, coe_image]
 
-/-- The `cup_mul` operation is associative. -/
-theorem cup_mul_assoc (X Y Z : finset ℕ) : X ** Y ** Z = X ** (Y ** Z) :=
-  by rw ← nth_notin_inj; repeat { rw cup_mul_range }
+/-- Lemma 1 in the official solution. The lemma implies `mul_assoc`.
+  Thus `ℕset` is a monoid, and the coercion to `function.End ℕ` is a monoid homomorphism. -/
+lemma coe_fun_mul (X Y : ℕset) : ((X * Y : ℕset) : function.End ℕ) = X * Y :=
+  (well_founded.eq_strict_mono_iff_eq_range is_well_founded.wf (coe_fun_strict_mono _) $
+    (coe_fun_strict_mono X).comp (coe_fun_strict_mono Y)).mp $ range_mul X Y
 
-/-- Lemma 2 in the official solution: if X ** Y = Y ** X and |X| = |Y| then X = Y -/
-lemma cup_mul_comm_card_eq {X Y : finset ℕ} (h : X.card = Y.card) (h0 : X ** Y = Y ** X) : X = Y :=
+lemma mul_assoc (X Y Z : ℕset) : X * Y * Z = X * (Y * Z) :=
+  coe_fun_inj.mp $ by iterate 4 { rw coe_fun_mul }; rw mul_assoc
+
+noncomputable instance : monoid ℕset :=
+{ one := has_one.one,
+  mul := has_mul.mul,
+  mul_assoc := mul_assoc,
+  one_mul := one_mul,
+  mul_one := mul_one }
+
+
+
+/-- Cardinality as a monoid homomorphism -/
+def card_hom : ℕset →* multiplicative ℕ :=
+  ⟨additive.to_mul ∘ finset.card, congr_arg additive.to_mul card_empty, card_mul⟩
+
+lemma card_pow : ∀ (X : ℕset) (k : ℕ), (X ^ k).card = k * X.card :=
+  card_hom.map_pow
+
+
+
+/-- Lemma 2 in the official solution. If `|X| = |Y|` and `XY = YX`, then `X = Y`. -/
+lemma commute_eq_card_imp_eq {X Y : ℕset}
+  (h : X.card = Y.card) (h0 : X * Y = Y * X) : X = Y :=
 begin
-  rw ← nth_notin_inj at h0 ⊢,
-  rw [cup_mul_range, cup_mul_range] at h0,
-  refine strict_mono_eq_at_large_comm (nth_notin_strict_mono X) (nth_notin_strict_mono Y) _ h0,
-  use max (X.sup id) (Y.sup id) + 1; intros n h1,
+  rw ← coe_fun_inj at h0 ⊢,
+  rw [coe_fun_mul, coe_fun_mul] at h0,
+  refine strict_mono_eq_at_large_comm (coe_fun_strict_mono X) (coe_fun_strict_mono Y)
+    ⟨(max X.sup Y.sup).succ, λ n h1, _⟩ h0,
   rw [nat.succ_le_iff, max_lt_iff] at h1,
   cases h1 with h1 h2,
-  rw [nth_notin_large X h1, nth_notin_large Y h2, h]
+  rw [coe_fun_map_large h1, coe_fun_map_large h2, h]
 end
 
-end cup_mul_lemmas
+/-- The main problem to be solved. The `final_solution` theorem will be its alias. -/
+lemma commute_pow_card_eq {X Y : ℕset} (h : X * Y = Y * X) : X ^ Y.card = Y ^ X.card :=
+  commute_eq_card_imp_eq
+    ((card_pow X Y.card).trans $ (mul_comm Y.card X.card).trans (card_pow Y X.card).symm)
+    (commute.pow_pow h Y.card X.card)
+
+end ℕset
 
 
 
-section cup_pow_lemmas
-
-lemma cup_pow_zero (X : finset ℕ) : X ^^ 0 = ∅ := rfl
-
-lemma cup_pow_succ (X : finset ℕ) (k : ℕ) : X ^^ k.succ = X ** X ^^ k :=
-  by rw [cup_pow, cup_pow, iterate_succ', comp_app]
-
-/- For any `X : finset ℕ` and `k : ℕ`, `|X^^k| = k |X|`. -/
-lemma cup_pow_card (X : finset ℕ) (k : ℕ) : (X ^^ k).card = k * X.card :=
-begin
-  induction k with k k_ih,
-  rw [cup_pow_zero, card_empty, zero_mul],
-  rw [cup_pow_succ, cup_mul_card, k_ih, nat.succ_mul, add_comm]
-end
-
-/- If `X` commutes with `Y`, then `X^^k` commutes with `Y` for any `k`. -/
-lemma cup_pow_comm' {X Y : finset ℕ} (h : X ** Y = Y ** X) (k : ℕ) :
-  X ^^ k ** Y = Y ** X ^^ k :=
-begin
-  induction k with k k_ih,
-  rw [cup_pow_zero, cup_mul_empty, empty_cup_mul],
-  rw [cup_pow_succ, cup_mul_assoc, k_ih, ← cup_mul_assoc, ← cup_mul_assoc, h]
-end
-
-/- If `X` commutes with `Y`, then `X^^k` commutes with `Y^^m` for any `k` and `m`. -/
-lemma cup_pow_comm {X Y : finset ℕ} (h : X ** Y = Y ** X) (k m : ℕ) :
-  X ^^ k ** Y ^^ m = Y ^^ m ** X ^^ k :=
-  cup_pow_comm' (eq_comm.mp (cup_pow_comm' (eq_comm.mp h) m)) k
-
-end cup_pow_lemmas
-
-
-
-/- Final solution -/
-theorem final_solution (X Y : finset ℕ) (h : X ** Y = Y ** X) : X ^^ Y.card = Y ^^ X.card :=
-  cup_mul_comm_card_eq (by rw [cup_pow_card, cup_pow_card, mul_comm]) (cup_pow_comm h _ _)
+/-- Final solution (alias of `commute_pow_card_eq`) -/
+theorem final_solution {X Y : ℕset} (h : X * Y = Y * X) : X ^ Y.card = Y ^ X.card :=
+  ℕset.commute_pow_card_eq h
 
 end IMO2017C7
 end IMOSL
