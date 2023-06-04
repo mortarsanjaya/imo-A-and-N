ec046965639ce551f54b509d2dcb669b0bb0bf61
Gian Sanjaya <43656481+mortarsanjaya@users.noreply.github.com>
2023-06-04 11:56:02 -0400

    2014 C2: Generalize

---
+++ src/extra/AM_GM_induction.lean
@@ -0,0 +1,26 @@
+import data.nat.basic
+
+/-!
+# AM-GM-style induction
+
+Let `P : ℕ → Prop` be an `ℕ`-predicate.
+Suppose that we have `P(1)`, `P(n) → P(2n)`, and `P(n + 1) → P(n)` for any `n : ℕ`.
+Then `P(n)` holds for all `n : ℕ`.
+This file provides a proof of this statement.
+
+This is the standard inductive proof for unweighted AM-GM.
+Hence the author decides to call it the AM-GM induction.
+-/
+
+namespace IMOSL
+namespace extra
+
+theorem AM_GM_induction {P : ℕ → Prop} (h : P 1)
+  (h0 : ∀ n : ℕ, P n → P (2 * n)) (h1 : ∀ n : ℕ, P n.succ → P n) : ∀ n : ℕ, P n
+| 0 := h1 0 h
+| 1 := h
+| (n+2) := nat.decreasing_induction h1 (nat.add_le_add_right (le_of_eq_of_le n.one_mul.symm $
+    nat.mul_le_mul_right n $ nat.succ_pos 1) 2) (h0 n.succ $ AM_GM_induction n.succ)
+
+end extra
+end IMOSL
