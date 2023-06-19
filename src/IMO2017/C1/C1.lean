36df7b70f74f6865a277306daa69e547e2b0fe14
Gian Sanjaya <43656481+mortarsanjaya@users.noreply.github.com>
2023-06-18 22:10:01 -0400

    2017 C1: Small change

--- src/IMO2017/C1/C1.lean
+++ src/IMO2017/C1/C1.lean
@@ -8,8 +8,11 @@ namespace IMO2017C1
 open finset
 
 /-- The lattice rectangle `Ico a (a + l) × Ico b (b + w)`-/
-def lattice_rectangle (q : (ℕ × ℕ) × (ℕ × ℕ)) : finset (ℕ × ℕ) :=
+def lattice_rect (q : (ℕ × ℕ) × (ℕ × ℕ)) : finset (ℕ × ℕ) :=
   Ico q.1.1 (q.1.1 + q.2.1) ×ˢ Ico q.1.2 (q.1.2 + q.2.2)
+
+/-- The weight of a finset in `ℕ × ℕ` -/
+def weight (S : finset (ℕ × ℕ)) := S.sum (λ p, ite (even (p.1 + p.2)) 1 (-1 : ℤ))
 
 
 
@@ -32,11 +35,10 @@ lemma sum_ite_neg_one_one_alt_Ico (a b n : ℕ) :
     exact sum_congr rfl (λ x _, if_congr
       (iff_of_eq $ congr_arg even (add_assoc b a x).symm) rfl rfl)
 
-lemma rectangle_sign_alt_sum (q : (ℕ × ℕ) × (ℕ × ℕ)) :
-  (lattice_rectangle q).sum (λ p, ite (even (p.1 + p.2)) 1 (-1 : ℤ)) =
-    ite (even q.2.1 ∨ even q.2.2) 0 (ite (even (q.1.1 + q.1.2)) 1 (-1 : ℤ)) :=
+lemma rect_weight_eq (q : (ℕ × ℕ) × (ℕ × ℕ)) : weight (lattice_rect q)
+  = ite (even q.2.1 ∨ even q.2.2) 0 (ite (even (q.1.1 + q.1.2)) 1 (-1 : ℤ)) :=
 begin
-  rw [lattice_rectangle, sum_product],
+  rw [weight, lattice_rect, sum_product],
   simp_rw sum_ite_neg_one_one_alt_Ico,
   by_cases h : even q.2.2,
 
@@ -50,12 +52,11 @@ begin
   exact if_congr (or_iff_left h).symm rfl rfl
 end
 
-lemma rectangle_sum_pos_iff {q : (ℕ × ℕ) × (ℕ × ℕ)} :
-  0 < (lattice_rectangle q).sum (λ p, ite (even (p.1 + p.2)) 1 (-1 : ℤ)) ↔
-    (odd q.2.1 ∧ odd q.2.2) ∧ even (q.1.1 + q.1.2) :=
+lemma rect_weight_pos_iff {q : (ℕ × ℕ) × (ℕ × ℕ)} :
+  0 < weight (lattice_rect q) ↔ (odd q.2.1 ∧ odd q.2.2) ∧ even (q.1.1 + q.1.2) :=
 begin
   rw [nat.odd_iff_not_even, nat.odd_iff_not_even,
-      ← not_or_distrib, rectangle_sign_alt_sum],
+      ← not_or_distrib, rect_weight_eq],
   by_cases h : even q.2.1 ∨ even q.2.2,
   rw [if_pos h, lt_self_iff_false, iff_true_intro h, not_true, false_and],
   rw [if_neg h, iff_true_intro h, true_and],
@@ -70,18 +71,18 @@ end
 
 /-- Final solution -/
 theorem final_solution {ι : Type*} {I : finset ι} {Q : ι → (ℕ × ℕ) × (ℕ × ℕ)}
-  (h : (I : set ι).pairwise_disjoint (lattice_rectangle ∘ Q))
+  (h : (I : set ι).pairwise_disjoint (lattice_rect ∘ Q))
   {m n : ℕ} (h0 : odd m ∧ odd n)
-  (h1 : lattice_rectangle ⟨⟨0, 0⟩, ⟨m, n⟩⟩ = I.disj_Union (lattice_rectangle ∘ Q) h) :
+  (h1 : lattice_rect ⟨⟨0, 0⟩, ⟨m, n⟩⟩ = I.disj_Union (lattice_rect ∘ Q) h) :
   ∃ (i : ι) (h : i ∈ I), (odd (Q i).2.1 ∧ odd (Q i).2.2) ∧ even ((Q i).1.1 + (Q i).1.2) :=
 begin
   let f := λ p : ℕ × ℕ, ite (even (p.1 + p.2)) 1 (-1 : ℤ),
-  replace h0 : 0 < (lattice_rectangle ((0, 0), m, n)).sum f :=
-    rectangle_sum_pos_iff.mpr ⟨h0, even_zero⟩,
-  replace h1 : 0 < I.sum (λ i, (lattice_rectangle $ Q i).sum f) :=
+  replace h0 : 0 < (lattice_rect ((0, 0), m, n)).sum f :=
+    rect_weight_pos_iff.mpr ⟨h0, even_zero⟩,
+  replace h1 : 0 < I.sum (λ i, (lattice_rect $ Q i).sum f) :=
     lt_of_lt_of_eq h0 ((sum_congr h1 $ λ p _, rfl).trans (sum_disj_Union I _ h)),
   rcases exists_lt_of_sum_lt (lt_of_eq_of_lt sum_const_zero h1) with ⟨i, h2, h3⟩,
-  exact ⟨i, h2, rectangle_sum_pos_iff.mp h3⟩
+  exact ⟨i, h2, rect_weight_pos_iff.mp h3⟩
 end
 
 end IMO2017C1
