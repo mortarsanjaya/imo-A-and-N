c06968d05a8c15eef5ae944f13b6df93596680d1
Gian Sanjaya <43656481+mortarsanjaya@users.noreply.github.com>
2023-06-28 01:41:13 -0400

    2011 A6: Further optimization

--- src/IMO2011/A6/A6.lean
+++ src/IMO2011/A6/A6.lean
@@ -5,9 +5,6 @@ import algebra.order.ring.defs
 namespace IMOSL
 namespace IMO2011A6
 
-set_option profiler true
-set_option profiler.threshold 0.1
-
 /-- Final solution -/
 theorem final_solution {R : Type*} [linear_ordered_comm_ring R]
   {f : R → R} (h : ∀ x y : R, f (x + y) ≤ y * f x + f (f x)) :
@@ -16,12 +13,13 @@ begin
   ---- A sequence of results
   have h0 : ∀ t x : R, f (f t) ≤ (f t - x) * f x + f (f x) :=
     λ t x, (congr_arg f (add_sub_cancel'_right x _).symm).trans_le (h x _),
-  replace h0 : ∀ x : R, x * f x ≤ 0 := λ x, begin
-    have h1 := add_le_add (h0 (2 * f x) x) (h0 x (2 * f x)),
-    rwa [add_comm, add_add_add_comm, le_add_iff_nonneg_left, ← one_sub_mul, bit0,
-         sub_add_cancel', sub_mul, mul_comm, mul_assoc, neg_one_mul,
-         ← sub_eq_add_neg, sub_sub_cancel_left, neg_nonneg] at h1
-  end,
+  replace h0 : ∀ t x : R, 0 ≤ (f t - x) * f x + (f x - t) * f t :=
+    λ t x, nonneg_of_le_add_left $ (h0 t x).trans $
+      (add_le_add_left (h0 x t) _).trans_eq (add_assoc _ _ _).symm,
+  replace h0 : ∀ x : R, x * f x ≤ 0 :=
+    λ x, nonpos_of_neg_nonneg $ (h0 (2 * f x) x).trans_eq $
+      by rw [← one_sub_mul, mul_right_comm, ← add_mul, ← add_sub_right_comm,
+        one_sub_mul, ← add_sub_assoc, ← two_mul, sub_self, zero_sub, neg_mul],
 
   have h1 : ∀ x : R, f x ≤ f (f x) :=
     λ x, (congr_arg f (add_zero x).symm).trans_le $
@@ -30,7 +28,7 @@ begin
     λ x, le_of_not_lt $ λ h2, not_lt_of_le (h0 $ f x) $
       mul_pos h2 $ h2.trans_le $ h1 x,
   replace h0 : ∀ x : R, x < 0 → f x = 0 :=
-    λ x h3, (h2 x).antisymm (nonneg_of_mul_nonpos_right (h0 x) h3),
+    λ x h3, (h2 x).antisymm $ nonneg_of_mul_nonpos_right (h0 x) h3,
 
   ---- Finishing
   intros x h3,
