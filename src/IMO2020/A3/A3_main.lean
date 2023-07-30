ade377c4b4765a72a220c1def1bb65813de4d736
Gian Sanjaya <43656481+mortarsanjaya@users.noreply.github.com>
2023-07-30 19:24:48 -0400

    2020 A3: Fix error

--- src/IMO2020/A3/A3_main.lean
+++ src/IMO2020/A3/A3_main.lean
@@ -7,7 +7,8 @@ namespace IMO2020A3
 
 /-- Final solution for the real case -/
 theorem final_solution :
-  is_least ((λ x : fin4 → ℝ, target_val x) '' {x | (∀ i : fin4, 0 < x i) ∧ good x}) 8 :=
+  is_least ((λ x : extra.fin4 → ℝ, target_val x) ''
+    {x | (∀ i : extra.fin4, 0 < x i) ∧ good x}) 8 :=
   target_val_minimum (real.mul_self_sqrt zero_le_three) (real.sqrt_pos.mpr zero_lt_three)
 
 end IMO2020A3
