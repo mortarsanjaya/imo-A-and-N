8569bd4d5c3d5b16d71407cba182d784545e7145
Gian Sanjaya <43656481+mortarsanjaya@users.noreply.github.com>
2023-09-16 18:21:10 -0400

    2010 A1: Optimize

--- src/IMO2010/A1/A1.lean
+++ src/IMO2010/A1/A1.lean
@@ -9,40 +9,39 @@ variables {F : Type*} [linear_ordered_field F] [floor_ring F]
 
 /-- For any `r : F` with `1 < r`, we have `⌊r⁻¹⌋ = 0`. -/
 lemma inv_floor_eq_zero {r : F} (h : 1 < r) : ⌊r⁻¹⌋ = 0 :=
-  int.floor_eq_zero_iff.mpr
-    ⟨inv_nonneg.mpr $ le_of_lt $ lt_trans zero_lt_one h, inv_lt_one h⟩
+  int.floor_eq_zero_iff.mpr ⟨inv_nonneg.mpr (zero_lt_one.trans h).le, inv_lt_one h⟩
 
 
 
 /-- Final solution -/
 theorem final_solution {R : Type*} [linear_ordered_ring R] [floor_ring R] (f : F → R) :
-  (∀ x y : F, f (⌊x⌋ * y) = f x * ⌊f y⌋) ↔ ∃ C : R, (⌊C⌋ = 1 ∨ C = 0) ∧ f = λ _, C :=
-begin
-  ---- `→` direction
-  symmetry; refine ⟨λ h x y, _, λ h, ⟨f 0, _⟩⟩,
-  rcases h with ⟨C, h, rfl⟩,
-  rwa [eq_comm, mul_right_eq_self₀, int.cast_eq_one],
-
-  ---- `←` direction; the case `⌊f(0)⌋ = 1` is easier
-  have h0 := h 0 0,
-  rw [mul_zero, eq_comm, mul_right_eq_self₀, ← int.cast_one, int.cast_inj] at h0,
-  refine ⟨h0, _⟩; cases h0 with h0 h0,
-  funext x; replace h := h x 0,
-  rwa [mul_zero, h0, int.cast_one, mul_one, eq_comm] at h,
-
-  ---- Now work on the case `f(0) = 0`
-  suffices h1 : f 1 = 0,
-    funext x; rw h0; replace h := h 1 x;
-      rwa [h1, zero_mul, int.floor_one, int.cast_one, one_mul] at h,
-  suffices h1 : ⌊f 2⁻¹⌋ = 0,
-    replace h := h 2 2⁻¹; rwa [h1, int.cast_zero, mul_zero, ← int.cast_two,
-      int.floor_int_cast, int.cast_two, mul_inv_cancel (two_ne_zero : (2 : F) ≠ 0)] at h,
-  replace h := h 2⁻¹ 2⁻¹,
-  rw [inv_floor_eq_zero (one_lt_two : (1 : F) < 2), int.cast_zero, zero_mul, h0, zero_eq_mul] at h,
-  cases h with h h,
-  rw [h, int.floor_zero],
-  rwa int.cast_eq_zero at h
-end
+  (∀ x y : F, f (⌊x⌋ * y) = f x * ⌊f y⌋) ↔ (∃ C : R, ⌊C⌋ = 1 ∧ f = λ _, C) ∨ f = 0 :=
+---- `→`
+⟨λ h, (mul_right_eq_self₀.mp ((congr_arg f (mul_zero _).symm).trans (h 0 0)).symm).imp
+  ---- Case 1: `⌊f(0)⌋ = 1`
+  (λ h0, ⟨f 0, int.cast_eq_one.mp h0, funext $ λ x,
+    (mul_one _).symm.trans $ h0 ▸ (h x 0).symm.trans $ congr_arg f (mul_zero _)⟩)
+  ---- Case 2: `f(0) = 0`
+  -- First reduce to `f(1) = 0`
+  (λ h0, suffices f 1 = 0,
+    from funext $ λ y, let h1 := (h 1 y).trans (mul_eq_zero_of_left this _) in
+      suffices (⌊(1 : F)⌋ : F) = 1, from one_mul y ▸ this ▸ h1,
+    int.cast_eq_one.mpr int.floor_one,
+  -- Now reduce to `⌊f(1/2)⌋ = 0`
+  suffices ⌊f 2⁻¹⌋ = 0, 
+    from let h1 := (h 2 2⁻¹).trans (mul_eq_zero_of_right _ $ int.cast_eq_zero.mpr this) in
+      suffices (⌊(2 : F)⌋ : F) * 2⁻¹ = 1, from this ▸ h1,
+    (mul_inv_eq_one₀ two_ne_zero).mpr $ (int.cast_two : ((2 : ℤ) : F) = 2) ▸
+      congr_arg coe $ int.floor_int_cast 2,
+  -- Now prove that `⌊f(1/2)⌋ = 0`
+  (mul_eq_zero.mp $ (h 2⁻¹ 2⁻¹).symm.trans $ (congr_arg f $ mul_eq_zero_of_left
+      (int.cast_eq_zero.mpr $ inv_floor_eq_zero one_lt_two) _).trans h0).elim
+    (λ h1, h1.symm ▸ int.floor_zero) int.cast_eq_zero.mp),
+---- `←`
+λ h x y, h.elim
+  (λ h, exists.elim h $ λ C h, h.2.symm ▸ h.1.symm ▸
+    (int.cast_one : ((1 : ℤ) : R) = 1).symm ▸ (mul_one C).symm)
+  (λ h, h.symm ▸ (zero_mul _).symm)⟩
 
 end IMO2010A1
 end IMOSL
