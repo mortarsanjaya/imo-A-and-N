0c7aba0063289913dfb6a8db914cc7b1a07845c0
Gian Sanjaya <43656481+mortarsanjaya@users.noreply.github.com>
2023-09-20 01:10:18 -0400

    2007 A1: Optimize

--- src/IMO2007/A1/A1.lean
+++ src/IMO2007/A1/A1.lean
@@ -1,4 +1,4 @@
-import algebra.group.defs data.finset.lattice
+import data.finset.lattice
 
 /-! # IMO 2007 A1 (P1) -/
 
@@ -13,19 +13,11 @@ variables {G : Type*} [linear_ordered_add_comm_group G] (a : ℕ → G) (n : ℕ
 theorem final_solution_part1 {x : ℕ → G} (h : monotone x) :
   ∀ {k m : ℕ}, k ≤ m → m ≤ n →
     a k - a m ≤ 2 • (range n.succ).sup' nonempty_range_succ (λ i, |x i - a i|) :=
-let L := (range n.succ).sup' nonempty_range_succ (λ i, |x i - a i|) in
-λ k m h0 h1, by calc
-  a k - a m ≤ (a k - a m) - (x k - x m) :
-    (le_sub_self_iff _).mpr $ sub_nonpos_of_le $ h h0
-  ... = -(x k - a k) + (x m - a m) :
-    by rw [neg_sub, sub_sub_sub_comm, sub_sub_eq_add_sub, add_sub_assoc]
-  ... ≤ |x k - a k| + |x m - a m| :
-    add_le_add (neg_le_abs_self _) (le_abs_self _)
-  ... ≤ L + L :
-    let h2 : ∀ k, k ≤ n → |x k - a k| ≤ L :=
-      λ k h2, le_sup' (λ i, |x i - a i|) (mem_range_succ_iff.mpr h2) in
-    add_le_add (h2 k $ h0.trans h1) (h2 m h1)
-  ... = 2 • L : (two_nsmul L).symm
+λ k m h0 h1, ((le_sub_self_iff _).mpr $ sub_nonpos_of_le (h h0)).trans $
+  (sub_sub_sub_comm _ _ _ _).trans_le $ (le_abs_self _).trans $ (abs_sub _ _).trans $
+suffices ∀ k, k ≤ n → |a k - x k| ≤ (range n.succ).sup' nonempty_range_succ (λ i, |x i - a i|),
+  from (add_le_add (this k $ h0.trans h1) (this m h1)).trans_eq (two_nsmul _).symm,
+λ k h2, (abs_sub_comm _ _).trans_le $ le_sup' (λ i, |x i - a i|) (mem_range_succ_iff.mpr h2)
 
 
 
@@ -35,22 +27,15 @@ theorem final_solution_part2 {g : G} (h : ∀ k m : ℕ, k ≤ m → m ≤ n →
 let x : ℕ → G := λ i : ℕ, (range i.succ).sup' nonempty_range_succ a - g in
 ⟨x, λ i j h0, sub_le_sub_right (sup'_le _ a $ λ t h1, le_sup' _ $
   mem_range.mpr $ (mem_range.mp h1).trans_le $ nat.succ_le_succ h0) g,
-begin
-  refine le_antisymm (sup'_le _ _ $ λ i h0, abs_le.mpr ⟨_, _⟩)
-    (le_sup'_of_le _ (mem_range_succ_iff.mpr n.zero_le) _),
-  ---- `-g ≤ x_i - a_i` for all `i ≤ n`
-  { rw [neg_le_sub_iff_le_add, sub_add_cancel],
-    exact le_sup' a (self_mem_range_succ i) },
-  ---- `x_i - a_i ≤ g` for all `i ≤ n`
-  { obtain ⟨j, h1, h2⟩ := exists_mem_eq_sup' (nonempty_range_succ : (range i.succ).nonempty) a,
-    rw [sub_sub, h2, add_comm, ← sub_sub, sub_le_iff_le_add, ← two_nsmul],
-    rw mem_range_succ_iff at h0 h1,
-    exact h j i h1 h0 },
-  ---- `|x_0 - a_0| ≥ g` via `a_0 - x_0 = g`
-  { refine (neg_le_abs_self _).trans_eq' _,
-    rw [neg_sub, eq_sub_iff_add_eq', sub_add_cancel,
-      sup'_congr _ range_one (λ _ _, rfl), sup'_singleton] }
-end⟩
+le_antisymm (sup'_le _ _ $ λ i h0, abs_le.mpr
+  ⟨neg_le_sub_iff_le_add.mpr $ (sub_add_cancel _ _).ge.trans' $
+    le_sup' a (self_mem_range_succ i),
+  exists.elim (exists_mem_eq_sup' (nonempty_range_succ : (range i.succ).nonempty) a) $
+    λ j h1, (sub_right_comm _ _ _).trans_le $ sub_le_iff_le_add.mpr $ h1.2.symm ▸
+      two_nsmul g ▸ h j i (mem_range_succ_iff.mp h1.1) (mem_range_succ_iff.mp h0)⟩)
+(le_sup'_of_le _ (mem_range_succ_iff.mpr n.zero_le) $ (neg_le_abs_self _).trans_eq' $ 
+  eq.symm $ (neg_sub _ _).trans $ sub_eq_iff_eq_add'.mpr $ eq.symm $
+    (sub_add_cancel _ _).trans $ sup'_congr _ range_one $ λ _ _, rfl)⟩
 
 end IMO2007A1
 end IMOSL
