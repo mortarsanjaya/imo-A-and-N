2f7864550d0d5bb0ae588a926b090e92652f1939
Gian Sanjaya <43656481+mortarsanjaya@users.noreply.github.com>
2023-09-24 13:51:05 -0400

    2019 A1: Optimize

--- src/IMO2019/A1/A1.lean
+++ src/IMO2019/A1/A1.lean
@@ -1,4 +1,4 @@
-import data.int.order.basic algebra.ring.regular
+import data.int.order.basic
 
 /-! # IMO 2019 A1 (P1) -/
 
@@ -8,38 +8,33 @@ namespace IMO2019A1
 /-- Final solution -/
 theorem final_solution {N : ℤ} (h : N ≠ 0) (f : ℤ → ℤ) :
   (∀ a b : ℤ, f (N * a) + N * f b = f (f (a + b))) ↔
-    f = 0 ∨ ∃ c : ℤ, f = λ n, N * n + c :=
-begin
-  ---- Easier direction: `←`
-  symmetry; refine ⟨λ h0 a b, _, λ h0, _⟩,
-  rcases h0 with rfl | ⟨c, rfl⟩,
-  rw [pi.zero_apply, zero_add, pi.zero_apply, mul_zero, pi.zero_apply],
-  rw [add_right_comm, ← mul_add, ← add_assoc, ← mul_add],
-
-  ---- Harder direction: `→`; start with proving linearity
-  have h1 : ∀ n : ℤ, N * (f (n + 1) - f n) = f N - f 0 :=
-    λ n, by rw [mul_sub, ← mul_zero N, sub_eq_sub_iff_add_eq_add,
-      add_comm, h0, zero_add, add_comm, ← h0, mul_one],
-  replace h1 : ∀ n : ℤ, f (n + 1) - f n = f 1 - f 0 :=
-    λ n, int.eq_of_mul_eq_mul_left h (by rw [h1, ← h1 0, zero_add]),
-  replace h1 : ∃ q c : ℤ, f = λ n, q * n + c :=
-  begin
-    refine ⟨f 1 - f 0, f 0, funext (λ n, int.induction_on' n 0 _ _ _)⟩,
-    rw [mul_zero, zero_add],
-    rintros k - h2; rw [mul_add_one, add_right_comm, ← h2, ← h1 k, add_sub_cancel'_right],
-    rintros k - h2; rw [mul_sub_one, ← add_sub_right_comm,
-      ← h2, ← h1 (k - 1), sub_add_cancel, sub_sub_cancel]
-  end,
-
-  ---- Now prove that either `q = N` or `q = c = 0`
-  rcases h1 with ⟨q, c, rfl⟩,
-  rcases eq_or_ne N q with rfl | h1,
-  right; use c,
-  left; funext n,
-  replace h0 := h0 n 0,
-  rwa [add_right_comm, add_zero, add_left_inj, mul_left_comm, ← mul_add,
-    ← add_assoc, ← mul_add, add_zero, mul_eq_mul_right_iff, or_iff_right h1] at h0
-end
+    (f = λ _, 0) ∨ ∃ c : ℤ, f = λ n, N * n + c :=
+---- `→` direction
+⟨λ h0, suffices ∃ q, ∀ n, f n = q * n + f 0,
+  from exists.elim this $ λ q h1, or.symm $ (eq_or_ne q N).imp (λ h2, ⟨f 0, h2 ▸ funext h1⟩) $
+    λ h2, funext $ λ n, suffices (N - q) * f n = 0,
+      from (int.eq_zero_or_eq_zero_of_mul_eq_zero this).resolve_left (sub_ne_zero_of_ne h2.symm),
+    suffices N * f n = q * f n, from (N.sub_mul q _).trans (sub_eq_zero_of_eq this),
+    add_right_injective (f (N * 0)) $ (h0 0 n).trans $ (h1 _).trans $ (add_comm _ _).trans $
+      congr_arg2 _ (congr_arg f N.mul_zero.symm) (n.zero_add.symm ▸ rfl),
+-- Now prove linearity
+suffices ∀ n : ℤ, f (n + 1) = f n + (f 1 - f 0),
+  from let q := f 1 - f 0 in ⟨q, λ n, int.induction_on' n 0
+    (q.mul_zero.symm ▸ (f 0).zero_add.symm)
+    (λ n _ h1, (this n).trans $ h1.symm ▸ (mul_add_one q n).symm ▸ add_right_comm _ _ _)
+    (λ n _ h1, (mul_sub_one q n).symm ▸ add_sub_right_comm (q * n) (f 0) q ▸ h1 ▸
+        eq_sub_of_add_eq ((this _).symm.trans $ congr_arg f $ sub_add_cancel n 1))⟩,
+have h1 : ∀ n : ℤ, N * (f (n + 1) - f n) = f N - f 0,
+  from λ n, (N.mul_sub _ _).trans $ sub_eq_sub_iff_add_eq_add.mpr $
+    (add_comm _ _).trans $ N.mul_zero ▸ (h0 _ _).trans
+    ((n + 1).zero_add.symm ▸ add_comm 1 n ▸ (h0 _ _).symm.trans (N.mul_one.symm ▸ rfl)),
+λ n, have h2 : N * (f (n + 1) - f n - (f (0 + 1) - f 0)) = 0,
+  from (N.mul_sub _ _).trans $ sub_eq_zero_of_eq $ (h1 n).trans (h1 0).symm,
+eq_add_of_sub_eq' $ eq_of_sub_eq_zero $ (int.eq_zero_or_eq_zero_of_mul_eq_zero h2).resolve_left h,
+---- `←` direction
+λ h a b, h.elim (λ h, h.symm ▸ (N * 0).zero_add.trans N.mul_zero) $
+  λ h, exists.elim h $ λ c h, h.symm ▸ (add_right_comm _ _ _).trans
+    (mul_add N (N * a) (N * b + c) ▸ (N * a).add_assoc (N * b) c ▸ mul_add N a b ▸ rfl)⟩
 
 end IMO2019A1
 end IMOSL
