ce34b6a9475cead1b22ace41fd0770316b09b9e4
Gian Sanjaya <43656481+mortarsanjaya@users.noreply.github.com>
2023-09-24 14:24:12 -0400

    2021 N2: Slight optimization?

--- src/IMO2021/N2/N2.lean
+++ src/IMO2021/N2/N2.lean
@@ -17,9 +17,10 @@ lemma good_cond1 {n} (a b c k l m) (h : n ≤ a) (h0 : a < b) (h1 : b < c) (h2 :
       (λ h7, ⟨a, c, h, h0.trans h1, h2, h6.trans h7.symm, l, h4⟩)
 
 lemma good_cond2 {n} (h : ∃ k, n ≤ 2 * k ^ 2 + 4 * k ∧ k ^ 2 + 6 * k + 8 ≤ n) : good n :=
-  exists.elim h $ λ k h,  good_cond1 (2 * k ^ 2 + 4 * k) (2 * k ^ 2 + 8 * k + 9)
+  exists.elim h $ λ k h, good_cond1 (2 * k ^ 2 + 4 * k) (2 * k ^ 2 + 8 * k + 9)
     (2 * (k ^ 2 + 6 * k + 8)) (2 * k + 3) (2 * k + 4) (2 * k + 5)
-    h.1 (nat.lt_succ_of_le $ le_iff_exists_add.mpr ⟨4 * k + 8, by ring⟩)
+    h.1 (nat.lt_succ_of_le $ le_iff_exists_add.mpr ⟨4 * k + 8,
+      (add_mul 4 4 k).symm ▸ (2 * k ^ 2).add_assoc (4 * k) (4 * k) ▸ add_assoc _ _ _⟩)
     (nat.lt_of_succ_le $ le_iff_exists_add.mpr
       ⟨4 * k + 6, (2 * k ^ 2 + 8 * k + 9).succ_eq_add_one.symm ▸ by ring⟩)
     (nat.mul_le_mul_left 2 h.2) (by ring) (by ring) (by ring)
@@ -32,31 +33,21 @@ lemma good_cond2 {n} (h : ∃ k, n ≤ 2 * k ^ 2 + 4 * k ∧ k ^ 2 + 6 * k + 8 �
 theorem final_solution {n : ℕ} (h : 99 ≤ n) : good n :=
 good_cond2 $ nat.le_induction
   ⟨7, nat.add_le_add_left (nat.succ_pos 27) 98, nat.le_refl 99⟩
-  (λ n h h0, begin
-    rcases h0 with ⟨k, h0, h1⟩,
-    rw le_iff_lt_or_eq at h0,
-    rcases h0 with h0 | rfl,
-    ---- Case 1: `n < 2k^2 + 4k`
-    exact ⟨k, h0, h1.trans n.le_succ⟩,
-    ---- Case 2: `n = 2k^2 + 4k`
-    refine ⟨k + 1, le_iff_exists_add.mpr ⟨4 * k + 5, by ring⟩, _⟩,
-    calc (k + 1) ^ 2 + 6 * (k + 1) + 8 = (4 * k + 14) + (k ^ 2 + 4 * k + 1) : by ring
-    ... ≤ k ^ 2 + (k ^ 2 + 4 * k + 1) : nat.add_le_add_right _ _
-    ... = 2 * k ^ 2 + 4 * k + 1 : by ring,
-
-    ---- It remains to show that if `99 ≤ 2k^2 + 4k`, then `4k + 14 ≤ k^2`
-    suffices : 7 ≤ k,
-    { rw [bit0_eq_two_mul 7, sq],
-      calc 4 * k + 2 * 7 ≤ 4 * k + 2 * k :
-        nat.add_le_add_left (nat.mul_le_mul_left 2 this) _
-      ... = 6 * k : (add_mul 4 2 k).symm
-      ... ≤ k * k : k.mul_le_mul_right ((nat.le_succ 6).trans this) },
-  
-    refine nat.succ_le_of_lt (lt_of_not_le $ λ h2, h.not_lt _),
-      calc _ ≤ 2 * 6 ^ 2 + 4 * 6 : nat.add_le_add
-      (nat.mul_le_mul_left _ $ nat.pow_le_pow_of_le_left h2 2) (nat.mul_le_mul_left 4 h2)
-    ... < 99 : by norm_num
-  end) n h
+  (λ n h h0, exists.elim h0 $ λ k h0, suffices 4 * k + 14 ≤ k ^ 2,
+    from h0.1.lt_or_eq.elim (λ h1, ⟨k, h1, h0.2.trans n.le_succ⟩) $
+      λ h1, h1.symm ▸ ⟨k + 1, le_iff_exists_add.mpr ⟨4 * k + 5, by ring⟩,
+        by calc (k + 1) ^ 2 + 6 * (k + 1) + 8 = (4 * k + 14) + (k ^ 2 + 4 * k + 1) : by ring
+        ... ≤ k ^ 2 + (k ^ 2 + 4 * k + 1) : nat.add_le_add_right this _
+        ... = 2 * k ^ 2 + 4 * k + 1 : (add_assoc _ _ _).symm.trans $
+          (two_mul $ k ^ 2).symm ▸ congr_arg nat.succ $ ((k ^ 2).add_assoc _ _).symm⟩,
+  -- Now show that if `99 ≤ 2k^2 + 4k`, then `4k + 14 ≤ k^2`
+  suffices 7 ≤ k, from (sq k).ge.trans' $
+    (nat.add_le_add_left (nat.add_le_add this this) _).trans $
+    ((4 * k).add_assoc k k).symm.trans_le $ nat.succ_mul 4 k ▸ nat.succ_mul 5 k ▸
+      k.mul_le_mul_right ((nat.le_succ 6).trans this),
+  nat.succ_le_of_lt $ lt_of_not_le $ λ h2, h.not_lt $ nat.lt_succ_of_le $ h0.1.trans $
+    (nat.add_le_add (nat.mul_le_mul_left 2 $ nat.pow_le_pow_of_le_left h2 2)
+      (nat.mul_le_mul_left 4 h2)).trans $ (nat.bit0_le $ nat.le_succ 48)) n h
 
 end IMO2021N2
 end IMOSL
