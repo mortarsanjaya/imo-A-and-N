4d4de2929030d1255d475dc4930f463d5e8008d1
Gian Sanjaya <43656481+mortarsanjaya@users.noreply.github.com>
2023-10-17 16:44:29 -0400

    2015 A4: Optimization

--- src/IMO2015/A4/A4.lean
+++ src/IMO2015/A4/A4.lean
@@ -1,153 +1,135 @@
-import algebra.char_p.two
+import algebra.ring.regular
 
 /-! # IMO 2015 A4 (P5) -/
 
 namespace IMOSL
 namespace IMO2015A4
 
-def good {R : Type*} [ring R] (f : R → R) :=
+variables {R : Type*} [ring R]
+
+def good (f : R → R) :=
   ∀ x y : R, f (x + f (x + y)) + f (x * y) = x + f (x + y) + f x * y
 
 
 
-section basic_results
-
-variables {R : Type*} [ring R]
-
-private lemma good_id : good (id : R → R) :=
+lemma good_id : good (id : R → R) :=
   λ x y, rfl
 
-private lemma good_two_sub : good (has_sub.sub (2 : R)) :=
-  λ x y, by rw [add_sub_left_comm x, sub_add_cancel', sub_add_cancel', neg_neg,
-    add_sub_left_comm, add_assoc, add_right_inj, ← one_sub_mul, ← neg_one_mul,
-    ← add_mul, ← add_sub_assoc, bit0, neg_add_cancel_comm_assoc]
-
-end basic_results
-
-
+lemma good_two_sub : good (has_sub.sub (2 : R)) :=
+  λ x y, add_sub_assoc x 2 (x + y) ▸ (add_sub_add_left_eq_sub 2 y x).symm ▸
+    (sub_sub_cancel 2 y).symm ▸ (sub_mul 2 x y).symm ▸ add_sub_assoc y 2 (x * y) ▸ 
+    add_sub_assoc (2 - y) (2 * y) (x * y) ▸ (two_mul y).symm ▸
+    congr_arg2 _ ((add_comm y 2).trans (sub_add_add_cancel 2 y y).symm) rfl
+ 
 
 
+variables [is_domain R] {f : R → R}
 
-
-
-/-- Final solution, case 1: `char(R) ≠ 2` -/
-theorem final_solution_char_ne_two {R : Type*} [ring R] [is_domain R] (Rchar : ring_char R ≠ 2) :
-  ∀ f : R → R, good f ↔ f = id ∨ f = has_sub.sub 2 :=
-begin
-  ---- `←` direction has been done
-  intros f; symmetry; refine ⟨λ h, _, λ h, _⟩,
-  { rcases h with rfl | rfl,
-    exacts [good_id, good_two_sub] },
-
-  ---- One important equality and the case `f(0) ≠ 0`
-  have h0 : ∀ t : R, f (t + f (t + 1)) = t + f (t + 1) :=
-    λ t, by replace h := h t 1; rwa [mul_one, mul_one, add_left_inj] at h,
-  cases ne_or_eq (f 0) 0 with h1 h1,
-  right; funext x,
-  replace h := h 0 (x - 1 + f (x - 1 + 1)),
-  rwa [zero_add, zero_add, zero_mul, h0, h0, add_right_inj, sub_add_cancel, eq_comm,
-       ← sub_eq_zero, ← mul_sub_one, mul_eq_zero, or_iff_right h1, sub_eq_zero,
-       ← add_sub_right_comm, sub_eq_iff_eq_add, ← eq_sub_iff_add_eq'] at h,
-
-  ---- The main case, `f(0) = 0`
-  ---- First obtain `f(1) = 1` and reduce to `f` being odd
-  left; have h2 := h0 (-1),
-  rw [neg_add_self, h1, add_zero] at h2,
-  replace h2 : f 1 = 1 :=
+lemma good_char_ne_two_imp (h : good f) (h0 : (2 : R) ≠ 0) :
+  f = has_sub.sub 2 ∨ f = id :=
+---- (1)
+have h1 : ∀ t : R, f (t + f (t + 1)) = t + f (t + 1),
+  from λ t, add_left_injective (f (t * 1)) $ (h t 1).trans $ congr_arg2 _ rfl $
+    (mul_one _).trans $ congr_arg f (mul_one t).symm,
+(ne_or_eq (f 0) 0).imp
+---- Case `f(0) ≠ 0`
+(λ h2, suffices ∀ t, t + f (t + 1) = 1 ∨ f 0 = 0, from funext $ λ t, sub_add_cancel t 1 ▸
+  (add_sub_add_right_eq_sub 1 (t - 1) 1).symm ▸ eq_sub_of_add_eq' ((this _).resolve_right h2),
+λ t, mul_right_eq_self₀.mp $ add_right_injective _ $ (h _ _).symm.trans $
+  congr_arg2 _ (let y := t + f (t + 1) in (zero_add y).symm ▸
+    (zero_add (f y)).symm ▸ congr_arg f (h1 t)) (congr_arg f $ zero_mul _))
+---- Case `f(0) = 0`
+(λ h2, have h3 : f (-1) = -1 := add_zero (-1 : R) ▸ h2 ▸ neg_add_self (1 : R) ▸ h1 _,
+-- `f(1) = 1`
+have h4 : f 1 = 1 :=
+  let h4 := h 1 (-1) in by
+  rwa [add_neg_self, h2, add_zero, one_mul, h3, mul_neg_one,
+    add_neg_eq_iff_eq_add, add_right_comm, eq_add_neg_iff_add_eq,
+    ← two_mul, ← two_mul, mul_eq_mul_left_iff, or_iff_left h0] at h4,
+-- Reduce to `f` being odd
+suffices ∀ x : R, f (-x) = -f x,
+  from funext $ λ x,
   begin
-    replace h := h 1 (-1),
-    rwa [add_neg_self, h1, add_zero, one_mul, h2, mul_neg_one, add_neg_eq_iff_eq_add,
-         add_right_comm, eq_add_neg_iff_add_eq, ← two_mul, ← two_mul,
-         mul_eq_mul_left_iff, or_iff_left (ring.two_ne_zero Rchar)] at h,
-  end,
-  suffices h3 : ∀ x : R, f (-x) = -f x,
-  { funext x,
-    replace h0 := h 1 x,
-    rw [one_mul, h2, one_mul] at h0,
-    replace h1 := h (-1) (-x),
-    rw [← neg_add, h3, ← neg_add, h3, h3, h2, neg_mul_neg, one_mul] at h1,
-    generalize_hyp : 1 + f (1 + x) = c at h0 h1,
-    replace h0 := congr_arg2 has_add.add h0 h1,
+    have h5 := h 1 x,
+    rw [one_mul, h4, one_mul] at h5,
+    have h6 := h (-1) (-x),
+    rw [← neg_add, this, ← neg_add, this, this, h4, neg_mul_neg, one_mul] at h6,
+    have h7 := congr_arg2 has_add.add h5 h6,
     rwa [add_add_add_comm, add_neg_self, zero_add, add_add_add_comm, add_neg_self, zero_add,
-         ← two_mul, ← two_mul, mul_eq_mul_left_iff, or_iff_left (ring.two_ne_zero Rchar)] at h0 },
-  
-  ---- Now prove that `f` is odd
-  intros x,
+         ← two_mul, ← two_mul, mul_eq_mul_left_iff, or_iff_left h0] at h7
+  end,
+-- Prove that `f` is odd
+λ x, begin
   rw [← mul_neg_one, ← add_right_inj (f (x + f (x + -1))), h,
       mul_neg_one, add_left_inj, eq_comm, ← sub_eq_add_neg],
   obtain ⟨y, rfl⟩ : ∃ y : R, y + (1 + 1) = x := ⟨x - (1 + 1), sub_add_cancel x _⟩,
   rw [add_sub_assoc, add_sub_cancel, ← bit0, add_right_comm],
-  replace h0 := h0 y,
-  have h3 := h (y + 1) 0,
-  rw [add_zero, mul_zero, mul_zero, add_zero, h1, add_zero, add_right_comm] at h3,
-  generalize_hyp : y + f (y + 1) = t at h0 h3 ⊢; clear y,
-  replace h := h 1 t,
-  rwa [h2, one_mul, h0, add_left_inj, add_comm 1 t, h3, add_left_comm] at h
-end
+  replace h1 := h1 y,
+  have h5 := h (y + 1) 0,
+  rw [add_zero, mul_zero, mul_zero, add_zero, h2, add_zero, add_right_comm] at h5,
+  let t := y + f (y + 1),
+  have h6 := h 1 t,
+  rwa [h4, one_mul, h1, add_left_inj, add_comm 1 t, h5, add_left_comm] at h6
+end)
 
-
-
-/-- Final solution, case 2: `char(R) = 2` -/
-theorem final_solution_char_eq_two {R : Type*} [ring R] [is_domain R] [char_p R 2] :
-  ∀ f : R → R, good f ↔ f = id :=
+lemma good_char_eq_two_imp (h : good f) (h0 : (2 : R) = 0) : f = id :=
+---- Work with `g = x ↦ f(x) - x` instead of `f`
+let g := λ x, f x - x in
+suffices ∀ x, g x = 0, from funext $ λ x, eq_of_sub_eq_zero (this x),
+have h : ∀ x y, g (y + g (x + y)) = g x * y - g (x * y) :=
+  λ x y, suffices x + f (x + y) = y + (f (x + y) - (x + y)),
+    from this ▸ (sub_eq_sub_iff_add_eq_add.mpr $ (h x y).trans (add_comm _ _)).trans
+      ((sub_mul (f x) x y).symm ▸ (sub_sub_sub_cancel_right (f x * y) (f (x * y)) (x * y)).symm),
+  (add_comm _ _).trans $ add_sub_left_comm (f (x + y)) y (x + y) ▸ congr_arg2 _ rfl $
+    eq_sub_of_add_eq $ (add_assoc x x y).symm.trans $ add_left_eq_self.mpr $
+    (two_mul x).symm.trans (mul_eq_zero_of_left h0 x),
+---- Reduce into (3) and (4)
+suffices (∀ t : R, g (t + 1) * (t + 1) = g t * t) ∧ ∀ t : R, g (g t * t) = g t,
+  from λ x, let h1 := this.1 x,
+    h2 := (this.2 (x + 1)).symm.trans $ (congr_arg g h1).trans (this.2 x) in
+  by rwa [h2, mul_add_one, add_right_eq_self] at h1,
+---- Set-up `g(0) = 0` and `g(1) = 0`
+have h1 : g 0 = 0 ∧ g 1 = 0 :=
 begin
-  ---- Easy case and changing `f` to `g + id`
-  intros f; symmetry; refine ⟨λ h, _, λ h, _⟩,
-  rw h; exact good_id,
-  unfold good at h,
-  obtain ⟨g, rfl⟩ : ∃ g : R → R, g + id = f := ⟨f - id, sub_add_cancel f id⟩,
-  rw add_left_eq_self; simp_rw [pi.add_apply, id.def] at h,
-  conv at h { find (_ = _) { rw [← add_assoc, add_mul, ← add_assoc _ _ (x * y),
-    add_left_inj, add_right_comm _ _ (g (x * y)), add_comm, add_right_inj,
-    add_left_comm, ← add_assoc x, char_two.add_self_eq_zero, zero_add] } },
-  
-  ---- Reduce into two equalities, then obtain `g(0) = 0` and `g(1) = 0`
-  suffices : (∀ t : R, g (t + 1) * (t + 1) = g t * t) ∧ ∀ t : R, g (g t * t) = g t,
-  { cases this with h0 h1,
-    funext t,
-    have h2 := h1 t,
-    rw [← h0, h1] at h2,
-    replace h0 := h0 t,
-    rwa [h2, mul_add_one, add_right_eq_self] at h0 },
-  have h0 := h 1 1,
-  rw [mul_one, mul_one, add_left_eq_self, char_two.add_self_eq_zero] at h0,
-  have h1 := h 0 (g 0 + 1),
-  rw [zero_mul, zero_add, h0, zero_add, h0, zero_add, mul_add_one, self_eq_add_left, ← sq] at h1,
-  replace h1 := pow_eq_zero h1,
-  rw [h1, zero_add] at h0,
-  refine ⟨λ t, _, λ t, _⟩,
+  have h1 := h 1 1,
+  rw [mul_one, mul_one, sub_self, ← bit0, h0, add_comm] at h1,
+  have h2 := h 0 (g 0 + 1),
+  rw [zero_mul, zero_add, h1, add_zero, h1, mul_add_one, add_sub_cancel, zero_eq_mul_self] at h2,
+  rw [h2, zero_add] at h1,
+  exact ⟨h2, h1⟩
+end,
+---- (3)
+⟨λ t, begin
+  have h : ∀ t : R, g (t + 1) + g (t * (t + 1)) = g t * (t + 1) :=
+    λ t, add_eq_of_eq_sub $ h t (t + 1) ▸ add_assoc t t 1 ▸ two_mul t ▸
+      (mul_eq_zero_of_left h0 t).symm ▸ (zero_add (1 : R)).symm ▸
+      h1.2.symm ▸ congr_arg g (add_zero _).symm,
+  have h2 := h (t + 1),
+  rw [add_assoc, ← bit0, h0, add_zero] at h2,
+  rw [mul_add_one, ← h2, add_one_mul, ← mul_add_one, add_assoc, add_comm (g (t * _)),
+    h, mul_add_one, add_left_comm, ← two_mul, mul_eq_zero_of_left h0, add_zero]
+end,
+---- (4)
+λ t, begin
+  have h2 := h t 0,
+  rw [add_zero, zero_add, mul_zero, mul_zero, h1.1, sub_self] at h2,
+  have h3 := h 0 t,
+  rw [zero_add, zero_mul, h1.1, zero_mul, sub_self, add_comm] at h3,
+  have h4 := h (g t) t,
+  rw [h3, add_zero, h2, zero_mul, zero_sub, ← neg_one_mul,
+      neg_eq_of_add_eq_zero_left h0, one_mul] at h4,
+  exact h4.symm
+end⟩
 
-  ---- The first equality
-  { replace h : ∀ t : R, g (t + 1) + g (t * (t + 1)) = g t * (t + 1) :=
-      λ t, by convert h t (t + 1) using 2;
-        rw [← add_assoc t, char_two.add_self_eq_zero, zero_add, h0, zero_add],
-    replace h0 := h (t + 1),
-    rw [add_assoc, char_two.add_self_eq_zero, add_zero] at h0,
-    rw [mul_add_one, ← h0, add_one_mul, ← mul_add_one, add_assoc, add_comm (g (t * _)),
-        h, mul_add_one, add_left_comm, char_two.add_self_eq_zero, add_zero] },
 
-  ---- The second equality
-  { replace h0 := h t 0,
-    rw [add_zero, add_zero, mul_zero, h1, add_zero, mul_zero] at h0,
-    have h2 := h 0 t,
-    rw [zero_add, zero_mul, h1, zero_mul, add_zero] at h2,
-    replace h1 := h (g t) t,
-    rw [h2, zero_add, h0, zero_mul, add_eq_zero_iff_eq_neg, char_two.neg_eq] at h1,
-    exact h1.symm }
-end
 
 
 
 /-- Final solution -/
-theorem final_solution {R : Type*} [ring R] [is_domain R] :
-  ∀ f : R → R, good f ↔ f = id ∨ f = has_sub.sub 2 :=
-begin
-  cases ne_or_eq (ring_char R) 2 with h h,
-  exact final_solution_char_ne_two h,
-  haveI : char_p R 2 := ring_char.of_eq h,
-  simp_rw [char_two.two_eq_zero, char_two.sub_eq_add', zero_add_eq_id, or_self],
-  exact final_solution_char_eq_two
-end
+theorem final_solution : good f ↔ f = (λ x, 2 - x) ∨ f = id :=
+⟨λ h, (ne_or_eq (2 : R) 0).elim (good_char_ne_two_imp h)
+  (λ h0, or.inr $ good_char_eq_two_imp h h0),
+λ h, h.elim (λ h, h.symm ▸ good_two_sub) (λ h, h.symm ▸ good_id)⟩
 
 end IMO2015A4
 end IMOSL
