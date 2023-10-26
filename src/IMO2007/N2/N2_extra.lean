01e76f119137179c27e5f87cd6032af45fa25e0b
Gian Sanjaya <43656481+mortarsanjaya@users.noreply.github.com>
2023-10-26 18:26:48 -0400

    2007 N2: Optimization

--- src/IMO2007/N2/N2_extra.lean
+++ src/IMO2007/N2/N2_extra.lean
@@ -5,43 +5,37 @@ import number_theory.legendre_symbol.quadratic_char.basic
 namespace IMOSL
 namespace IMO2007N2
 
-variables {F : Type*} [field F] [fintype F]
+variables {F : Type*} [field F] [fintype F] [decidable_eq F]
 
-private lemma not_is_square_ne_zero {a : F} (h : ¬is_square a) : a ≠ 0 :=
-  λ h0, h $ cast (congr_arg is_square h0.symm) (is_square_zero F)
-
-private lemma is_square_or_mul [decidable_eq F] (a b : F) :
+lemma is_square_or_mul (a b : F) :
   is_square a ∨ is_square b ∨ is_square (a * b) :=
-begin
-  rw [or_iff_not_imp_left, or_iff_not_imp_left]; intros h h0,
-  have h1 : a * b ≠ 0 := mul_ne_zero (not_is_square_ne_zero h) (not_is_square_ne_zero h0),
-  rw ← quadratic_char_neg_one_iff_not_is_square at h h0,
-  rw [← quadratic_char_one_iff_is_square h1, map_mul, h0, h, neg_mul_neg, mul_one]
-end
+have X : ∀ {a : F}, quadratic_char F a = -1 ↔ ¬is_square a :=
+  λ a, quadratic_char_neg_one_iff_not_is_square,
+have h : (-1 : ℤ) < -1 * -1 := neg_one_lt_zero.trans int.one_pos,
+or_iff_not_imp_left.mpr $ λ h0, or_iff_not_imp_left.mpr $ λ h1, by_contra $ λ h2,
+  absurd (X.mpr h2) (((quadratic_char F).map_mul a b).symm.trans_gt $
+    h.trans_eq $ congr_arg2 _ (X.mpr h0).symm (X.mpr h1).symm).ne.symm
 
 
 
-/-- Final solution, part 2 -/
-theorem final_solution_part2 [decidable_eq F] : ∃ a : F, a ^ 8 = 16 :=
-begin
-  ---- Reduce to finding a square `c : F` such that `c ^ 4 = 16`
-  suffices : ∃ a : F, is_square a ∧ a ^ 4 = 2 ^ 4,
-  { rcases this with ⟨_, ⟨c, rfl⟩, h⟩,
-    refine ⟨c, _⟩,
-    rw [bit0, ← two_mul, pow_mul, sq, h],
-    norm_num },
-
-  ---- If `2` or `-2` is a square mod `p`, we are easily done
-  rcases (is_square_or_mul (-1 : F) (2 : F)).symm with (h | h) | ⟨i, h⟩,
-  exact ⟨2, h, rfl⟩,
-  exact ⟨(-1) * 2, h, by rw [neg_one_mul, neg_pow_bit0]⟩,
-  
-  -- The case where `-1` is a square mod `p` remains
-  rw ← sq at h; refine ⟨2 * i, ⟨1 + i, _⟩, _⟩,
-  rw [← sq, add_sq', one_pow, ← h, add_neg_self, zero_add, mul_one],
-  rw [mul_comm, mul_pow]; change i ^ (2 * 2) * 2 ^ 4 = 2 ^ 4,
-  rw [pow_mul, ← h, neg_one_sq, one_mul]
-end
+/-- Final solution, extra -/
+theorem final_solution_extra [decidable_eq F] : ∃ a : F, a ^ 8 = 16 :=
+---- Reduce to finding a square `c : F` such that `c ^ 4 = 2 ^ 4`
+suffices ∃ a : F, is_square a ∧ a ^ 4 = 2 ^ 4,
+  from exists.elim this $ λ a h, exists.elim h.1 $ λ c h0,
+    ⟨c, (pow_mul c 2 4).trans $ (sq c).symm ▸ h0 ▸ h.2.trans (by ring)⟩,
+---- Find such `c`
+(is_square_or_mul (-1 : F) (2 : F)).elim
+-- Case 1: `-1` is a square mod `p`
+(λ h, exists.elim h $ λ i h, ⟨2 * i,
+  ⟨1 + i, by rw [add_mul_self_eq, mul_one, mul_one, ← h, add_neg_cancel_comm]⟩,
+  suffices i ^ 4 = 1, from (mul_pow 2 i 4).trans $ this.symm ▸ mul_one _,
+  (pow_mul i 2 2).trans $ (sq i).symm ▸ h ▸ neg_one_sq⟩)
+$ λ h, h.elim
+-- Case 2: `2` is a square mod `p`
+(λ h, ⟨2, h, rfl⟩)
+-- Case 3: `-2` is a square mod `p`
+(λ h, ⟨(-1) * 2, h, (neg_one_mul (2 : F)).symm ▸ neg_pow_bit0 _ 2⟩)
 
 end IMO2007N2
 end IMOSL
--- src/IMO2007/N2/N2_main.lean
+++ src/IMO2007/N2/N2_main.lean
@@ -5,29 +5,34 @@ import ring_theory.int.basic data.nat.parity
 namespace IMOSL
 namespace IMO2007N2
 
-/-- Final solution, part 1 -/
-theorem final_solution_part1 {b n : ℕ} (h : 0 < b)
+set_option profiler true
+set_option profiler.threshold 0.05
+
+/-- Final solution with explicit `k` -/
+theorem final_solution' {b n : ℕ} (h : 0 < b) (h0 : ∃ c : ℤ, (b ^ 2 : ℤ) ∣ b - c ^ n) :
+  ∃ a : ℤ, (b : ℤ) = a ^ n :=
+---- Reduce to `c^n ∼ b`
+suffices ∃ c : ℤ, associated (c ^ n) b,
+  from exists.elim this $ λ c h1, (int.associated_iff.mp h1).elim (λ h1, ⟨c, h1.symm⟩) $
+    λ h1, ⟨-c, suffices odd n, from (neg_eq_iff_eq_neg.mpr h1).symm.trans (this.neg_pow c).symm,
+      nat.odd_iff_not_even.mpr $ λ h0, (h0.pow_nonneg c).not_lt $
+        h1.trans_lt $ neg_neg_of_pos $ int.coe_nat_pos.mpr h⟩,
+---- Prove `c^n ∼ b`
+exists.elim h0 $ λ c h0, exists.elim h0 $ λ d h0,
+have h0 : (b : ℤ) * (1 - b * d) = c ^ n,
+  from (mul_one_sub _ _).trans $ mul_assoc (b : ℤ) b d ▸ sq (b : ℤ) ▸
+    sub_eq_iff_eq_add.mpr (sub_eq_iff_eq_add'.mp h0),
+have h1 : gcd (b : ℤ) (1 - b * d) = 1,
+  from (gcd_eq_of_dvd_sub_right ⟨d, sub_sub_cancel 1 (b * d)⟩).symm.trans (gcd_one_right b),
+exists_associated_pow_of_mul_eq_pow (int.is_unit_iff.mpr $ or.inl h1) h0
+
+
+
+/-- Final solution -/
+theorem final_solution {b n : ℕ} (h : 0 < b)
     (h0 : ∀ k : ℕ, 0 < k → ∃ c : ℤ, (k : ℤ) ∣ b - c ^ n) :
   ∃ a : ℤ, (b : ℤ) = a ^ n :=
-begin
-  replace h0 := h0 (b ^ 2) (pow_pos h 2),
-  suffices h1 : ∃ c : ℤ, associated (c ^ n) b,
-  { cases h1 with c h1,
-    rw int.associated_iff at h1,
-    cases h1 with h1 h1,
-    exact ⟨c, h1.symm⟩,
-    use (-c); rw [neg_pow, h1, odd.neg_one_pow, neg_one_mul, neg_neg],
-    rw nat.odd_iff_not_even; intros h0,
-    refine not_lt_of_le (even.pow_nonneg h0 c) _,
-    rwa [h1, right.neg_neg_iff, int.coe_nat_pos] },
-
-  rcases h0 with ⟨c, d, h0⟩,
-  rw [nat.cast_pow, sq, mul_assoc, sub_eq_iff_eq_add, ← sub_eq_iff_eq_add', ← mul_one_sub] at h0,
-  refine exists_associated_pow_of_mul_eq_pow (int.is_unit_iff.mpr _) h0; left,
-  rw [← int.coe_gcd, nat.cast_eq_one, int.gcd_eq_one_iff_coprime],
-  refine ⟨d, 1, _⟩,
-  rw [one_mul, mul_comm, add_sub_cancel'_right]
-end
+  final_solution' h $ int.coe_nat_pow b 2 ▸ h0 (b ^ 2) (pow_pos h 2)
 
 end IMO2007N2
 end IMOSL
